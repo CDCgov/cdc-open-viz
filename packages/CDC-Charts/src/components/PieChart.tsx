8c32394794312a40d0d5691d061d3a9778efce15
mpallansch <matthew.pallansch@gmail.com>
2020-12-08 16:49:07 -0500

    Maps Data to Pie Chart

--- packages/CDC-Charts/src/components/PieChart.tsx
+++ packages/CDC-Charts/src/components/PieChart.tsx
@@ -1,110 +1,13 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import Pie, { ProvidedProps, PieArcDatum } from '@visx/shape/lib/shapes/Pie';
 import { scaleOrdinal } from '@visx/scale';
 import { Group } from '@visx/group';
-import { GradientPinkBlue } from '@visx/gradient';
-import letterFrequency, { LetterFrequency } from '@visx/mock-data/lib/mocks/letterFrequency';
-import browserUsage, { BrowserUsage as Browsers } from '@visx/mock-data/lib/mocks/browserUsage';
 import { animated, useTransition, interpolate } from 'react-spring';
 import Context from '../context.tsx';
-
-// data and types
-type BrowserNames = keyof Browsers;
-
-interface BrowserUsage {
-  label: BrowserNames;
-  usage: number;
-}
-
-const letters: LetterFrequency[] = letterFrequency.slice(0, 4);
-const browserNames = Object.keys(browserUsage[0]).filter(k => k !== 'date') as BrowserNames[];
-const browsers: BrowserUsage[] = browserNames.map(name => ({
-  label: name,
-  usage: Number(browserUsage[0][name]),
-}));
-
-// accessor functions
-const usage = (d: BrowserUsage) => d.usage;
-const frequency = (d: LetterFrequency) => d.frequency;
-
-// color scales
-const getBrowserColor = scaleOrdinal({
-  domain: browserNames,
-  range: [
-    'rgba(255,255,255,0.7)',
-    'rgba(255,255,255,0.6)',
-    'rgba(255,255,255,0.5)',
-    'rgba(255,255,255,0.4)',
-    'rgba(255,255,255,0.3)',
-    'rgba(255,255,255,0.2)',
-    'rgba(255,255,255,0.1)',
-  ],
-});
-const getLetterFrequencyColor = scaleOrdinal({
-  domain: letters.map(l => l.letter),
-  range: ['rgba(93,30,91,1)', 'rgba(93,30,91,0.8)', 'rgba(93,30,91,0.6)', 'rgba(93,30,91,0.4)'],
-});
-
-const defaultMargin = { top: 20, right: 20, bottom: 20, left: 20 };
-
-export default function PieChart() {
-  const { pageContext } = useContext<any>(Context);
-  const [selectedBrowser, setSelectedBrowser] = useState<string | null>(null);
-  const [selectedAlphabetLetter, setSelectedAlphabetLetter] = useState<string | null>(null);
-
-  let width = pageContext.dimensions.chartWidth;
-  let height = Math.max(500, pageContext.dimensions.height);
-  let margin = pageContext.config.padding;
-
-  if (width < 10) return null;
-
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-  const radius = Math.min(innerWidth, innerHeight) / 2;
-  const centerY = innerHeight / 2;
-  const centerX = innerWidth / 2;
-  const donutThickness = 50;
-
-  return (
-    <svg width={width} height={height}>
-      <Group top={centerY + margin.top} left={centerX + margin.left}>
-        <Pie
-          data={
-            selectedAlphabetLetter
-              ? letters.filter(({ letter }) => letter === selectedAlphabetLetter)
-              : letters
-          }
-          pieValue={frequency}
-          pieSortValues={() => -1}
-          outerRadius={radius - donutThickness * 1.3}
-        >
-          {pie => (
-            <AnimatedPie<LetterFrequency>
-              {...pie}
-              getKey={({ data: { letter } }) => letter}
-              onClickDatum={({ data: { letter } }) =>
-                setSelectedAlphabetLetter(
-                  selectedAlphabetLetter && selectedAlphabetLetter === letter ? null : letter,
-                )
-              }
-              getColor={({ data: { letter } }) => getLetterFrequencyColor(letter)}
-            />
-          )}
-        </Pie>
-      </Group>
-    </svg>
-  );
-}
 
 // react-spring transition definitions
 type PieStyles = { startAngle: number; endAngle: number; opacity: number };
 
-const fromLeaveTransition = ({ endAngle }: PieArcDatum<any>) => ({
-  // enter from 360° if end angle is > 180°
-  startAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
-  endAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
-  opacity: 0,
-});
 const enterUpdateTransition = ({ startAngle, endAngle }: PieArcDatum<any>) => ({
   startAngle,
   endAngle,
@@ -156,13 +59,11 @@ function AnimatedPie<Datum>({
             <g key={key}>
               <animated.path
                 // compute interpolated path d attribute from intermediate angle values
-                d={interpolate([props.startAngle, props.endAngle], (startAngle, endAngle) =>
-                  path({
-                    ...arc,
-                    startAngle,
-                    endAngle,
-                  }),
-                )}
+                d={interpolate([props.startAngle, props.endAngle], (startAngle, endAngle) => path({
+                  ...arc,
+                  startAngle,
+                  endAngle,
+                }))}
                 fill={getColor(arc)}
                 onClick={() => onClickDatum(arc)}
                 onTouchStart={() => onClickDatum(arc)}
@@ -187,5 +88,49 @@ function AnimatedPie<Datum>({
         },
       )}
     </>
+  );
+}
+
+export default function PieChart() {
+  const { pageContext } = useContext<any>(Context);
+
+  const getColor = scaleOrdinal({
+    domain: pageContext.data.map(d => d[pageContext.config.xAxis.dataKey]),
+    range: ['rgba(93,30,91,1)', 'rgba(93,30,91,0.8)', 'rgba(93,30,91,0.6)', 'rgba(93,30,91,0.4)'],
+  });
+
+  const width = pageContext.dimensions.chartWidth;
+  const height = Math.max(500, pageContext.dimensions.height);
+  const margin = pageContext.config.padding;
+
+  if (width < 10) return null;
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  const radius = Math.min(innerWidth, innerHeight) / 2;
+  const centerY = innerHeight / 2;
+  const centerX = innerWidth / 2;
+  const donutThickness = 50;
+
+  return (
+    <svg width={width} height={height}>
+      <Group top={centerY + margin.top} left={centerX + margin.left}>
+        <Pie
+          data={pageContext.data}
+          pieValue={d => d[pageContext.config.yAxis.dataKey]}
+          pieSortValues={() => -1}
+          outerRadius={radius - donutThickness * 1.3}
+        >
+          {pie => (
+            <AnimatedPie<any>
+              {...pie}
+              getKey={d => d.data[pageContext.config.xAxis.dataKey]}
+              getColor={d => getColor(d.data[pageContext.config.yAxis.dataKey])}
+              onClickDatum={() => {}}
+            />
+          )}
+        </Pie>
+      </Group>
+    </svg>
   );
 }
