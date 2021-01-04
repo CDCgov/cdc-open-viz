a87edaacb313aa0cca594200b6162be2325d9f3a
Daniel Immke <me@daniel.do>
2021-01-04 13:24:02 -0500

    Add editor flag in standalone mode for easy debugging

--- packages/CDC-Maps/src/index.js
+++ packages/CDC-Maps/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 
 import CdcMap from './App';
 
+// Editor flag available when working in standalone mode
+let isEditor = window.location.href.includes('editor=true');
+
 ReactDOM.render(
   <React.StrictMode>
-    <CdcMap />
+    <CdcMap isEditor={isEditor} />
   </React.StrictMode>,
   document.querySelector('#root')
 );
