2bb0b1456ceb3d5bb4238726cb359b4ad4d0b919
Daniel Immke <me@daniel.do>
2021-01-22 16:50:33 -0500

    Implement basic editor flow

---
+++ packages/editor/src/components/ConfigTab.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import CdcMap from '@cdc/map'; // TODO: Lazy
+import GlobalState from '../context';
+
+
+export default function ConfigTab() {
+    const { data, type } = useContext(GlobalState);
+
+    switch (type) {
+        case 'map':
+            return <CdcMap isEditor={true} config={{data: data}} />
+        default:
+            return <p>No visualization type selected.</p>
+    }
+}
