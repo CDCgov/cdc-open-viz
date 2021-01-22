a57aac3a3c870833b0d4a2ecbf2025d1abd600a0
Jeff Rainey <rainey.jeff@gmail.com>
2021-01-22 10:39:42 -0500

    beginning work on the choose vis tab, playing with idea around a dropdown

---
+++ packages/editor/src/components/ConfigureVisualization.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+export default function ConfigVis() {
+    return (
+        <div className="container-fuild">
+            <div className="row">
+                <div className="col">
+                    
+                </div>
+                <div className="col-sm-8">
+
+                </div>
+            </div>
+        </div>
+    )
+}
