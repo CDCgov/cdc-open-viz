58e8a34466ade362a1e561295f55be3193bc3fe1
Jeff Rainey <jeffrainey@Jeffs-MBP.rjpsystems>
2021-03-10 14:56:30 -0500

    adding ability to sort with enter key

--- packages/map/src/components/DataTable.js
+++ packages/map/src/components/DataTable.js
@@ -248,7 +248,7 @@ const DataTable = (props) => {
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th tabIndex="0" {...column.getHeaderProps(column.getSortByToggleProps())} className={column.isSorted ? column.isSortedDesc ? 'sort sort-desc' : 'sort sort-asc' : 'sort'}>
+                <th tabIndex="0" {...column.getHeaderProps(column.getSortByToggleProps())} className={column.isSorted ? column.isSortedDesc ? 'sort sort-desc' : 'sort sort-asc' : 'sort'} onKeyDown={(e) => { if (e.keyCode === 13) { column.toggleSortBy(); } }}>
                   {column.render('Header')}
                   <div {...column.getResizerProps()} className="resizer" />
                 </th>
