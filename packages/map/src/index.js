a2370404b9fe0bdfbf26c73387dbc154dc42d333
Jeff Rainey <jeffrainey@Jeffs-MBP.rjpsystems>
2021-04-06 16:57:46 -0400

    functional static hex map

--- packages/map/src/index.js
+++ packages/map/src/index.js
@@ -1,14 +1,16 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
-import CdcMap from './CdcMap';
+import HexMap from './components/HexMap';
+// import CdcMap from './CdcMap';
 
 // Editor flag available when working in standalone mode
 let isEditor = window.location.href.includes('editor=true');
 
 ReactDOM.render(
   <StrictMode>
-    <CdcMap isEditor={isEditor} />
+    {/* <CdcMap isEditor={isEditor} /> */}
+    <HexMap  width={1000} height={500} />
   </StrictMode>,
   document.querySelector('#react-container')
 );
