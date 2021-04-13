cbf830548b8d063f15c97c32536eecddd003c720
Jeff Rainey <jeffrainey@Jeffs-MBP.rjpsystems>
2021-04-13 15:07:14 -0400

    refactor hex maps to function like the USA maps

--- packages/map/src/index.js
+++ packages/map/src/index.js
@@ -1,16 +1,14 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
-import HexMap from './components/HexMap';
-// import CdcMap from './CdcMap';
+import CdcMap from './CdcMap';
 
 // Editor flag available when working in standalone mode
 let isEditor = window.location.href.includes('editor=true');
 
 ReactDOM.render(
   <StrictMode>
-    {/* <CdcMap isEditor={isEditor} /> */}
-    <HexMap  width={1000} height={500} />
+    <CdcMap isEditor={isEditor} />
   </StrictMode>,
   document.querySelector('#react-container')
 );
