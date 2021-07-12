935459fc81c04be146217a41d97ef829940620b4
Rob Shelnutt <rob@blackairplane.com>
2021-07-12 17:15:24 -0400

    Refactor to add editor and editor options, finalize canvas draw algorithm

--- packages/waffle-chart/src/CdcWaffleChart.js
+++ packages/waffle-chart/src/CdcWaffleChart.js
@@ -1,34 +1,217 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-let generateData = () => {
-  let demoData = 75.63
-  let spacer = 30
-  let radius = 14
-  let width = 298
+import EditorPanel from './components/EditorPanel'
+import defaults from './data/initial-state'
+import Loading from '@cdc/core/components/Loading'
+import ResizeObserver from 'resize-observer-polyfill'
+import Context from './context'
+import './scss/main.scss'
 
-  let svgData = []
+let chartText = 'of cases occurred at home/apartment'
+const canvas = React.createRef()
 
-  for (let i = 0; i <= 100; i++) {
-    svgData.push(<circle cx={radius} cy='284' r='14' style={{ fill: '#add8e6' }} shapeRendering={'geometricPrecision'} />)
+const WaffleChart = ({ data, isEditor, color = '#B0BBDB', highlight = '#33437B', spacer, radius }) => {
+  const ratio = (10 * (radius * 2)) + (9 * spacer)
+
+  const calculatePos = (axis, index) => {
+    let mod = axis === 'x' ? index % 10 : axis === 'y' ? Math.floor(index / 10) : null
+    return mod > 0 ? (mod * ((radius * 2) + spacer)) + radius : radius
   }
 
-  return svgData
-}
+  const drawCircle = (ctx, x, y, fill = color, stroke = false, strokeWidth = 0) => {
+    ctx.beginPath()
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    if (fill) {
+      ctx.fillStyle = fill
+      ctx.fill()
+    }
+    if (stroke) {
+      ctx.lineWidth = strokeWidth
+      ctx.strokeStyle = stroke
+      ctx.stroke()
+    }
+  }
 
-let Chart = () => {
+  useEffect(() => {
+    const ctx = canvas.current.getContext('2d')
+    ctx.width = ratio
+    ctx.height = ratio
+
+    for (let i = 0; i <= 100; i++) {
+      drawCircle(ctx, calculatePos('x', i), calculatePos('y', i), i + 1 <= (100 - Math.round(data)) ? color : highlight)
+    }
+  })
+
   return (
-    <svg viewBox="0 0 298 298" xmlns="http://www.w3.org/2000/svg">
-      {generateData()}
-    </svg>
+    <section className={'cdc-waffle-chart'} style={isEditor ? {paddingLeft: 350 + 'px'} : null}>
+      <div className="cdc-waffle-chart__chart">
+        <canvas ref={canvas} width={ratio} height={ratio}/>
+      </div>
+      <div className="cdc-waffle-chart__data">
+        <div className="cdc-waffle-chart__data--primary" style={{ color: highlight }}>{data + '%'}</div>
+        <div className="cdc-waffle-chart__data--text" style={{ color: highlight }}>{chartText}</div>
+      </div>
+    </section>
   )
 }
 
-function CdcWaffleChart(configUrl) {
+const CdcWaffleChart = (
+  {
+    configUrl,
+    config: configObj,
+    isDashboard = false,
+    isEditor = false,
+    setConfig: setParentConfig
+  }
+) => {
+  const [ config, setConfig ] = useState({})
+  const [ loading, setLoading ] = useState(true)
+
+  const {
+    title,
+    filters,
+    subtext
+  } = config
+
+  const getViewport = size => {
+    let result = 'lg'
+
+    const viewports = {
+      'lg': 1200,
+      'md': 992,
+      'sm': 768,
+      'xs': 576,
+      'xxs': 350
+    }
+
+    if (size > 1200) return result
+
+    for (let viewport in viewports) {
+      if (size <= viewports[viewport]) {
+        result = viewport
+      }
+    }
+
+    return result
+  }
+
+  const [ currentViewport, setCurrentViewport ] = useState('lg')
+
+  //Observes changes to outermost container and changes viewport size in state
+  const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      let newViewport = getViewport(entry.contentRect.width)
+      setCurrentViewport(newViewport)
+    }
+  })
+
+  const updateConfig = (newConfig) => {
+    // Deeper copy
+    Object.keys(defaults).forEach(key => {
+      if (newConfig[key] && 'object' === typeof newConfig[key] && !Array.isArray(newConfig[key])) {
+        newConfig[key] = { ...defaults[key], ...newConfig[key] }
+      }
+    })
+
+    //Enforce default values that need to be calculated at runtime
+    newConfig.runtime = {}
+    newConfig.runtime.uniqueId = Date.now()
+
+    //Check things that are needed and set error messages if needed
+    newConfig.runtime.editorErrorMessage = ''
+    setConfig(newConfig)
+  }
+
+  const loadConfig = async () => {
+    let response = configObj || await (await fetch(configUrl)).json()
+
+    // If data is included through a URL, fetch that and store
+    let responseData = response.data ?? {}
+
+    if (response.dataUrl) {
+      const dataString = await fetch(response.dataUrl)
+      responseData = await dataString.json()
+    }
+
+    response.data = responseData
+
+    updateConfig({ ...defaults, ...response })
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    loadConfig()
+  }, [])
+
+  let body = (<Loading/>)
+
+  if (loading === false) {
+    let classList = []
+
+    classList.push(config.theme)
+
+    body = (
+      <div className={'cdc-open-viz-module type-waffle-chart'}>
+        {isEditor && <EditorPanel/>}
+        <WaffleChart data={78} spacer={1} radius={6} isEditor={isEditor}/>
+      </div>
+    )
+  }
+
   return (
-    <div className={'cdc-waffle-chart'}>
-      <Chart />
-    </div>
+    <Context.Provider value={{ config, updateConfig, loading, data: config.data, setParentConfig, isDashboard }}>
+      {body}
+    </Context.Provider>
   )
 }
 
 export default CdcWaffleChart
+
+/* Constant */
+export const DATA_FUNCTION_MAX = 'Max'
+export const DATA_FUNCTION_COUNT = 'Count'
+export const DATA_FUNCTION_MEAN = 'Mean (Average)'
+export const DATA_FUNCTION_MEDIAN = 'Median'
+export const DATA_FUNCTION_MIN = 'Min'
+export const DATA_FUNCTION_MODE = 'Mode'
+export const DATA_FUNCTION_RANGE = 'Range'
+export const DATA_FUNCTION_SUM = 'Sum'
+export const DATA_FUNCTIONS = [
+  DATA_FUNCTION_COUNT,
+  DATA_FUNCTION_MAX,
+  DATA_FUNCTION_MEAN,
+  DATA_FUNCTION_MEDIAN,
+  DATA_FUNCTION_MIN,
+  DATA_FUNCTION_MODE,
+  DATA_FUNCTION_RANGE,
+  DATA_FUNCTION_SUM
+]
+
+export const BITE_LOCATION_TITLE = 'title'
+export const BITE_LOCATION_BODY = 'body'
+export const BITE_LOCATION_GRAPHIC = 'graphic'
+export const BITE_LOCATIONS =
+{
+  'title'
+:
+  'As a title in the body',
+    'body'
+:
+  'At the beginning of the body text',
+    'graphic'
+:
+  'As a graphic'
+}
+
+
+export const IMAGE_POSITION_LEFT = 'Left'
+export const IMAGE_POSITION_RIGHT = 'Right'
+export const IMAGE_POSITION_TOP = 'Top'
+export const IMAGE_POSITION_BOTTOM = 'Bottom'
+export const IMAGE_POSITIONS = [
+  IMAGE_POSITION_LEFT,
+  IMAGE_POSITION_RIGHT,
+  IMAGE_POSITION_TOP,
+  IMAGE_POSITION_BOTTOM,
+]
+
---
+++ packages/waffle-chart/src/components/EditorPanel.js
@@ -0,0 +1,357 @@
+import React, { useState, useEffect, memo, useContext } from 'react'
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from 'react-accessible-accordion';
+
+import { useDebounce } from 'use-debounce';
+import Context from '../context';
+import ErrorBoundary from '@cdc/core/components/ErrorBoundary';
+import { IMAGE_POSITIONS, BITE_LOCATIONS, DATA_FUNCTIONS } from '../CdcWaffleChart'
+
+const TextField = memo(({label, section = null, subsection = null, fieldName, updateField, value: stateValue, type = "input", i = null, min = null, max = null, ...attributes}) => {
+  const [ value, setValue ] = useState(stateValue);
+
+  const [ debouncedValue ] = useDebounce(value, 500);
+
+  useEffect(() => {
+    if('string' === typeof debouncedValue && stateValue !== debouncedValue ) {
+      updateField(section, subsection, fieldName, debouncedValue, i)
+    }
+  }, [debouncedValue, section, subsection, fieldName, i, stateValue, updateField])
+
+  let name = subsection ? `${section}-${subsection}-${fieldName}` : `${section}-${subsection}-${fieldName}`;
+
+  const onChange = (e) => {
+    //TODO: This block gives a warning/error in the console, but it still works.
+    if('number' !== type || min === null){
+      setValue(e.target.value);
+    } else {
+      if(!e.target.value || ( parseFloat(min) <= parseFloat(e.target.value ) & parseFloat(max) >= parseFloat(e.target.value))) {
+        setValue(e.target.value);
+      } else {
+        setValue(min.toString());
+      }
+    }
+  };
+
+  let formElement = <input type="text" name={name} onChange={onChange} {...attributes} value={value} />
+
+  if('textarea' === type) {
+    formElement = (
+      <textarea name={name} onChange={onChange} {...attributes} value={value}></textarea>
+    )
+  }
+
+  if('number' === type) {
+    formElement = <input type="number" name={name} onChange={onChange} {...attributes} value={value} />
+  }
+
+  return (
+    <label>
+      <span className="edit-label column-heading">{label}</span>
+      {formElement}
+    </label>
+  )
+})
+
+const Select = memo(({label, value, options, fieldName, section = null, subsection = null, required = false, updateField, initial: initialValue, ...attributes}) => {
+  let optionsJsx = '';
+  if ( Array.isArray(options)) { //Handle basic array
+    optionsJsx = options.map(optionName => <option value={optionName} key={optionName}>{optionName}</option>)
+  } else { //Handle object with value/name pairs
+    optionsJsx = [];
+    for (const [optionValue, optionName] of Object.entries(options)) {
+      optionsJsx.push(<option value={optionValue} key={optionValue}>{optionName}</option>)
+    }
+  }
+
+  if(initialValue) {
+    optionsJsx.unshift(<option value="" key="initial">{initialValue}</option>)
+  }
+
+  return (
+    <label>
+      <span className="edit-label">{label}</span>
+      <select className={required && !value ? 'warning' : ''} name={fieldName} value={value} onChange={(event) => { updateField(section, subsection, fieldName, event.target.value) }} {...attributes}>
+        {optionsJsx}
+      </select>
+    </label>
+  )
+})
+
+const headerColors = ['theme-blue','theme-purple','theme-brown','theme-teal','theme-pink','theme-orange','theme-slate','theme-indigo','theme-cyan','theme-green','theme-amber']
+
+const EditorPanel = memo(() => {
+  const {
+    config,
+    updateConfig,
+    loading,
+    data,
+    setParentConfig,
+    isDashboard
+  } = useContext(Context);
+
+  const [ displayPanel, setDisplayPanel ] = useState(true);
+  const enforceRestrictions = (updatedConfig) => {
+   //If there are any dependencies between fields, etc../
+  };
+
+  const updateField = (section, subsection, fieldName, newValue) => {
+    // Top level
+    if( null === section && null === subsection) {
+      let updatedConfig = {...config, [fieldName]: newValue};
+
+      if ( 'filterColumn' === fieldName ) {
+        updatedConfig.filterValue = '';
+      }
+
+      enforceRestrictions(updatedConfig);
+
+      updateConfig(updatedConfig);
+      return
+    }
+
+    const isArray = Array.isArray(config[section]);
+
+    let sectionValue = isArray ? [...config[section], newValue] : {...config[section], [fieldName]: newValue};
+
+    if(null !== subsection) {
+      if(isArray) {
+        sectionValue = [...config[section]]
+        sectionValue[subsection] = {...sectionValue[subsection], [fieldName]: newValue}
+      } else if(typeof newValue === "string") {
+        sectionValue[subsection] = newValue
+      } else {
+        sectionValue = {...config[section], [subsection]: { ...config[section][subsection], [fieldName]: newValue}}
+      }
+    }
+
+    let updatedConfig = {...config, [section]: sectionValue};
+
+    enforceRestrictions(updatedConfig);
+
+    updateConfig(updatedConfig)
+  }
+
+  const missingRequiredSections = () => {
+    //Whether to show error message if something is required to show a data-bite and isn't filled in
+    return false;
+  };
+
+  useEffect(() => {
+    // Pass up to Editor if needed
+    if(setParentConfig) {
+      const newConfig = convertStateToConfig()
+
+      setParentConfig(newConfig)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [config])
+
+  const onBackClick = () => {
+    if(isDashboard){
+      updateConfig({...config, editing: false});
+    } else {
+      setDisplayPanel(!displayPanel);
+    }
+  }
+
+  const Error = () => {
+    return (
+      <section className="waiting">
+        <section className="waiting-container">
+          <h3>Error With Configuration</h3>
+          <p>{config.runtime.editorErrorMessage}</p>
+        </section>
+      </section>
+    );
+  }
+
+  const Confirm = () => {
+    return (
+      <section className="waiting">
+        <section className="waiting-container">
+          <h3>Finish Configuring</h3>
+          <p>Set all required options to the left and confirm below to display a preview of the chart.</p>
+          <button className="btn" style={{margin: '1em auto'}} disabled={missingRequiredSections()} onClick={(e) => {e.preventDefault(); updateConfig({...config, newViz: false})}}>I'm Done</button>
+        </section>
+      </section>
+    );
+  }
+
+  const convertStateToConfig = () => {
+    let strippedState = JSON.parse(JSON.stringify(config))
+    if(false === missingRequiredSections()) {
+      delete strippedState.newViz
+    }
+    delete strippedState.runtime
+
+    return strippedState
+  }
+
+  const removeFilter = (index) => {
+    let filters = [...config.filters];
+
+    filters.splice(index, 1);
+
+    updateConfig({...config, filters})
+  }
+
+  const updateFilterProp = (name, index, value) => {
+    let filters = [...config.filters];
+
+    filters[index][name] = value;
+
+    updateConfig({...config, filters});
+  }
+
+  const addNewFilter = () => {
+    let filters = config.filters ? [...config.filters] : [];
+
+    filters.push({values: []});
+
+    updateConfig({...config, filters});
+  }
+
+  const getColumns = (filter = true) => {
+    let columns = {}
+
+    data.map(row => {
+      Object.keys(row).forEach(columnName => columns[columnName] = true)
+    })
+
+    return Object.keys(columns)
+  }
+
+  const getFilterColumnValues = (index) => {
+    let filterDataOptions = []
+    const filterColumnName = config.filters[index].columnName;
+    if (data && filterColumnName) {
+      data.forEach(function(row) {
+        if ( undefined !== row[filterColumnName] && -1 === filterDataOptions.indexOf(row[filterColumnName]) ) {
+          filterDataOptions.push(row[filterColumnName]);
+        }
+      })
+      filterDataOptions.sort();
+    }
+    return filterDataOptions;
+  }
+
+  if(loading) {
+    return null
+  }
+
+  return (
+    <ErrorBoundary component="EditorPanel">
+      {!config.newViz && config.runtime && config.runtime.editorErrorMessage && <Error /> }
+      {config.newViz && <Confirm />}
+      <button className={displayPanel ? `editor-toggle` : `editor-toggle collapsed`} title={displayPanel ? `Collapse Editor` : `Expand Editor`} onClick={onBackClick}></button>
+      <section className={displayPanel ? 'editor-panel' : 'hidden editor-panel'}>
+        <h2>Configure Data Bite</h2>
+        <section className="form-container">
+          <form>
+            <Accordion allowZeroExpanded={true}>
+              <AccordionItem> {/* General */}
+                <AccordionItemHeading>
+                  <AccordionItemButton>
+                    General
+                  </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <TextField value={config.title} fieldName="title" label="Title" placeholder="Data Bite Title" updateField={updateField} />
+                  <TextField type="textarea" value={config.biteBody} fieldName="biteBody" label="Message" updateField={updateField} />
+                  <TextField value={config.subtext} fieldName="subtext" label="Subtext/Citation" placeholder="Data Bite Subtext or Citation" updateField={updateField} />
+                </AccordionItemPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>
+                    Data
+                  </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <Select value={config.dataColumn || ""} fieldName="dataColumn" label="Data Column" updateField={updateField} initial="Select" options={getColumns()} />
+                  <Select value={config.dataFunction || ""} fieldName="dataFunction" label="Data Function" updateField={updateField} initial="Select" options={DATA_FUNCTIONS} />
+                  <ul className="column-edit">
+                    <li className="three-col">
+                      <TextField value={config.prefix} fieldName="prefix" label="Prefix" updateField={updateField} />
+                      <TextField value={config.suffix} fieldName="suffix" label="Suffix" updateField={updateField} />
+                      <TextField type="number" value={config.roundToPlace} fieldName="roundToPlace" label="Round" updateField={updateField} />
+                    </li>
+                  </ul>
+                </AccordionItemPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>
+                    Filters
+                  </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <ul className="filters-list">
+                    {config.filters && config.filters.map((filter, index) => (
+                        <fieldset className="edit-block">
+                          <button type="button" className="remove-column" onClick={() => {removeFilter(index)}}>Remove</button>
+                          <label>
+                            <span className="edit-label column-heading">Column</span>
+                            <select value={filter.columnName} onChange={(e) => {updateFilterProp('columnName', index, e.target.value)}}>
+                              <option value="">- Select Option -</option>
+                              {getColumns().map((dataKey) => (
+                                <option value={dataKey}>{dataKey}</option>
+                              ))}
+                            </select>
+                          </label>
+                          <label>
+                            <span className="edit-label column-heading">Column Value</span>
+                            <select value={filter.columnValue} onChange={(e) => {updateFilterProp('columnValue', index, e.target.value)}}>
+                              <option value="">- Select Option -</option>
+                              {getFilterColumnValues(index).map((dataKey) => (
+                                <option value={dataKey}>{dataKey}</option>
+                              ))}
+                            </select>
+                          </label>
+                        </fieldset>
+                      )
+                    )}
+                  </ul>
+
+                  <button type="button" onClick={addNewFilter} className="btn btn-primary">Add Filter</button>
+                </AccordionItemPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>
+                    Visual
+                  </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <Select value={config.biteLocation} fieldName="biteLocation" label="Data Bite Placement" updateField={updateField} options={BITE_LOCATIONS} initial="Select" />
+                  <TextField value={config.imageUrl} fieldName="imageUrl" label="Image URL" updateField={updateField} />
+                  <Select value={config.imagePosition || ""} fieldName="imagePosition" label="Image or Data Bite Graphic Position" updateField={updateField} initial="Select" options={IMAGE_POSITIONS} />
+                  <TextField type="number" value={config.biteFontSize} fieldName="biteFontSize" label="Bite Font Size" updateField={updateField} min="16" max="65" />
+                  <Select value={config.fontSize} fieldName="fontSize" label="Overall Font Size" updateField={updateField} options={['small', 'medium', 'large']} />
+                  <label className="header">
+                    <span className="edit-label">Theme</span>
+                    <ul className="color-palette">
+                      {headerColors.map( (palette) => (
+                        <li title={ palette } key={ palette } onClick={ () => { updateConfig({...config, theme: palette})}} className={ config.theme === palette ? "selected " + palette : palette}>
+                        </li>
+                      ))}
+                    </ul>
+                  </label>
+                </AccordionItemPanel>
+              </AccordionItem>
+            </Accordion>
+          </form>
+        </section>
+      </section>
+    </ErrorBoundary>
+  )
+})
+
+export default EditorPanel;
