20eeeca66daa4c56f789ce40e10146d070014ec0
mpallansch <matthew.pallansch@gmail.com>
2021-07-23 12:50:23 -0400

    WCMSRD-12182

--- packages/dashboard/src/components/Grid.js
+++ packages/dashboard/src/components/Grid.js
@@ -12,14 +12,15 @@ const Grid = () => {
       rows: [
         ...rows,
         [{width: 12}, {}, {}]
-      ]
+      ],
+      uuid: Date.now()
     })
   }
 
   return (
     <div className="builder-grid">
       <button onClick={() => {setPreview(true)}}>Preview</button>
-      {rows.map((row, idx) => <Row row={row} idx={idx} key={idx}/>)}
+      {rows.map((row, idx) => <Row row={row} idx={idx} uuid={row.uuid} key={idx}/>)}
       <button className="btn btn--fluid add-row" onClick={addRow}>Add Row</button>
     </div>
   )
