3a9547f7b298843b39e7c8b6f48332079e0629bd
David Cummo <dmo7@cdc.gov>
2021-09-14 11:31:11 -0400

    Fixed cant find react issue in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -79,9 +79,6 @@ module.exports = (env = {}, { mode }) => {
         },
     },
     resolve: {
-      alias: {
-        react: path.resolve('./node_modules/react')
-      },
       extensions: ['*', '.tsx', '.ts', '.js'],
     },
     target: 'web',
