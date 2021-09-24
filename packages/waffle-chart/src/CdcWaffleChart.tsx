c5e2f6cd7ef54d888b6aed15a62f70a2362cbaa8
Rob Shelnutt <rob@blackairplane.com>
2021-09-24 13:52:59 -0400

    Cleanup

--- packages/waffle-chart/src/CdcWaffleChart.tsx
+++ packages/waffle-chart/src/CdcWaffleChart.tsx
@@ -283,19 +283,6 @@ const WaffleChart = ({ config }) => {
     return (width * 10) + (spacer * 9)
   }
 
-  const personShapeScale = () => {
-    return width / 512
-  }
-
-  /* Fullsize person icon ------------------
-    <path style={{transform: `scale(${personShapeScale()}) translateX(${node.x * (personShapeScale() * 512)}px) translateY(${node.y * (personShapeScale() * 512)}px)`}}
-          fill={node.color} fillOpacity={node.opacity} key={key}
-          d="M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59
-          10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0
-          24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z">
-    </path>
- */
-
   return (
     <section className={`cdc-waffle-chart ${theme}${config.fontSize ? ' font-' + config.fontSize : ''}`}>
       <div className="cdc-waffle-chart__header">{parse(title)}</div>
