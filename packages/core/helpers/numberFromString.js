40c998784a9218f90802d4e8c88c55b9b8805069
Rob Shelnutt <rob@blackairplane.com>
2021-10-04 11:26:12 -0400

    Update numberFromString helper function to support negative entries

--- packages/core/helpers/numberFromString.js
+++ packages/core/helpers/numberFromString.js
@@ -1,6 +1,6 @@
 export default function numberFromString(value = '') {
     // Only do this to values that are ONLY numbers - without this parseFloat strips all the other text
-    let nonNumeric = /[^\d.]/g
+    let nonNumeric = /[^\d.-]/g
 
     if( false === Number.isNaN( parseFloat(value) ) && null === String(value).match(nonNumeric) ) {
         return parseFloat(value)
