53b77a18fb0443631ad3a537a34c4804333086c8
David Cummo <dmo7@cdc.gov>
2022-01-28 10:30:28 -0500

    fixing fips codes in sample data

--- packages/map/src/CdcMap.js
+++ packages/map/src/CdcMap.js
@@ -923,6 +923,7 @@ const CdcMap = ({containerEl, className, config, navigationHandler: customNaviga
         })
 
         // If there's a name for the geo, add UIDs
+        debugger;
         if(newState.columns.geo.name || newState.columns.geo.fips) {
             addUIDs(newState, newState.columns.geo.name || newState.columns.geo.fips)
         }
