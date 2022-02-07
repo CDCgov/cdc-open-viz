93616dd7ac2cb387a6057c78bf435fd45e6d2b19
mpallansch <matthew.pallansch@gmail.com>
2022-02-07 11:29:15 -0500

    Fixes Map Filters Not Working With Dynamic Data

--- packages/map/src/CdcMap.js
+++ packages/map/src/CdcMap.js
@@ -511,7 +511,7 @@ const CdcMap = ({className, config, navigationHandler: customNavigationHandler, 
                 for(let i = 0; i < filters.length; i++) {
                     const {columnName, active} = filters[i]
 
-                    if (row[columnName] !== active) return false // Bail out, not part of filter
+                    if (String(row[columnName]) !== String(active)) return false // Bail out, not part of filter
                 }
             }
 
@@ -776,7 +776,8 @@ const CdcMap = ({className, config, navigationHandler: customNavigationHandler, 
                     .then(responseText => {
                         const parsedCsv = Papa.parse(responseText, {
                             header: true,
-                            dynamicTyping: true
+                            dynamicTyping: true,
+                            skipEmptyLines: true
                         })
                         return parsedCsv.data
                     })
