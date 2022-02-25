205eb96717fbeaf206699224d9a3d2892129f456
Adam Doe <adamnickolasdoe@gmail.com>
2022-02-25 12:14:28 -0800

    Maps > Loading issue

--- packages/map/src/CdcMap.js
+++ packages/map/src/CdcMap.js
@@ -973,13 +973,21 @@ const CdcMap = ({className, config, navigationHandler: customNavigationHandler, 
 
         // Once we have a config verify that it is an object and load it
         if('object' === typeof configData) {
-            loadConfig(configData)
+            if(configData.general.geoType === 'us-county') {
+                console.log('here')
+                setTimeout( () =>{
+                    loadConfig(configData)
+                }, 5000)
+            } else {
+                loadConfig(configData)
+            }
         }
     }
 
     // Initial load
     useEffect(() => {
         init()
+        //window.addEventListener('load', init)
     }, [])
 
     useEffect(() => {
