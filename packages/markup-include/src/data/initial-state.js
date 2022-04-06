6b5f27d6e50a60edcba8ecb895a7cdbeba720120
Rob Shelnutt <rob@blackairplane.com>
2022-04-06 13:10:14 -0400

    Add error captures, cleanup

--- packages/markup-include/src/data/initial-state.js
+++ packages/markup-include/src/data/initial-state.js
@@ -1,6 +1,6 @@
 export default {
   "title": "Markup Include",
-  "dataUrl": "/examples/example-data.json",
+  "srcUrl": "/examples/example-data.json",
   "showHeader": false,
   "type": "markup-include"
 }
