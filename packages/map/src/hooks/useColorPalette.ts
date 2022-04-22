dd249fc85f16fe467141fceab1eedb83c2012a0d
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-04-21 21:47:33 -0400

    register useReducer hook

--- packages/map/src/hooks/useColorPalette.ts
+++ packages/map/src/hooks/useColorPalette.ts
@@ -1,7 +1,11 @@
+import { useReducer } from 'react';
 import { colorPalettes } from '../data/color-palettes';
 
 
 // create custon hook and export
 export function useColorPalette(){
+    // register reducer hook to handle multiple states at a time
+  const [state, dispatch] = useReducer(reducer, initialState);
+
     return ;
 }
