ea4db14544e68ba2b7f2c74c4cbaedb9e20071a9
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-04-22 18:07:53 -0400

    Chart > create constants & import hooks

---
+++ packages/chart/src/hooks/useColorPalette.ts
@@ -0,0 +1,7 @@
+import { useEffect, useReducer } from 'react';
+// create constants 
+export const SEQUENTIAL = "SEQUENTIAL";
+export const SEQUENTIAL_REVERSE ='SEQUENTIAL_REVERSE';
+export const NON_SEQUENTIAL = "NON_SEQUENTIAL";
+export const NON_SEQUENTIAL_REVERSE = "NON_SEQUENTIAL_REVERSE";
+
