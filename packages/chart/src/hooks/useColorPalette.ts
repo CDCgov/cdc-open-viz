540b55757c4c285c209a52b108fda2031da4f589
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-04-22 18:09:33 -0400

    Chart > create custom types for state & action and create state & initialstate

--- packages/chart/src/hooks/useColorPalette.ts
+++ packages/chart/src/hooks/useColorPalette.ts
@@ -5,3 +5,29 @@ export const SEQUENTIAL_REVERSE ='SEQUENTIAL_REVERSE';
 export const NON_SEQUENTIAL = "NON_SEQUENTIAL";
 export const NON_SEQUENTIAL_REVERSE = "NON_SEQUENTIAL_REVERSE";
 
+// state interface
+interface State {
+    readonly isSequentialReversed: boolean;
+     readonly isNonSequentialReversed: boolean;
+     readonly filteredSequential:any[];
+     readonly filteredQualitative: any[];
+  }
+  // custom type
+  type Case = "SEQUENTIAL_REVERSE"|"NON_SEQUENTIAL_REVERSE";
+  // action type
+  interface Action<Pallets> {
+   readonly type:
+      | "SEQUENTIAL" 
+      | "SEQUENTIAL_REVERSE"
+      | "NON_SEQUENTIAL"
+      | "NON_SEQUENTIAL_REVERSE"
+    payload: Pallets ;
+  }
+  
+  const initialState: State = {
+    isNonSequentialReversed: false,
+    isSequentialReversed: false,
+    filteredSequential: [],
+    filteredQualitative: [],
+  };
+
