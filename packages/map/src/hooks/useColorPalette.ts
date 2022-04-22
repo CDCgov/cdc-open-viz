8fac7163825f96b7cefa0dcc82f76da2ed00fc35
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-04-22 15:24:16 -0400

    Map > update all contants

--- packages/map/src/hooks/useColorPalette.ts
+++ packages/map/src/hooks/useColorPalette.ts
@@ -33,66 +33,65 @@ const initialState:State = {
   };
 
 // create reducer function to handle multiple states & manupilate with each state
-function reducer (state:State,action:Action):State{
+function reducer<T> (state:State,action:Action<T>):State{     // <T> refers to generic type
   const palletNamesArr:string[] = Object.keys(action.payload); // action.payload === colorPalettes object
   let reverseRegex = new RegExp('reverse$'); // matches a string that ends in with "reverse".
   let qualitativeRegex = new RegExp('^qualitative'); //matches any string that starts with "qualitative".
 
   switch(action.type){
-    case ON_SEQUENTIAL:
+    case SEQUENTIAL:
       const sequential = palletNamesArr.filter((name: string) => !name.match(qualitativeRegex) && !name.match(reverseRegex));
   
       return { ...state, isSequentialReversed: false, filteredPallets: sequential };
-     case ON_SEQUENTIAL_REVERSE:
+     case SEQUENTIAL_REVERSE:
         const sequentialReverse = palletNamesArr.filter((name: string) => !name.match(qualitativeRegex) && name.match(reverseRegex));
         return { ...state, isSequentialReversed: true, filteredPallets: sequentialReverse };
   
-     case ON_NON_SEQUENTIAL:
+     case NON_SEQUENTIAL:
         const qualitative = palletNamesArr.filter((name: string) => name.match(qualitativeRegex) && (!name.match(reverseRegex) && !name.includes('qualitative9')));
         return { ...state, isNonSequentialReversed: false, filteredQualitative: qualitative };
       
-    case ON_NON_SEQUENTIAL_REVERSE:
+    case NON_SEQUENTIAL_REVERSE:
         const qualitativeReverse = palletNamesArr.filter((name: string) => name.match(qualitativeRegex) && name.match(reverseRegex));
         return { ...state,isNonSequentialReversed: true,filteredQualitative: qualitativeReverse};
       default : return state;
   }
 }
 // create custon hook and export
-export function useColorPalette(){
+export function useColorPalette<T>(colorPalettes:T){
     // register reducer hook to handle multiple states at a time
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleSwitch = (caseValue:CaseValue) => {
-   // this func will handle sequantial & sequatial-reverse colors and ll be mounted to switch toogle bar.
-      if((caseValue !== Case.ON_NON_SEQUENTIAL  && caseValue !== Case.ON_SEQUENTIAL)  || caseValue === undefined ){
+  const handleSwitch = (caseValue:Case) => {
+  // this func will handle sequantial & sequatial-reverse colors and ll be mounted to switch toogle bar.
+      if(caseValue === undefined || caseValue === null ){
           console.error('Enter valid arguments to handleSwitch function')
           return ;
       };
 
-      if(caseValue === Case.ON_SEQUENTIAL){
+    if(caseValue === "SEQUENTIAL_REVERSE"){
         if (state.isSequentialReversed) {
-            dispatch({ type: ON_SEQUENTIAL, payload: colorPalettes });
-          } else {
-            dispatch({ type: ON_SEQUENTIAL_REVERSE, payload: colorPalettes });
+            dispatch({ type: SEQUENTIAL, payload: colorPalettes });
+        } else {
+            dispatch({ type: SEQUENTIAL_REVERSE, payload: colorPalettes });
           };
       }
 
-
-      if(caseValue === Case.ON_NON_SEQUENTIAL){
-        if (state.isNonSequentialReversed) {
-            dispatch({ type: ON_NON_SEQUENTIAL, payload: colorPalettes });
-          } else {
-            dispatch({ type: ON_NON_SEQUENTIAL_REVERSE, payload: colorPalettes });
-          };
+    if (caseValue === "NON_SEQUENTIAL_REVERSE") {
+      if (state.isNonSequentialReversed) {
+          
+        } else {
+        dispatch({type: NON_SEQUENTIAL_REVERSE, payload: colorPalettes});
       }
+    }
 };
 
   /// useEffect will run when page render first time and provide default filtered colors
 
   useEffect(() => {
-    dispatch({ type: ON_SEQUENTIAL, payload: colorPalettes });
-    dispatch({ type: ON_NON_SEQUENTIAL, payload: colorPalettes });
-  }, []);
+    dispatch({ type: SEQUENTIAL, payload: colorPalettes });
+    dispatch({ type: NON_SEQUENTIAL, payload: colorPalettes });
+  }, [colorPalettes]);
 
     return { state,handleSwitch}
 }
