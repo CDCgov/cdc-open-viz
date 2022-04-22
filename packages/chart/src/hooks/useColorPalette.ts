4316d7da8b450ae4645b909c34fb3c593a569874
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-04-22 18:10:40 -0400

    Chart > create reducer function and handle 4 states at a time

--- packages/chart/src/hooks/useColorPalette.ts
+++ packages/chart/src/hooks/useColorPalette.ts
@@ -30,4 +30,31 @@ interface State {
     filteredSequential: [],
     filteredQualitative: [],
   };
+  
+  // reducer func ll handle 4 different state changes
+  function reducer<T>(state: State, action: Action<T>): State {
+    const colorNamesArr = Object.keys(action.payload); // action.payload ===colorPalletes object;
+    let reverseRegex = new RegExp('reverse$'); // matches a string that ends in with "reverse".
+    let sequentialRegex = new RegExp('^sequential'); //matches any string that starts with "sequential".
+    let qualitativeRegex = new RegExp('^qualitative'); //matches any string that starts with "qualitative".
+       
+    switch (action.type) {
+      case SEQUENTIAL:
+        const sequential: string[] = colorNamesArr.filter((name: string) =>name.match(sequentialRegex) && !name.match(reverseRegex));
+        return {...state,filteredSequential: sequential,isSequentialReversed:false};
+      case SEQUENTIAL_REVERSE:
+        const sequantialReversed: string[] = colorNamesArr.filter((name: string) =>!name.match(qualitativeRegex) && name.match(reverseRegex));
+        return {...state,isSequentialReversed: true,filteredSequential: sequantialReversed};
+      case NON_SEQUENTIAL:
+          const qualitative = colorNamesArr.filter((name:string)=>name.match(qualitativeRegex) && !name.match(reverseRegex) )
+        return { ...state, isNonSequentialReversed: false,filteredQualitative:qualitative };
+      case NON_SEQUENTIAL_REVERSE:
+          const qualitativeReverse = colorNamesArr.filter((name:string)=>!name.match(sequentialRegex) && name.match(reverseRegex) )
+  
+        return { ...state, isNonSequentialReversed: true,filteredQualitative:qualitativeReverse };
+      default:
+        return state;
+    }
+  }
+  
 
