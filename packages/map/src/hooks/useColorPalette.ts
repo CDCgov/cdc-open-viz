f5d6a1f9cb0f984d5c5116871da3ffd0cb942180
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-04-21 21:55:34 -0400

    create reducer func & initial state

--- packages/map/src/hooks/useColorPalette.ts
+++ packages/map/src/hooks/useColorPalette.ts
@@ -1,11 +1,20 @@
 import { useReducer } from 'react';
 import { colorPalettes } from '../data/color-palettes';
 
+// create initial state
+const initialState = {
+    filteredPallets: [],
+    filteredQualitative: [],
+    isSwitched: false,
+    isSwitched2: false,
+  };
 
+// create reducer function to handle multiple states & manupilate with each state
+function reducer (state,action){}
 // create custon hook and export
 export function useColorPalette(){
     // register reducer hook to handle multiple states at a time
   const [state, dispatch] = useReducer(reducer, initialState);
 
-    return ;
+    return;
 }
