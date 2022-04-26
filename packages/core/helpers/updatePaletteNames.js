5933a8ef02920b52c2dff08089a45f420dcb2c26
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-04-26 19:23:37 -0400

    Core > make naming readable.

--- packages/core/helpers/updatePaletteNames.js
+++ packages/core/helpers/updatePaletteNames.js
@@ -3,10 +3,10 @@ export function updatePaletteNames(colorPalettes) {
     let sequentialReversed = {};
   
     for (const [paletteName, hexCodeArr] of Object.entries(colorPalettes)) {
-       const paletteNameStr = String(paletteName);
+       const palette = String(paletteName);
 
-      if (!paletteNameStr.startsWith('qualitative') && !paletteNameStr.endsWith('reverse')){
-        let reverse = paletteNameStr.concat('reverse'); // add to the end of the string "reverse"
+      if (!palette.startsWith('qualitative') && !palette.endsWith('reverse')){
+        let reverse = palette.concat('reverse'); // add to the end of the string "reverse"
         sequentialReversed[reverse] = [...hexCodeArr].reverse(); // reverses arrays elements and create new keys on object
       }
     }
