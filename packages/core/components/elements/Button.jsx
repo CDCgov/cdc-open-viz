8f9ddc89c471f36b2a71f5ac9a499cb4aa21318d
Rob Shelnutt <rob@blackairplane.com>
2022-05-02 14:47:16 -0400

    Cleanup button test logging

--- packages/core/components/elements/Button.jsx
+++ packages/core/components/elements/Button.jsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import '../../styles/v2/components/button.scss'
 
 const Button = ({children, ...attributes}) => {
 
   let attributesObj = {...attributes, className: 'cove-button ' + attributes.className }
-
-  useEffect(() => {
-    console.log(attributesObj)
-  }, [])
 
   return (
     <button {...attributesObj}>{children}</button>
