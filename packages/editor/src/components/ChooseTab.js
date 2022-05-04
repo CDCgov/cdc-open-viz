fed90b5438a4cb541072eaa01fe5bc14c67b793a
Shawn Stringfield <shawn.stringfield@gmail.com>
2022-05-04 13:29:44 -0400

    added parens on map label

--- packages/editor/src/components/ChooseTab.js
+++ packages/editor/src/components/ChooseTab.js
@@ -94,7 +94,7 @@ export default function ChooseTab() {
             </ul>
             <div className="heading-2">Maps</div>
             <ul className="grid">
-                <li><IconButton label="U.S. State- or County-Level" type="map" subType="us" icon={ <UsaIcon /> } /></li>
+                <li><IconButton label="United States (State- or County-Level)" type="map" subType="us" icon={ <UsaIcon /> } /></li>
                 <li><IconButton label="World" type="map" subType="world" icon={ <GlobeIcon /> } /></li>
                 <li><IconButton label="U.S. State" type="map" subType="single-state" icon={ <AlabamaGraphic /> } /></li>
             </ul>
