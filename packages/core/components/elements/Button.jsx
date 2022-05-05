7c53c64ea7f6fb05e944103ab6d12dee1140a9d8
Rob Shelnutt <rob@blackairplane.com>
2022-05-05 19:10:35 -0400

    Update button component to support custom colors

--- packages/core/components/elements/Button.jsx
+++ packages/core/components/elements/Button.jsx
@@ -1,16 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import '../../styles/v2/components/button.scss'
 
-const Button = ({ fullwidth, children, ...attributes }) => {
+const Button = ({ fullwidth, textColor = null, bgColor = null, children, ...attributes }) => {
+  const [ isRendering, setIsRendering ] = useState(true)
+  const [ customColors, setCustomColors ] = useState({
+    color: null,
+    colorHover: null,
+    bg: null,
+    bgHover: null
+  })
 
   let attributesObj = {
     ...attributes,
-    className: 'cove-button' + (fullwidth ? ' full-width' : '') + (attributes.className ? ' ' + attributes.className : '')
+    className: 'cove-button' + (fullwidth ? ' fluid' : '') + (attributes.className ? ' ' + attributes.className : '')
+  }
+
+  const registerColorValue = (prop, keyBase, keyHover) => {
+    // Set up payload to modify state color object
+    let payload = {}
+
+    // Validate that the entry has content, else return null
+    const validate = (obj) => {
+      return obj.length > 0 ? obj : null
+    }
+
+    // Check provided prop to see if string or array, and parse colors accordingly
+    if (prop) {
+      if (typeof prop === 'string') {
+        payload[[ keyBase ]] = validate(prop)
+      } else if (Array.isArray(prop)) {
+        if (prop.length === 1) {
+          payload[[ keyBase ]] = validate(prop[0])
+        } else if (prop.length === 2) {
+          payload[[ keyBase ]] = validate(prop[0])
+          payload[[ keyHover ]] = validate(prop[1])
+        }
+      }
+    }
+
+    setCustomColors(colors => ({ ...colors, ...payload }))
+  }
+
+  useEffect(() => {
+    registerColorValue(textColor, 'color', 'colorHover')
+    registerColorValue(bgColor, 'bg', 'bgHover')
+  }, [ textColor, bgColor ])
+
+  setTimeout(() => {
+    setIsRendering(false)
+  }, 200)
+
+  let styleOverride = {
+    transition: isRendering ? 'none' : null,
+    color: customColors.color,
+    backgroundColor: customColors.bg
+  }
+
+  const setHover = (e) => {
+    e.target.style.color = customColors.colorHover
+    e.target.style.backgroundColor= customColors.bgHover
+  }
+
+  const setHoverOut = (e) => {
+    e.target.style.transition = isRendering ? 'none' : null
+    e.target.style.color = customColors.color
+    e.target.style.backgroundColor = customColors.bg
   }
 
   return (
-    <button {...attributesObj}>{children}</button>
+    <button {...attributesObj}
+            style={{...attributesObj.style, ...styleOverride}}
+            onMouseOver={setHover}
+            onMouseOut={setHoverOut}>
+      {children}
+    </button>
   )
 }
 
