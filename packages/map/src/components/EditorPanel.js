b7b6862c61588c734b646a65a5ebdfcda5be41a4
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-05-06 18:06:53 -0400

    Map> add state as a second param to the hook

--- packages/map/src/components/EditorPanel.js
+++ packages/map/src/components/EditorPanel.js
@@ -109,7 +109,7 @@ const EditorPanel = (props) => {
 
 	const [activeFilterValueForDescription, setActiveFilterValueForDescription] = useState([0, 0]);
 
-	const {handleSwitch,dispatch,filteredPallets,filteredQualitative,isPaletteReversed,paletteName} = useColorPalette(colorPalettes);
+	const {handleSwitch,dispatch,filteredPallets,filteredQualitative,isPaletteReversed,paletteName} = useColorPalette(colorPalettes,state);
 
 
 	const [editorCatOrder, setEditorCatOrder] = useState(state.legend.categoryValuesOrder || []);
