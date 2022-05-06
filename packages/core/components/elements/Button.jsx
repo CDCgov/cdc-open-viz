401b673c705fdeb07288f2bc04113ead0443ff4a
Rob Shelnutt <rob@blackairplane.com>
2022-05-06 14:19:33 -0400

    Update button component and styles

--- packages/core/components/elements/Button.jsx
+++ packages/core/components/elements/Button.jsx
@@ -1,81 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
 import '../../styles/v2/components/button.scss'
 
-const Button = ({ fullwidth, textColor = null, bgColor = null, children, ...attributes }) => {
-  const [ isRendering, setIsRendering ] = useState(true)
-  const [ customColors, setCustomColors ] = useState({
-    color: null,
-    colorHover: null,
-    bg: null,
-    bgHover: null
-  })
+const Button = ({ fluid = false, hoverStyle = {}, children, ...attributes }) => {
+  const [ customStyles, setCustomStyles ] = useState({...attributes.style})
 
   let attributesObj = {
     ...attributes,
-    className: 'cove-button' + (fullwidth ? ' fluid' : '') + (attributes.className ? ' ' + attributes.className : '')
+    className: 'cove-button' + (fluid ? ' fluid' : '') + (attributes.className ? ' ' + attributes.className : '')
   }
 
-  const registerColorValue = (prop, keyBase, keyHover) => {
-    // Set up payload to modify state color object
-    let payload = {}
-
-    // Validate that the entry has content, else return null
-    const validate = (obj) => {
-      return obj.length > 0 ? obj : null
-    }
-
-    // Check provided prop to see if string or array, and parse colors accordingly
-    if (prop) {
-      if (typeof prop === 'string') {
-        payload[[ keyBase ]] = validate(prop)
-      } else if (Array.isArray(prop)) {
-        if (prop.length === 1) {
-          payload[[ keyBase ]] = validate(prop[0])
-        } else if (prop.length === 2) {
-          payload[[ keyBase ]] = validate(prop[0])
-          payload[[ keyHover ]] = validate(prop[1])
-        }
-      }
-    }
-
-    setCustomColors(colors => ({ ...colors, ...payload }))
-  }
-
-  useEffect(() => {
-    registerColorValue(textColor, 'color', 'colorHover')
-    registerColorValue(bgColor, 'bg', 'bgHover')
-  }, [ textColor, bgColor ])
-
-  setTimeout(() => {
-    setIsRendering(false)
-  }, 200)
-
-  let styleOverride = {
-    transition: isRendering ? 'none' : null,
-    color: customColors.color,
-    backgroundColor: customColors.bg
-  }
-
-  const setHover = (e) => {
-    e.target.style.color = customColors.colorHover
-    e.target.style.backgroundColor= customColors.bgHover
-  }
-
-  const setHoverOut = (e) => {
-    e.target.style.transition = isRendering ? 'none' : null
-    e.target.style.color = customColors.color
-    e.target.style.backgroundColor = customColors.bg
+  const setStyles = (direction) => {
+    return direction === 'in'
+      ? setCustomStyles(style => ({...style, ...hoverStyle}))
+      : direction === 'out'
+        ? setCustomStyles({ ...attributes.style })
+        : false
   }
 
   return (
     <button {...attributesObj}
-            style={{...attributesObj.style, ...styleOverride}}
-            onMouseOver={setHover}
-            onMouseOut={setHoverOut}>
+            style={customStyles}
+            onMouseOver={()=>setStyles('in')}
+            onMouseOut={()=>setStyles('out')}>
       {children}
     </button>
   )
+}
+
+Button.propTypes = {
+  /** Object containing styles to overwrite base styles on hover */
+  hoverStyle: PropTypes.object,
+  /** Enables button to stretch to the full width of the content */
+  fluid: PropTypes.bool
 }
 
 export default Button
