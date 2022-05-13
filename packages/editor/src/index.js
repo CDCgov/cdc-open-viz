9558365313718f056df7f8056569e730a1f30ce3
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2022-05-13 13:57:54 -0700

    Must Share > add info box

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -11,7 +11,7 @@ const domContainer = document.querySelector('.react-container')
 
 ReactDOM.render(
   <React.StrictMode>
-    <CdcEditor startingTab={activeTab} containerEl={domContainer} sharepath="/my/share/path"/>
+    <CdcEditor startingTab={activeTab} containerEl={domContainer} />
   </React.StrictMode>,
   document.querySelector('.react-container')
 );
