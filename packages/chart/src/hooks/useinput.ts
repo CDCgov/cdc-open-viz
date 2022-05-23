275690affa7ce7e03e9c18db0b939adcb30b312c
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-05-23 14:21:59 -0400

    Chart> update extension to ts

--- packages/chart/src/hooks/useinput.ts
+++ packages/chart/src/hooks/useinput.ts
@@ -117,7 +117,7 @@ export default useInput;
 const getMaxValueFromData = (config,data)=>{
     let max;  // will hold max number from data.
 
-      if (config.visualizationType === 'Bar' && config.visualizationSubType === 'stacked') {
+    if (config.visualizationType === 'Bar' && config.visualizationSubType === 'stacked') {
         const yTotals = data.reduce((allTotals, xValue) => {
           const totalYValues = config.runtime.seriesKeys.reduce((yTotal, k) => {
             yTotal += Number(xValue[k]);
@@ -126,15 +126,15 @@ const getMaxValueFromData = (config,data)=>{
           allTotals.push(totalYValues);
           if(totalYValues > max){
             max = totalYValues;
-          };
+          }
           return allTotals;
-        });
+        }, [] as number[]);
 
-        max = String( Math.max(...yTotals));
+        max = Math.max(...yTotals);
       } else if(config.visualizationType === 'Bar' && config.confidenceKeys && config.confidenceKeys.upper) {
-        max = String(Math.max(...data.map((d) => Number(d[config.confidenceKeys.upper]))));
+        max = Math.max(...data.map((d) => Number(d[config.confidenceKeys.upper])));
       } else {
-        max = String(Math.max(...data.map((d) => Math.max(...config.runtime.seriesKeys.map((key) => Number(d[key]))))));
+        max = Math.max(...data.map((d) => Math.max(...config.runtime.seriesKeys.map((key) => Number(d[key])))));
       }
 
       return max;
