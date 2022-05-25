2ad7260e927271a0f684dda0986ebcc196837440
Adam Doe <adamnickolasdoe@gmail.com>
2022-05-25 10:25:22 -0700

    Horizontal Stack > Fix Bar Chart Height

--- packages/chart/src/components/BarChart.tsx
+++ packages/chart/src/components/BarChart.tsx
@@ -150,20 +150,22 @@ export default function BarChart({ xScale, yScale, seriesScale, xMax, yMax, getX
                       }
                     }
 
-                    config.height = ( Number(config.barPadding) + barHeight) * data.length;
+                    config.height = (barsPerGroup * Number(barHeight) ) * data.length + (config.barPadding * data.length);
 
                     let labelColor = "#000000";
 
                     if (chroma.contrast(labelColor, bar.color) < 4.9) {
                       labelColor = '#FFFFFF';
                     }
-                    
+
+                    let offset = yMax / data.length * (config.barHeight) / 2;
+
                     return (
                       <Group key={index}>
                         <rect
                           key={`barstack-horizontal-${barStack.index}-${bar.index}-${index}`}
                           x={bar.x}
-                          y={bar.y - config.barHeight - 3}
+                          y={ bar.y - config.barHeight }
                           width={bar.width}
                           height={config.barHeight}
                           fill={bar.color}
@@ -178,10 +180,10 @@ export default function BarChart({ xScale, yScale, seriesScale, xMax, yMax, getX
                       {(orientation === 'horizontal' && visualizationSubType === 'stacked') && isLabelBelowBar && barStack.index === 0 && !config.yAxis.hideLabel &&
                           <Text
                             x={ `${bar.x + (config.isLollipopChart ? 15 : 5)}` } // padding
-                            y={ bar.y + 8 }
+                            y={ bar.y + 5}
                             fill={ '#000000' }
                             textAnchor="start"
-                            verticalAnchor="middle"
+                            verticalAnchor="start"
                           >
                            {data[bar.index][config.runtime.originalXAxis.dataKey]}
                           </Text>
@@ -190,10 +192,10 @@ export default function BarChart({ xScale, yScale, seriesScale, xMax, yMax, getX
                       { displayNumbersOnBar && textWidth + 50 < bar.width &&
                           <Text
                             x={ bar.x + barStack.bars[bar.index].width / 2 } // padding
-                            y={ bar.y - config.barHeight/2 + 3 }
+                            y={ bar.y - config.barHeight/2 }
                             fill={ labelColor }
                             textAnchor="middle"
-                            verticalAnchor="end"
+                            verticalAnchor="middle"
                             innerRef={
                               (e) => {
                                 if(e) {
