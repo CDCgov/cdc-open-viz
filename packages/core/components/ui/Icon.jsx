4b3794252b1beb87585c321d18b57772e62bb19e
Rob Shelnutt <rob@blackairplane.com>
2022-05-26 17:27:25 -0400

    Resolve style issues with markup include and waffle charts

--- packages/core/components/ui/Icon.jsx
+++ packages/core/components/ui/Icon.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import iconClose from '../../assets/icon-close.svg'
 import iconQuestion from '../../assets/question-circle.svg'
@@ -10,38 +11,53 @@ import iconInfo from '../../assets/icon-info.svg'
 
 import '../../styles/v2/components/icon.scss'
 
-const Icon = ({display = null, alt = '', size, color, ...attributes}) => {
+const iconHash = {
+  "close": iconClose,
+  "question": iconQuestion,
+  "link": iconLink,
+  "upload": iconUpload,
+  "fileUpload": iconFileUpload,
+  "warning": iconWarning,
+  "info": iconInfo
+}
 
-  const iconList = {
-    "close": iconClose,
-    "question": iconQuestion,
-    "link": iconLink,
-    "upload": iconUpload,
-    "fileUpload": iconFileUpload,
-    "warning": iconWarning,
-    "info": iconInfo
-  }
-
-  let IconObj = null
-
-  if (display) {
-    IconObj = iconList[display]
-  }
+const Icon = ({ display = null, base, alt = '', size, color, style, ...attributes }) => {
+  const IconObj = iconHash[display] || null
 
   const filteredAttrs = { ...attributes }
   delete filteredAttrs.className
 
   const styles = {
-    ...attributes.style,
+    ...style,
     color: color ? color : null,
     width: size ? size + 'px' : null
   }
 
   return (
-    <span className={`cove-icon${attributes.className ? ' ' + attributes.className : ''}`} style={styles} {...filteredAttrs}>
-      <IconObj title={alt} />
-    </span>
+    <>
+      {base
+        ? <IconObj title={alt}/>
+        : (
+          <span className={`cove-icon${attributes.className ? ' ' + attributes.className : ''}`} style={styles} {...filteredAttrs}>
+            <IconObj title={alt}/>
+          </span>
+        )
+      }
+    </>
   )
+}
+
+Icon.propTypes = {
+  /* Define the icon to display */
+  display: PropTypes.oneOf(Object.keys(iconHash)),
+  /* Returns icon data as plain svg */
+  base: PropTypes.bool,
+  /* Sets alt text for the icon */
+  alt: PropTypes.string,
+  /* Override the width of the icon (scales height proportionally)*/
+  size: PropTypes.number,
+  /* Override the color of the icon */
+  color: PropTypes.string
 }
 
 export default Icon
