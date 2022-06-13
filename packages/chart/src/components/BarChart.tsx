7c811380bcb8c14621103b3f2b32e570c44fcc86
Murad CDC <atamurad.babakulyyev@gmail.com>
2022-06-12 20:45:02 -0400

    Chart > update config when selecting Lollipop option

--- packages/chart/src/components/BarChart.tsx
+++ packages/chart/src/components/BarChart.tsx
@@ -68,6 +68,15 @@ export default function BarChart({ xScale, yScale, seriesScale, xMax, yMax, getX
       })
     }
   }, []);
+  
+  useEffect(()=>{
+    if(config.barStyle==='lollipop'){
+      updateConfig({ ...config, isLollipopChart:true })
+    }
+    if(config.barStyle==='rounded' || config.barStyle==='flat'){
+      updateConfig({ ...config, isLollipopChart:false })
+    }
+  },[config.barStyle])
 
   return (
     <ErrorBoundary component="BarChart">
