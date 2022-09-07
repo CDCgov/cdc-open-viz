20a855c3809e0f821803f3247d978142dd71da47
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2022-09-07 13:21:51 -0700

    Remove testing data

--- packages/chart/src/index.tsx
+++ packages/chart/src/index.tsx
@@ -8,37 +8,6 @@ const domContainers = document.querySelectorAll('.react-container');
 
 let isEditor = window.location.href.includes('editor=true');
 
-document.addEventListener('cove_loaded', (e) => {
-  console.log('cove_loaded', e)
-})
-
-document.addEventListener('cove_updateConfig', (e) => {
-  console.log('cove_updateConfig', e)
-})
-
-let select = (document.getElementById("cove_select")) as HTMLSelectElement;
-
-select.addEventListener('change', function(e) {
-
-  const getValues = () => {
-    var values = Array.from(select.options).map(e => e.value);
-    let externalFilters = {
-      columnName: 'Race',
-      active: select.value,
-      values: values,
-    }
-
-    if(externalFilters.active === "") {
-      console.log('pushing nada')
-      publish('cove_filterData', [])
-    } else {
-      publish('cove_filterData', externalFilters)
-    }
-  }
-
-  getValues()
-});
-
 domContainers.forEach((domContainer) => {	
   render(	
     <React.StrictMode>
