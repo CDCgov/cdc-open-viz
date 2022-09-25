9b7269fb5d82ddf03d3807635876c5e067c31bb0
Adam Doe <adamnickolasdoe@gmail.com>
2022-09-25 10:24:34 -0700

    Rename county topojson file

--- packages/map/src/components/SingleStateMap.js
+++ packages/map/src/components/SingleStateMap.js
@@ -7,7 +7,7 @@ import { feature, mesh } from "topojson-client";
 import { CustomProjection } from '@visx/geo';
 import colorPalettes from '../../../core/data/colorPalettes';
 import { geoAlbersUsaTerritories } from 'd3-composite-projections';
-import testJSON from '../data/dfc-map.json';
+import testJSON from '../data/county-map.json';
 
 const abbrs = {
   Alabama: 'AL',
