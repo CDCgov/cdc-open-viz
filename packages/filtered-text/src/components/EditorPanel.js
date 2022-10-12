20f780347f7b46ddce1a9c253470bade9c0eada5
Adam Doe <adamnickolasdoe@gmail.com>
2022-10-12 13:55:23 -0700

    [DEV-898] Use Grouping Styles in Editor Panel for style checkboxes

--- packages/filtered-text/src/components/EditorPanel.js
+++ packages/filtered-text/src/components/EditorPanel.js
@@ -295,7 +295,7 @@ const EditorPanel = memo((props) => {
 			  </ul>
 			</label>
 			
-			<div className="cove-accordion__panel-section">
+			<div className="cove-accordion__panel-section checkbox-group">
 			  <InputCheckbox inline size='small' value={config.visual.border} section="visual" fieldName="border" label="Display Border" updateField={updateField} />
 			  <InputCheckbox inline size='small' value={config.visual.borderColorTheme} section="visual" fieldName="borderColorTheme" label="Use theme border color" updateField={updateField} />
 			  <InputCheckbox size='small' value={config.visual.accent} section="visual" fieldName="accent" label="Use Accent Style" updateField={updateField} />
