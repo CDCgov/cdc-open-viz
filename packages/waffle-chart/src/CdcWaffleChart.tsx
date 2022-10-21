5702f0be1274f0cd7b99c77abc5e76eda09f59e9
mpallansch <matthew.pallansch@gmail.com>
2022-10-21 11:43:53 -0400

    Fixes Waffle Chart Dashboard Filters

--- packages/waffle-chart/src/CdcWaffleChart.tsx
+++ packages/waffle-chart/src/CdcWaffleChart.tsx
@@ -441,6 +441,13 @@ const CdcWaffleChart = (
     loadConfig().catch((err) => console.log(err))
   }, [])
 
+  //Reload config if parent passes different config
+  useEffect(() => {
+    if(!configObj.dataUrl){
+      updateConfig({ ...defaults, ...configObj});
+    }
+  }, [configObj.data])
+
   let content = (<Loading/>)
 
   if (loading === false) {
