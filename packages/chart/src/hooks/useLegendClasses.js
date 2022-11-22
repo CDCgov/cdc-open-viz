5c8ec98b08f753b6c4614da249a753dd32ce193e
Adam Doe <adamnickolasdoe@gmail.com>
2022-11-22 14:32:29 -0800

    [DEV-786a] Format: Charts

--- packages/chart/src/hooks/useLegendClasses.js
+++ packages/chart/src/hooks/useLegendClasses.js
@@ -1,20 +1,20 @@
 export default function useLegendClasses(config) {
-	let containerClasses = ['legend-container']
-	let innerClasses = ['legend-container__inner'];
+  let containerClasses = ['legend-container']
+  let innerClasses = ['legend-container__inner']
 
-	// Legend Positioning
-	if (config.legend.position === "left") {
-		containerClasses.push('left')
-	}
+  // Legend Positioning
+  if (config.legend.position === 'left') {
+    containerClasses.push('left')
+  }
 
-	// Legend > Item Ordering
-	if (config.legend.reverseLabelOrder) {
-		innerClasses.push('d-flex')
-		innerClasses.push('flex-column-reverse')
-	}
+  // Legend > Item Ordering
+  if (config.legend.reverseLabelOrder) {
+    innerClasses.push('d-flex')
+    innerClasses.push('flex-column-reverse')
+  }
 
-	return {
-		containerClasses,
-		innerClasses
-	};
+  return {
+    containerClasses,
+    innerClasses
+  }
 }
