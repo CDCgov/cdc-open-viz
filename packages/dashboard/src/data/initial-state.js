04462ab9fca5fa75d42db4cb878f5e02d23f0dac
Adam Doe <adamnickolasdoe@gmail.com>
2022-11-23 08:36:48 -0800

    [DEV-786a] Format: Dashboard

--- packages/dashboard/src/data/initial-state.js
+++ packages/dashboard/src/data/initial-state.js
@@ -1,14 +1,8 @@
 export default {
   dashboard: {
-      theme: 'theme-blue'
+    theme: 'theme-blue'
   },
-  rows: [
-    [
-      {width: 12},
-      {},
-      {}
-    ]
-  ],
+  rows: [[{ width: 12 }, {}, {}]],
   visualizations: {},
   table: {
     label: 'Data Table',
