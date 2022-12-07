f8ace3413236c287cc5d4c116289bd4b87174824
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2022-12-06 17:30:59 -0800

    [DEV-35a] Maps: Filtering is working now

--- packages/map/src/components/Filters.js
+++ packages/map/src/components/Filters.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState, useContext } from 'react'
+import React, {useState, useContext } from 'react'
 import Context from './../context'
 import Button from '@cdc/core/components/elements/Button'
 
+// TODO: Combine Charts/Maps Filters.js files
 const useFilters = () => {
-  const { config, setConfig, filteredData, setFilteredData, excludedData, filterData } = useContext(Context)
+  const { state: config, setState: setConfig, runtimeFilters, setRuntimeFilters } = useContext(Context)
   const [showApplyButton, setShowApplyButton] = useState(false)
 
   const sortAsc = (a, b) => {
@@ -17,89 +18,74 @@ const useFilters = () => {
   const announceChange = text => {}
 
   const changeFilterActive = (index, value) => {
-    let newFilters =  config.filters
+    let newFilters =  runtimeFilters
     newFilters[index].active = value
-    setConfig({
-      ...config,
-      filters: newFilters
-    })
+    setRuntimeFilters(newFilters)
     setShowApplyButton(true)
   }
 
   const handleApplyButton = (newFilters) => {
     setConfig({ ...config, filters: newFilters })
-    setFilteredData(filterData(newFilters, excludedData))
     setShowApplyButton(false)
   }
 
   const handleReset = () => {
-    let newFilters = config.filters
+    let newFilters = runtimeFilters
 
     // reset to first item in values array.
     newFilters.map( filter => {
       filter.active = filter.values[0]
     })
 
-    setFilteredData(filterData(newFilters, excludedData))
     setConfig({...config, filters: newFilters })
   }
 
-  return { handleApplyButton, changeFilterActive, announceChange, sortAsc, sortDesc, showApplyButton, handleReset }
+  return { announceChange, sortAsc, sortDesc, changeFilterActive, showApplyButton, handleReset, handleApplyButton  }
 }
 
-const Filters = () => {
+export const Filters = () => {
 
-  const { config } = useContext(Context)
+  const { runtimeFilters,  state: config } = useContext(Context)
   const { handleApplyButton, changeFilterActive, announceChange, sortAsc, sortDesc, showApplyButton, handleReset } = useFilters()
-  const { filters  } = config
+
   const buttonText = 'Apply Filters'
   const resetText = 'Reset All'
 
-  // A List of Dropdowns
+  const { filters  } = config
+
+
   const FilterList = () => {
 
-    if (config.filters) {
-      return config.filters.map((singleFilter, index) => {
-
+    if (runtimeFilters) {
+      return runtimeFilters.map((singleFilter, idx) => {
         const values = []
 
+        if (undefined === singleFilter.active) return null
 
-        if (!singleFilter.order || singleFilter.order === '') {
-          singleFilter.order = 'asc'
-        }
-
-        if (singleFilter.order === 'desc') {
-          singleFilter.values = singleFilter.values.sort(sortDesc)
-        }
-
-        if (singleFilter.order === 'asc') {
-          singleFilter.values = singleFilter.values.sort(sortAsc)
-        }
-
-        singleFilter.values.forEach((filterOption, index) => {
+        singleFilter.values.forEach((filterOption, idx) => {
           values.push(
-            <option key={index} value={filterOption}>
+            <option key={idx} value={filterOption}>
               {filterOption}
             </option>
           )
         })
 
         return (
-          <div className='single-filter' key={index}>
-            <label htmlFor={`filter-${index}`}>{singleFilter.label}</label>
+          <section className='filter-col' key={idx}>
+            {singleFilter.label.length > 0 && <label htmlFor={`filter-${idx}`}>{singleFilter.label}</label>}
             <select
-              id={`filter-${index}`}
+              id={`filter-${idx}`}
               className='filter-select'
-              data-index='0'
+              aria-label='select filter'
               value={singleFilter.active}
-              onChange={e => {
-                changeFilterActive(index, e.target.value)
+              onChange={ e => {
+                changeFilterActive(idx, e.target.value)
                 announceChange(`Filter ${singleFilter.label} value has been changed to ${e.target.value}, please reference the data table to see updated values.`)
               }}
             >
               {values}
             </select>
-          </div>
+          </section>
         )
       })
     } else {
@@ -110,20 +96,14 @@ const Filters = () => {
 
   return (
     <section className={`filters-section`} style={{ display: 'block', width: '100%' }}>
-      {config.filters.length > 0 &&
-      <>
-        <h3 className="filters-section__title">Filters</h3>
-        <hr />
-      </>
-      }
+      <h3 className="filters-section__title">Filters</h3>
+      <hr />
       <div className="filters-section__wrapper" style={{ flexWrap: 'wrap', display: 'flex', gap: '7px 15px'}}>
-        <FilterList />
-        {config.filters.length > 0 &&
+          <FilterList />
           <div className="filter-section__buttons" style={{ width: '100%' }}>
-            <Button onClick={ () => handleApplyButton(filters) } disabled={!showApplyButton} style={{ marginRight: '10px' }}>{buttonText}</Button>
+          <Button onClick={ () => handleApplyButton(filters) } disabled={!showApplyButton} style={{ marginRight: '10px' }}>{buttonText}</Button>
             <a href="#!" role="button" onClick={handleReset}>{resetText}</a>
           </div>
-        }
       </div>
     </section>
   )
