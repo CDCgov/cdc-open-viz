cd52ae6f5e3b04534ce4b5112103a722e7331cfe
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-01-03 16:32:41 -0800

    [box-plots] Box Plot: Add tooltips

--- packages/chart/src/components/BoxPlot.js
+++ packages/chart/src/components/BoxPlot.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { BoxPlot } from '@visx/stats'
 import { Group } from '@visx/group'
 import { scaleBand, scaleLinear } from '@visx/scale'
 import Context from '../context'
 import ErrorBoundary from '@cdc/core/components/ErrorBoundary'
 import { colorPalettesChart } from '@cdc/core/data/colorPalettes'
+import ReactTooltip from 'react-tooltip'
 
 const CoveBoxPlot = ({ xScale, yScale }) => {
   const { transformedData: data, config } = useContext(Context)
@@ -18,9 +19,20 @@ const CoveBoxPlot = ({ xScale, yScale }) => {
   const yMax = config.height - config.runtime.xAxis.size
   const xMax = config.width - config.runtime.yAxis.size - config.yAxis.rightAxisSize
   const boxWidth = xScale.bandwidth()
-  const constrainedWidth = Math.min(40, boxWidth)
+  const constrainedWidth = Math.min(20, boxWidth)
   const color_0 = colorPalettesChart[config?.palette][0] ? colorPalettesChart[config?.palette][0] : '#000'
 
+  // tooltips
+  const tooltip_id = `cdc-open-viz-tooltip-${config.runtime.uniqueId}`
+  const handleTooltip = d => {
+    return `
+      <strong>${d.columnCategory}</strong></br>
+      Q1: ${d.columnFirstQuartile}<br/>
+      Q3: ${d.columnThirdQuartile}<br/>
+      IQR: ${d.columnIqr}<br/>
+      Median: ${d.columnMedian}
+    `
+  }
   return (
     <ErrorBoundary component='BoxPlot'>
       <Group className='boxplot' key='boxplot-wrapper'>
@@ -31,7 +43,7 @@ const CoveBoxPlot = ({ xScale, yScale }) => {
               key={`box-plot-${i}`}
               min={d.columnMin}
               max={d.columnMax}
-              left={xScale(d.columnCategory) + config.yAxis.size + offset / 2}
+              left={xScale(d.columnCategory) + config.yAxis.size + offset / 2 + 0.5}
               firstQuartile={d.columnFirstQuartile}
               thirdQuartile={d.columnThirdQuartile}
               median={d.columnMedian}
@@ -56,12 +68,18 @@ const CoveBoxPlot = ({ xScale, yScale }) => {
               boxProps={{
                 style: {
                   stroke: 'black'
-                }
+                },
+                'data-tip': 'cool'
               }}
               maxProps={{
                 style: {
                   stroke: 'black'
                 }
+              }}
+              container
+              containerProps={{
+                'data-tip': handleTooltip(d),
+                'data-for': tooltip_id
               }}
             />
           )
