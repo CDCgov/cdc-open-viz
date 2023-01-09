ca9ac5ce8239bf9483d22daca588b768220f4283
Adam Doe <adamnickolasdoe@gmail.com>
2023-01-09 14:04:08 -0800

    [area-chart] areas working

--- packages/chart/src/components/AreaChart.js
+++ packages/chart/src/components/AreaChart.js
@@ -3,7 +3,7 @@ import { Group } from '@visx/group'
 import Context from '../context'
 import ErrorBoundary from '@cdc/core/components/ErrorBoundary'
 import { colorPalettesChart } from '@cdc/core/data/colorPalettes'
-import { AreaClosed } from '@visx/shape'
+import { AreaClosed, LinePath } from '@visx/shape'
 import { curveMonotoneX } from '@visx/curve'
 
 const CoveAreaChart = ({ xScale, yScale }) => {
@@ -15,19 +15,34 @@ const CoveAreaChart = ({ xScale, yScale }) => {
         <Group className='area-chart' key='boxplot-wrapper' left={config.yAxis.size}>
           {config.series.map((s, index) => {
             let seriesColor = colorPalettesChart[config.palette][index]
+            let curveType = curveMonotoneX
             return (
-              // prettier-ignore
-              <AreaClosed
-                curve={curveMonotoneX}
-                key={'area-chart'}
-                fill={seriesColor}
-                data={data}
-                x={d => xScale(d[config.xAxis.dataKey])}
-                y={d => yScale(d[config.series[index].dataKey])}
-                yScale={yScale}
-                data-tip={handleTooltip}
-                data-for={`cdc-open-viz-tooltip-${config.runtime.uniqueId}`}
-              />
+              <>
+                {/* prettier-ignore */}
+                <LinePath
+                  data={data}
+                  x={d => xScale(d[config.xAxis.dataKey])}
+                  y={d => yScale(d[config.series[index].dataKey])}
+                  stroke={seriesColor}
+                  strokeWidth={2}
+                  strokeOpacity={1}
+                  shapeRendering='geometricPrecision'
+                  curve={curveType}
+                />
+                {/* // prettier-ignore */}
+                <AreaClosed
+                  curve={curveType}
+                  key={'area-chart'}
+                  fill={seriesColor}
+                  fillOpacity={0.5}
+                  data={data}
+                  x={d => xScale(d[config.xAxis.dataKey])}
+                  y={d => yScale(d[config.series[index].dataKey])}
+                  yScale={yScale}
+                  data-tip='testing!'
+                  data-for={`cdc-open-viz-tooltip-${config.runtime.uniqueId}`}
+                />
+              </>
             )
           })}
         </Group>
