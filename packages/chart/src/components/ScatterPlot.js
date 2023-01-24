5a19319d4f7fac80abfa3d0f6464666b04aa6df7
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-01-24 15:01:40 -0800

    [scatter-plot] start work on continuous lines

--- packages/chart/src/components/ScatterPlot.js
+++ packages/chart/src/components/ScatterPlot.js
@@ -9,34 +9,40 @@ const CoveScatterPlot = ({ xScale, yScale, getXAxisData, getYAxisData }) => {
   // should probably be a constant somewhere.
   let circleRadii = 4.5
 
-  // first series key for now
-  let seriesKey = config.runtime.seriesKeys[0]
-
   let pointStyles = {
     filter: 'unset',
     opacity: 1,
-    fill: colorScale ? colorScale(config.runtime.seriesLabels ? config.runtime.seriesLabels[seriesKey] : seriesKey) : '#000',
     stroke: 'black'
   }
 
-  return data.map((item, dataIndex) => {
-    // prettier-ignore
-    return (
-      <Group left={config.runtime.yAxis.size}>
-        <circle
-          key={`${dataIndex}`}
-          r={circleRadii}
-          cx={xScale(item[config.xAxis.dataKey])}
-          cy={yScale(item[config.yAxis.dataKey])}
-          fill={'black'}
-          fillOpacity={1}
-          opacity={1}
-          style={pointStyles}
-          data-for={`cdc-open-viz-tooltip-${config.runtime.uniqueId}`}
-        />
-      </Group>
-    )
-  })
-}
+  const handleTooltip = item => `<div>
+ ${JSON.stringify(item)}<br/>
+ ${config.xAxis.label}: ${item[config.xAxis.dataKey]} <br/>
+ ${config.yAxis.label}: ${item[config.yAxis.dataKey]}
+</div>`
 
+  return (
+    <Group className='scatter-plot' left={config.yAxis.size}>
+      {data.map((item, dataIndex) => {
+        // prettier-ignore
+        return config.runtime.seriesKeys.map( s => {
+            return (
+              <circle
+                key={`${dataIndex}`}
+                r={circleRadii}
+                cx={xScale(item[config.xAxis.dataKey])}
+                cy={yScale(item[config.yAxis.dataKey])}
+                fillOpacity={1}
+                opacity={1}
+                style={pointStyles}
+                fill={colorScale ? colorScale(config.runtime.seriesLabels ? config.runtime.seriesLabels[s] : s) : '#000'}
+                data-tip={handleTooltip(item)}
+                data-for={`cdc-open-viz-tooltip-${config.runtime.uniqueId}`}
+              />
+            )
+          })
+      })}
+    </Group>
+  )
+}
 export default CoveScatterPlot
