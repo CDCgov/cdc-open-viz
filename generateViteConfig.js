555d8cc3135b4e606648d5c487aacf597a9165a5
Rob Shelnutt <rob@blackairplane.com>
2023-01-31 11:59:42 -0500

    Resolve issue with dev server initiating on local IP vs localhost

--- generateViteConfig.js
+++ generateViteConfig.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import svgr from 'vite-plugin-svgr' // Svg Support
 import dsv from '@rollup/plugin-dsv' // CSV Support
+import dns from 'dns' // nodeJS
 
+// Force load dev server on `localhost` vs 127.0.0.1
+dns.setDefaultResultOrder('verbatim')
+
+// DEV NOTE: Modifications made to this file will not be hot-loaded through HMR for component.
+// - Active dev servers ('lerna run start') must be restarted in order to view the changed settings.
 const generateViteConfig = (componentName, configOptions = {}, reactOptions = {}) => {
   let configOptionsDefault = {
     server: { port: 8080 },
