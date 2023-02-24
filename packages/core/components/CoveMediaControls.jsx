30c75edc4cb9e3d900aefd270b181a58f86b78d4
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-02-24 10:56:01 -0800

    [package-updates] security updates

--- packages/core/components/CoveMediaControls.jsx
+++ packages/core/components/CoveMediaControls.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import html2pdf from 'html2pdf.js'
+// import html2pdf from 'html2pdf.js'
 import html2canvas from 'html2canvas'
 
 const buttonText = {
@@ -64,22 +64,23 @@ const generateMedia = (state, type, elementToCapture) => {
       })
       return
     case 'pdf':
-      let opt = {
-        margin: 0.2,
-        filename: filename + '.pdf',
-        image: { type: 'png' },
-        html2canvas: { scale: 2, logging: false },
-        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-      }
+      // let opt = {
+      //   margin: 0.2,
+      //   filename: filename + '.pdf',
+      //   image: { type: 'png' },
+      //   html2canvas: { scale: 2, logging: false },
+      //   jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+      // }
 
-      html2pdf()
-        .set(opt)
-        .from(baseSvg)
-        .save()
-        .then(() => {
-          generatedImage.remove() // Remove generated png
-          baseSvg.style.display = null // Re-display initial svg map
-        })
+      // html2pdf()
+      //   .set(opt)
+      //   .from(baseSvg)
+      //   .save()
+      //   .then(() => {
+      //     generatedImage.remove() // Remove generated png
+      //     baseSvg.style.display = null // Re-display initial svg map
+      //   })
+      console.warn('COVE: pdf downloads disabled')
       break
     default:
       console.warn("generateMedia param 2 type must be 'image' or 'pdf'")
