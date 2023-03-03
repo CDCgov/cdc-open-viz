c516f320ba5c2f8925b22e058d507f130e06f6a7
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-03-03 09:54:14 -0800

    4.23.2: Filter Text Regression

--- packages/filtered-text/src/CdcFilteredText.jsx
+++ packages/filtered-text/src/CdcFilteredText.jsx
@@ -8,10 +8,10 @@ import defaults from './data/initial-state'
 import ConfigContext from './ConfigContext'
 import parse from 'html-react-parser'
 import './scss/main.scss'
-
+import fetchRemoteData from '@cdc/core/helpers/fetchRemoteData'
 import useDataVizClasses from '@cdc/core/helpers/useDataVizClasses'
 
-const CdcFilteredText = ({ configObj, configUrl, isDashboard = false, isEditor = false, setConfig: setParentConfig }) => {
+const CdcFilteredText = ({ config: configObj, configUrl, isDashboard = false, isEditor = false, setConfig: setParentConfig }) => {
 
   const transform = new DataTransform()
   // Default States
@@ -30,6 +30,8 @@ const CdcFilteredText = ({ configObj, configUrl, isDashboard = false, isEditor =
     let response = configObj || (await (await fetch(configUrl)).json())
     // If data is included through a URL, fetch that and store
     let data = response.formattedData || response.data || {}
+
+    console.log('RESPONSE', response)
 
     if (response.dataUrl) {
       const dataString = await fetch(response.dataUrl)
