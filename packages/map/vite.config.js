9a1146d9711fa80f4ab59e2ab2e2fc2e2df0749f
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-03-03 08:38:48 -0800

    [testing] commiting to switch to another issue

--- packages/map/vite.config.js
+++ packages/map/vite.config.js
@@ -1,9 +1,16 @@
 import GenerateViteConfig from '../../generateViteConfig.js'
 import { moduleName } from './package.json'
+import react from '@vitejs/plugin-react'
 
-export default GenerateViteConfig(moduleName,null,{
+export default GenerateViteConfig(moduleName, null, {
   jsxImportSource: '@emotion/react',
   babel: {
-    plugins: [ '@emotion/babel-plugin' ],
+    plugins: ['@emotion/babel-plugin']
   },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js'
+  }
 })
