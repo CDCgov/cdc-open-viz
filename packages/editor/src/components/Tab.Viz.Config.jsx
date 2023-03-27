51ddfacd457359042e9b4b0b7c30ca7189a96b05
mpallansch <matthew.pallansch@gmail.com>
2023-03-27 16:33:12 -0400

    Adds isEditor Prop for All Viz in Editor

--- packages/editor/src/components/Tab.Viz.Config.jsx
+++ packages/editor/src/components/Tab.Viz.Config.jsx
@@ -7,12 +7,12 @@ import ConfigContext from '../ConfigContext'
 import Ghost from '@cdc/core/components/element/Ghost.jsx'
 
 // Lazy Load Components
-const CdcDashboard = lazy(() => import ('@cdc/dashboard'))
-const CdcMap = lazy(() => import ('@cdc/map'))
-const CdcChart = lazy(() => import ('@cdc/chart'))
-const CdcDataBite = lazy(() => import ('@cdc/data-bite'))
-const CdcWaffleChart = lazy(() => import ('@cdc/waffle-chart'))
-const CdcMarkupInclude = lazy(() => import ('@cdc/markup-include'))
+const CdcDashboard = lazy(() => import('@cdc/dashboard'))
+const CdcMap = lazy(() => import('@cdc/map'))
+const CdcChart = lazy(() => import('@cdc/chart'))
+const CdcDataBite = lazy(() => import('@cdc/data-bite'))
+const CdcWaffleChart = lazy(() => import('@cdc/waffle-chart'))
+const CdcMarkupInclude = lazy(() => import('@cdc/markup-include'))
 
 const TabVizConfig = ({ containerEl }) => {
   const { config, setTempConfig, hostname } = useContext(ConfigContext)
@@ -26,21 +26,15 @@ const TabVizConfig = ({ containerEl }) => {
   }
 
   const visualizationList = {
-    'map': <CdcMap {...baseProps} isEditor={true} hostname={hostname} containerEl={containerEl}/>,
-    'chart': <CdcChart {...baseProps} isEditor={true}/>,
-    'dashboard': <CdcDashboard {...baseProps} isEditor={true}/>,
-    'data-bite': <CdcDataBite {...baseProps} />,
-    'waffle-chart': <CdcWaffleChart {...baseProps} />,
-    'markup-include': <CdcMarkupInclude {...baseProps} />
+    map: <CdcMap {...baseProps} isEditor={true} hostname={hostname} containerEl={containerEl} />,
+    chart: <CdcChart {...baseProps} isEditor={true} />,
+    dashboard: <CdcDashboard {...baseProps} isEditor={true} />,
+    'data-bite': <CdcDataBite {...baseProps} isEditor={true} />,
+    'waffle-chart': <CdcWaffleChart {...baseProps} isEditor={true} />,
+    'markup-include': <CdcMarkupInclude {...baseProps} isEditor={true} />
   }
 
-  return type && visualizationList[type]
-    ? (
-      <Suspense fallback={<Ghost display="editor"/>}>
-        {visualizationList[type]}
-      </Suspense>
-    )
-    : <p>No visualization type selected.</p>
+  return type && visualizationList[type] ? <Suspense fallback={<Ghost display='editor' />}>{visualizationList[type]}</Suspense> : <p>No visualization type selected.</p>
 }
 
 export default TabVizConfig
