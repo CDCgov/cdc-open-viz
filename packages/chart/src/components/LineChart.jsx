e06105d69c66ed6973b2f2ec56b4be0e7df64cb1
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-04-03 18:02:49 -0700

    [DEV-3149-stacked-chart-nonnumerics] Review changes

--- packages/chart/src/components/LineChart.jsx
+++ packages/chart/src/components/LineChart.jsx
@@ -101,7 +101,7 @@ export default function LineChart({ xScale, yScale, getXAxisData, getYAxisData, 
                 curve={allCurves.curveLinear}
                 data={cleanedData}
                 x={d => xScale(getXAxisData(d))}
-                y={d => (seriesAxis === 'Right' ? yScale(getYAxisData(d, seriesKey)) : yScale(getYAxisData(d, seriesKey)))}
+                y={d => (seriesAxis === 'Right' ? yScaleRight(getYAxisData(d, seriesKey)) : yScale(getYAxisData(d, seriesKey)))}
                 stroke={
                   colorScale && !config.legend.dynamicLegend
                     ? colorScale(config.runtime.seriesLabels ? config.runtime.seriesLabels[seriesKey] : seriesKey)
