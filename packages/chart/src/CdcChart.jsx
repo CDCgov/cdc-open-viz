8a6201a55dedf9c01d91294cadb848fb90516086
Tim Turner <timturner@Tims-MacBook-Pro.local>
2023-05-17 10:58:56 -0400

    [DEV-930-add-cols-changes] Fixed accidental typos

--- packages/chart/src/CdcChart.jsx
+++ packages/chart/src/CdcChart.jsx
@@ -966,7 +966,7 @@ export default function CdcChart({ configUrl, config: configObj, isEditor = fals
   // used for Additional Column
   const displayDataAsText = (value, columnName) => {
     if (value === null || value === '' || value === undefined) {
-      return ''j
+      return ''
     }
 
     if (typeof value === 'string' && value.length > 0 && config.legend.type === 'equalnumber') {
@@ -988,7 +988,6 @@ export default function CdcChart({ configUrl, config: configObj, isEditor = fals
     }
 
     if (columnObj === undefined) {
-       debugger
       // then use left axis config
       columnObj = config.type === 'chart' ? config.dataFormat : config.primary
       // NOTE: Left Value Axis uses different names
