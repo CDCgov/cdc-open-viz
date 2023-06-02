4f4f366b86b2727587673edb243c67a61160a303
Tim Turner <timturner@Tims-MacBook-Pro.local>
2023-06-02 12:18:27 -0400

    [DEV-4137-fix-csv-by-url] Code that works with rules still has console.logs

--- packages/core/helpers/fetchRemoteData.js
+++ packages/core/helpers/fetchRemoteData.js
@@ -12,13 +12,29 @@ export default async function (url, visualizationType = '') {
 
     if ('csv' === ext) {
       data = await fetch(url.href)
-        .then(response => response.text())
+        .then(response => {
+          //debugger
+          console.log("response type",typeof response)
+          let cleanText = response.text()
+          console.log("#fetch cleanText",cleanText)
+          return cleanText
+        })
         .then(responseText => {
+          // put placeholder for any commas inside headers since we parse cols separated with commas
+          
+          // remove all double quotes
+          responseText = responseText.replace(/["]+/g, '')
+ //debugger
           const parsedCsv = Papa.parse(responseText, {
             header: true,
             dynamicTyping: true,
-            skipEmptyLines: true
+            skipEmptyLines: true,
+            //delimiter: ',', // dont add this, just leave it at "default" which will guess the delimiter and is more flexible
+            //quoteChar: "'", // dont add this or it will break on single quotes or if text has apostrophe in it (TT)
           })
+          console.log("fetchRemote responseText:",responseText)
+          console.log("fetchRemote parsedCsv:",parsedCsv)
+          //debugger
           return parsedCsv.data
         })
     }
