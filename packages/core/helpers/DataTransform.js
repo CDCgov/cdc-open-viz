28c8a8cbb0297be4df9f692d8287376a191c156d
Tim Turner <122304631+tturnerswdev33@users.noreply.github.com>
2023-06-22 16:25:10 -0400

    [DEV-4137-fix-csv-by-url] Fixed issue with Editor charts

--- packages/core/helpers/DataTransform.js
+++ packages/core/helpers/DataTransform.js
@@ -12,11 +12,9 @@ export class DataTransform {
   //Performs standardizations that can be completed automatically without use input
   autoStandardize(data) {
     const errorsFound = [];
-debugger
-    // Empty data
+
     if (0 === data.length) {
       errorsFound.push(this.constants.errorMessageEmptyData);
-      //return undefined;
     }
 
     // Does it have the correct data structure?
