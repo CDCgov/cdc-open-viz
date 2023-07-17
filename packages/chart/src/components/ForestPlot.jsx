0bc93ee10128d3f5c748586c55e97fe06babadca
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-07-17 14:20:00 -0700

    [forest-plot] starting to add additional columns

--- packages/chart/src/components/ForestPlot.jsx
+++ packages/chart/src/components/ForestPlot.jsx
@@ -1,54 +1,52 @@
 import React, { useContext } from 'react'
 import { Group } from '@visx/group'
-import { Line } from '@visx/shape'
+import { Line, Bar } from '@visx/shape'
 import { Text } from '@visx/text'
-import { AxisLeft } from '@visx/axis'
-import { scaleLinear, scaleBand } from '@visx/scale'
-import { Bar } from '@visx/shape'
 import ConfigContext from '../ConfigContext'
+import { colorPalettesChart } from '@cdc/core/data/colorPalettes'
 
 const ForestPlot = props => {
-  const { transformedData: data } = useContext(ConfigContext)
-  const { yMax: height, xMax: width } = props
+  const { rawData: data } = useContext(ConfigContext)
+  const { xScale, yScale, config } = props
 
-  const yScale = scaleBand({
-    domain: data.map(d => d['Author(s) and Year']),
-    range: [0, height],
-    padding: 0.2
-  })
+  // TODO: pull from editor settings
+  const columns = {
+    upper: 'Upper',
+    lower: 'Lower',
+    estimate: 'Estimate',
+    series: 'Author(s) and Year'
+  }
 
-  const xScale = scaleLinear({
-    domain: [Math.min(...data.map(d => parseFloat(d.Lower))), Math.max(...data.map(d => parseFloat(d.Upper)))],
-    range: [0, width]
-  })
+  const fontSize = 12
+
+  console.log(colorPalettesChart[config.palette])
 
   return (
-    <svg width={width} height={height}>
-      <AxisLeft
-        scale={yScale}
-        tickLabelProps={() => ({
-          dy: '0.33em'
-        })}
-      />
-      <Group>
-        {data.map(d => {
-          const barWidth = Math.abs(xScale(parseFloat(d.Lower)) - xScale(parseFloat(d.Upper)))
-          const barX = xScale(Math.min(parseFloat(d.Lower), parseFloat(d.Upper)))
+    <Group>
+      {data.map(d => {
+        const barWidth = Math.abs(xScale(Number(d[columns.lower])) - xScale(Number(d[columns.upper])))
+        const barX = xScale(Math.min(Number(d[columns.lower]), Number(d[columns.upper])))
 
-          return (
-            <Group key={d['Author(s) and Year']} top={yScale(d['Author(s) and Year'])} left={0}>
-              <Line from={{ x: xScale(parseFloat(d.Estimate)), y: yScale.bandwidth() / 2 }} to={{ x: xScale(parseFloat(d.Estimate)), y: yScale.bandwidth() / 2 }} stroke='black' />
-              <Line from={{ x: xScale(parseFloat(d.Lower)), y: yScale.bandwidth() / 4 }} to={{ x: xScale(parseFloat(d.Upper)), y: yScale.bandwidth() / 4 }} stroke='black' />
-              <Line from={{ x: xScale(parseFloat(d.Lower)), y: yScale.bandwidth() * 0.75 }} to={{ x: xScale(parseFloat(d.Upper)), y: yScale.bandwidth() * 0.75 }} stroke='black' />
-              <Bar x={barX} y={yScale.bandwidth() / 4} width={barWidth} height={yScale.bandwidth() / 2} fill='steelblue' opacity={0.5} />
-              <Text x={xScale(parseFloat(d.Estimate)) + 5} y={yScale.bandwidth() / 2} verticalAnchor='middle' fontSize={12}>
-                {d.Estimate}
-              </Text>
-            </Group>
-          )
-        })}
-      </Group>
-    </svg>
+        return (
+          <Group key={`${d[columns.series]}`} top={yScale(d[columns.series])} left={config.yAxis.size}>
+            <Line from={{ x: xScale(Number(d[columns.estimate])), y: yScale.bandwidth() / 2 }} to={{ x: xScale(Number(d.Estimate)), y: yScale.bandwidth() / 2 }} stroke='black' />
+            <Line from={{ x: xScale(Number(d[columns.lower])), y: yScale.bandwidth() / 4 }} to={{ x: xScale(Number(d[columns.upper])), y: yScale.bandwidth() / 4 }} stroke='black' />
+            <Line from={{ x: xScale(Number(d[columns.lower])), y: yScale.bandwidth() * 0.75 }} to={{ x: xScale(Number(d[columns.upper])), y: yScale.bandwidth() * 0.75 }} stroke='black' />
+            <Bar x={barX} y={yScale.bandwidth() / 4} width={barWidth} height={yScale.bandwidth() / 2} fill={colorPalettesChart[config.palette][0]} opacity={0.5} />
+            <Text x={xScale(Number(d[columns.estimate])) + 5} y={yScale.bandwidth() / 2} verticalAnchor='middle' fontSize={fontSize}>
+              {d[columns.estimate]}
+            </Text>
+            <Text x={config.yAxis.size - 10} y={yScale.bandwidth() / 2} verticalAnchor='middle' fontSize={fontSize} textAnchor='end'>
+              {d['Timing']}
+            </Text>
+
+            <Text x={config.yAxis.size - 30} y={yScale.bandwidth() / 2} verticalAnchor='middle' fontSize={fontSize} textAnchor='end'>
+              {d['Confidence']}
+            </Text>
+          </Group>
+        )
+      })}
+    </Group>
   )
 }
 
