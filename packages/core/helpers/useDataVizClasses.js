f430dfd32bb0e70afa797e8c2fad68b92e30170b
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-08-31 12:17:12 -0700

    [DEV-4139] committing to test errors

--- packages/core/helpers/useDataVizClasses.js
+++ packages/core/helpers/useDataVizClasses.js
@@ -15,7 +15,12 @@ export default function useDataVizClasses(config) {
   let innerContainerClasses = ['cove-component__inner']
   let contentClasses = ['cove-component__content']
 
-  config.title && innerContainerClasses.push('component--has-title')
+  const { visualizationType, title, showTitle } = config
+
+  if (visualizationType === 'Spark Line' || visualizationType === 'chart') {
+    if (title && showTitle) contentClasses.push('component--has-title')
+  }
+  config.title && config.visualizationType !== 'chart' && config.visualizationType !== 'Spark Line' && contentClasses.push('component--has-title')
   config.subtext && innerContainerClasses.push('component--has-subtext')
   config.biteStyle && innerContainerClasses.push(`bite__style--${config.biteStyle}`)
   config.general?.isCompactStyle && innerContainerClasses.push(`component--isCompactStyle`)
