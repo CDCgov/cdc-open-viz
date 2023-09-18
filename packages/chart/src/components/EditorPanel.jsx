99815ae925af60e34343fb1fb77db8800ba5ee17
Murad CDC <atamurad.babakulyyev@gmail.com>
2023-09-18 13:13:13 -0400

    [DEV-5217a] add lined style legend

--- packages/chart/src/components/EditorPanel.jsx
+++ packages/chart/src/components/EditorPanel.jsx
@@ -2678,6 +2678,8 @@ const EditorPanel = () => {
 
                   {config.visualizationType !== 'Box Plot' && <CheckBox value={config.legend.showLegendValuesTooltip ? true : false} section='legend' fieldName='showLegendValuesTooltip' label='Show Legend Values in Tooltip' updateField={updateField} />}
 
+                  {config.visualizationType === 'Line' && <CheckBox value={config.legend.lineMode} section='legend' fieldName='lineMode' label='Show Lined Style Legend' updateField={updateField} />}
+
                   {config.visualizationType === 'Bar' && config.visualizationSubType === 'regular' && config.runtime.seriesKeys.length === 1 && (
                     <Select value={config.legend.colorCode} section='legend' fieldName='colorCode' label='Color code by category' initial='Select' updateField={updateField} options={getDataValueOptions(data)} />
                   )}
