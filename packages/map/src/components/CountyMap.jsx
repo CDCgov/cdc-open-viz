655f35e21eac0f9d2452640640cfc8ebb2945880
mpallansch <matthew.pallansch@gmail.com>
2023-09-19 11:42:41 -0400

    Fix For Geocode Maps

--- packages/map/src/components/CountyMap.jsx
+++ packages/map/src/components/CountyMap.jsx
@@ -383,7 +383,7 @@ const CountyMap = props => {
       // Centers the projection on the paramter passed
       if (focus.center) {
         topoData.projection.scale(canvas.width * (focus.id === '72' ? 10 : 2.5))
-        let offset = projection(focus.center)
+        let offset = topoData.projection(focus.center)
         topoData.projection.translate([-offset[0] + canvas.width, -offset[1] + canvas.height])
       }
 
