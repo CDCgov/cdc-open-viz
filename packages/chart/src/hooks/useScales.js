c02b5b8de48e49a1e1680420ba1759060b22ef46
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-09-22 08:52:27 -0700

    [respiratory-dashboard] 4.23.9 patching bars with date scaling for now

--- packages/chart/src/hooks/useScales.js
+++ packages/chart/src/hooks/useScales.js
@@ -48,7 +48,7 @@ const useScales = properties => {
   }
 
   // handle Area chart
-  if (config.xAxis.type === 'date') {
+  if (config.xAxis.type === 'date' && config.visualizationType !== 'Bar') {
     xScale = scaleTime({
       domain: [Math.min(...xAxisDataMapped), Math.max(...xAxisDataMapped)],
       range: [0, xMax]
