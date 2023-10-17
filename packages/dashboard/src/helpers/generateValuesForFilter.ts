d7c969979bb41306ca485f919ec476aaf9f90459
Lacey <pro7@cdc.gov>
2023-10-17 13:29:25 -0400

    changed ts config to fix compile errors

--- packages/dashboard/src/helpers/generateValuesForFilter.ts
+++ packages/dashboard/src/helpers/generateValuesForFilter.ts
@@ -1,28 +1,29 @@
-import { FilterBehavior } from "../components/Header"
+import { FilterBehavior } from '../components/Header'
 
-  // Gets filter values from API response
+// Gets filter values from API response
 export const generateValuesForAPIFilter = (columnName, _data): string[] => {
-    type Row = { [key: string]: any }
-    return Object.values(_data)
-      .filter(row => row && !!(row as Row)[columnName])
-      .map(row => (row as Row)[columnName])
-  }
+  type Row = { [key: string]: any }
+  return Object.values(_data)
+    .filter(row => row && !!(row as Row)[columnName])
+    .map(row => (row as Row)[columnName])
+}
 
-  // Gets filter values from dataset
+// Gets filter values from dataset
 export const generateValuesForFilter = (columnName, _data, filterBehavior) => {
-    if (filterBehavior === FilterBehavior.Apply) {
-      return generateValuesForAPIFilter(columnName, _data)
-    }
-    const values: string[] = []
-
-    Object.keys(_data).forEach(key => {
-      _data[key].forEach(row => {
-        const value = row[columnName]
-        if (value && false === values.includes(value)) {
-          values.push(value)
-        }
-      })
-    })
-
-    return values
+  if (filterBehavior === FilterBehavior.Apply) {
+    return generateValuesForAPIFilter(columnName, _data)
   }
+
+  const values: string[] = []
+
+  Object.keys(_data).forEach(key => {
+    _data[key].forEach(row => {
+      const value = row[columnName]
+      if (value && false === values.includes(value)) {
+        values.push(value)
+      }
+    })
+  })
+
+  return values
+}
