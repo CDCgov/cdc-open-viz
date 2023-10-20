7110e4e7a4fbe5dd2bcac6a1a40b7eda70dd5f40
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-10-20 12:48:25 -0700

    [PORT-134] Data Bite TS & useReducer

---
+++ packages/data-bite/src/store/db.actions.ts
@@ -0,0 +1,19 @@
+import type { Config } from '../../types/Config'
+
+type Action<T, P> = { type: T; payload: P }
+
+type SET_CONFIG = Action<'SET_CONFIG', Config>
+type SET_LOADING = Action<'SET_LOADING', boolean>
+type SET_CURRENT_VIEWPORT = Action<'SET_CURRENT_VIEWPORT', string>
+type SET_COVE_LOADED_HAS_RAN = Action<'SET_COVE_LOADED_HAS_RAN', boolean>
+type SET_CONTAINER = Action<'SET_CONTAINER', any>
+
+// prettier-ignore
+type DataBiteActions =
+  | SET_CONFIG
+  | SET_LOADING
+  | SET_CURRENT_VIEWPORT
+  | SET_COVE_LOADED_HAS_RAN
+  | SET_CONTAINER
+
+export default DataBiteActions
---
+++ packages/data-bite/src/store/db.reducer.ts
@@ -0,0 +1,32 @@
+import { Config } from '../../types/Config'
+import DataBiteActions from './db.actions'
+
+export type DataBiteState = {
+  config?: Config
+  loading: boolean
+  currentViewport: string
+  coveLoadedHasRan: boolean
+  container: any
+}
+
+const reducer = (state: DataBiteState, action: DataBiteActions): DataBiteState => {
+  switch (action.type) {
+    case 'SET_CONFIG': {
+      return { ...state, config: action.payload }
+    }
+    case 'SET_LOADING': {
+      return { ...state, loading: action.payload }
+    }
+    case 'SET_CURRENT_VIEWPORT': {
+      return { ...state, currentViewport: action.payload }
+    }
+    case 'SET_COVE_LOADED_HAS_RAN': {
+      return { ...state, coveLoadedHasRan: action.payload }
+    }
+    case 'SET_CONTAINER': {
+      return { ...state, container: action.payload }
+    }
+  }
+}
+
+export default reducer
