c33a32cad4c3e919c352a3f5b81d1fad1e171d73
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2023-10-20 14:10:57 -0700

    [map-ts] continue updating to useReducer

--- packages/map/src/store/map.actions.ts
+++ packages/map/src/store/map.actions.ts
@@ -1,7 +1,21 @@
-import type { Config } from '../types/Config'
-
 type Action<T, P> = { type: T; payload: P }
 
-type MapActions;
+type SET_CONFIG = Action<'SET_CONFIG', Config>
+type SET_LOADING = Action<'SET_LOADING', boolean>
+type SET_CURRENT_VIEWPORT = Action<'SET_CURRENT_VIEWPORT', string>
+type SET_RUNTIME_FILTERS = Action<'SET_RUNTIME_FILTERS', Object[]>
+type SET_RUNTIME_LEGEND = Action<'SET_RUNTIME_LEGEND', Object[]>
+type SET_ACCESSIBLE_STATUS = Action<'SET_ACCESSIBLE_STATUS', string>
+type SET_MODAL = Action<'SET_MODAL', any>
+
+// prettier-ignore
+type MapActions =
+  | SET_CONFIG
+  | SET_LOADING
+  | SET_CURRENT_VIEWPORT
+  | SET_RUNTIME_FILTERS
+  | SET_RUNTIME_LEGEND
+  | SET_ACCESSIBLE_STATUS
+  | SET_MODAL
 
 export default MapActions
