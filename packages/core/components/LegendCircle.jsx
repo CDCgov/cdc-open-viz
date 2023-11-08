23ded246e4eb926705a8cd42feb71426bb5ee360
Murad CDC <atamurad.babakulyyev@gmail.com>
2023-11-07 20:46:30 -0500

    [DEV-4523a]  Update Legend- Data suppression

--- packages/core/components/LegendCircle.jsx
+++ packages/core/components/LegendCircle.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function LegendCircle({ fill, borderColor }) {
+export default function LegendCircle({ fill, borderColor, display = 'block' }) {
   const styles = {
     marginRight: '5px',
     borderRadius: '300px',
@@ -9,7 +9,8 @@ export default function LegendCircle({ fill, borderColor }) {
     height: '1em',
     width: '1em',
     border: borderColor ? `${borderColor}  1px solid` : 'rgba(0,0,0,.3) 1px solid',
-    backgroundColor: fill
+    backgroundColor: fill,
+    display: display
   }
 
   return <span className='legend-item' style={styles} />
