bb8e61ad580c1ba70bade75da06a391b9f009646
Murad CDC <atamurad.babakulyyev@gmail.com>
2023-11-17 11:10:30 -0500

    [DEV-4523a] Remoce duplicates

--- packages/core/components/LegendCircle.jsx
+++ packages/core/components/LegendCircle.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
-export default function LegendCircle({ fill, borderColor, display = 'block' }) {
+export default function LegendCircle({ fill, borderColor, display = 'inline-block' }) {
   const styles = {
     marginRight: '5px',
     borderRadius: '300px',
     verticalAlign: 'middle',
-    display: 'inline-block',
+    display: display,
     height: '1em',
     width: '1em',
     border: borderColor ? `${borderColor}  1px solid` : 'rgba(0,0,0,.3) 1px solid',
-    backgroundColor: fill,
-    display: display
+    backgroundColor: fill
   }
 
   return <span className='legend-item' style={styles} />
