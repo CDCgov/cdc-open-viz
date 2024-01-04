9669ca32cedc067843613c18899821fec042bb04
Lacey <pro7@cdc.gov>
2024-01-04 13:53:23 -0500

    Updates

---
+++ storybook-e38fcd55.es.js
@@ -0,0 +1,149 @@
+import { M as a, d as i } from "./storybook-cdd77059.es.js";
+import { j as n, a as s, F as u } from "./storybook-96bcff98.es.js";
+import { u as c } from "./storybook-c733c29e.es.js";
+import "./storybook-e988567b.es.js";
+import "./storybook-e61d95ab.es.js";
+import "./storybook-c5d32002.es.js";
+import "./storybook-ac19c974.es.js";
+import "./storybook-d79fba58.es.js";
+import "./storybook-70f3d2a3.es.js";
+import "./storybook-2a203c68.es.js";
+import "./storybook-21ad7f25.es.js";
+import "./storybook-398c73a4.es.js";
+const l = `# Contributing to COVE
+
+Table of Contents
+
+- [Contributing to COVE](#contributing-to-cove)
+  - [Introduction](#introduction)
+  - [How Can I Contribute?](#how-can-i-contribute)
+  - [How to Request New Features and Report Bugs](#how-to-request-new-features-and-report-bugs)
+  - [Asking for Help](#asking-for-help)
+  - [Project Setup Instructions](#project-setup-instructions)
+    - [Branching Information](#branching-information)
+  - [Opening a Pull Request](#opening-a-pull-request)
+  - [Thank You!](#thank-you)
+
+## Introduction
+
+Welcome! We’re excited that you’re interested in using the CDC Open Visualization Environment (COVE), and are looking forward to working with you on this project. As you work through the project's initial setup, you’re in the best position to report any issues you might run into (ie. compilation errors or gaps in our documentation).
+If something doesn't make sense, please [open an issue](https://github.com/CDCgov/cdc-open-viz/issues) and let us know!
+
+## How Can I Contribute?
+
+We welcome many different types of contributions including:
+
+- Code
+- Design/Usability
+- Copy/Documentation
+- Accesibility
+- Security & Performance
+
+Please see the additional sections below before initiating a pull request. All ideas and questions should be routed through a Github Issue.
+
+## How to Request New Features and Report Bugs
+
+Please start all feature requests, bug reports, and ideas by [opening an issue](https://github.com/CDCgov/cdc-open-viz/issues) so that our internal project maintainers are aware of the ideas you want to contribute. We'll do our best to accommodate your request, however, our internal team makes final decisions on how the project is structured. Pitching your ideas early will help avoid unnecessary work and increase your chances of a successful pull request.
+
+_Issues labeled "Help Wanted" are a great place to start for a first contribution._
+
+## Asking for Help
+
+The best way to get questions answered is by [opening an issue](https://github.com/CDCgov/cdc-open-viz/issues). We treat Github Issues the same as our internal communication tools and will respond in a timely manner. In the future, we hope to provide additional communication tools for project support.
+
+## Project Setup Instructions
+
+This repository is a [monorepo](https://en.wikipedia.org/wiki/Monorepo) that is managed with [Lerna](https://github.com/lerna/lerna#readme) and uses [yarn](https://yarnpkg.com/) for package management. Make sure you have yarn installed before beginning. To work on individual packages you must setup the entire monorepo.
+
+1. Start by cloning/forking this repo and running \`yarn install\` at the root.
+2. Run \`lerna bootstrap —force-local \`and Lerna will initialize all the packages for you.
+3. (If using Windows) Change the import from 'react-table' to 'react-table/src' in the DataTable component of these pacakges: Map, Chart, Editor, Dashboard (TODO: fix this)
+4. Run \`lerna run build\` to build all of the packages in the correct order.
+5. To begin working on an individual package, run \`lerna run --scope @cdc/package_name start\`, replacing package_name with the package's name (ex: @cdc/map).
+
+### Branching Information
+
+Notice - all pull requests you should branch from and target test.
+
+| Branch Name | Description                              |
+| ----------- | ---------------------------------------- |
+| main        | Default branch we create releases on     |
+| test        | Development branch we use to test on     |
+| dev         | Development branch we use to branch features on |
+
+After pulling the code to your local, the \`main\` branch will be checked out by default. Switch to test using \`git checkout test\` and then create a feature branch to work on using gitflow, or manually using:
+
+\`git checkout -b feature/new-feature-branch\`
+
+## Opening a Pull Request
+
+You should open a pull request after a project maintainer has approved your idea in a Github Issue.
+
+Steps for submitting a pull request:
+
+- Fork the repository
+
+  - When forking, uncheck _Copy the main branch only_
+
+![Creating a fork](./docs/img/forking_1.png)
+
+- Clone to your local using ssh
+
+- Important - Create a new branch from the test branch
+- Reference relevant issues in your pull request
+- Include supporting documentation and screenshots
+- Fill out the pull request template to the best of your ability, and be sure to reset the base branch to _test_
+
+![Reset the branch to test](./docs/img/forking_2.png)
+
+- Note - Some items in the template might not apply
+- Tag a project maintainer in the pull request for review
+- One of our project maintainers will provide timely feedback
+
+## Thank You!
+
+The COVE team thanks you for considering our project. Once your pull request is accepted and merged through our internal release process, you'll be able to see your updates on the main branch and npm.
+`;
+function r(e) {
+  return /* @__PURE__ */ s(u, {
+    children: [/* @__PURE__ */ n(a, {
+      title: "Contributions"
+    }), `
+`, /* @__PURE__ */ n(i, {
+      children: l
+    })]
+  });
+}
+function p(e = {}) {
+  const {
+    wrapper: o
+  } = Object.assign({}, c(), e.components);
+  return o ? /* @__PURE__ */ n(o, {
+    ...e,
+    children: /* @__PURE__ */ n(r, {
+      ...e
+    })
+  }) : r();
+}
+const h = () => {
+  throw new Error("Docs-only story");
+};
+h.parameters = {
+  docsOnly: !0
+};
+const t = {
+  title: "Contributions",
+  tags: ["stories-mdx"],
+  includeStories: ["__page"]
+};
+t.parameters = t.parameters || {};
+t.parameters.docs = {
+  ...t.parameters.docs || {},
+  page: p
+};
+const D = ["__page"];
+export {
+  D as __namedExportsOrder,
+  h as __page,
+  t as default
+};
--- storybook.js
+++ storybook.js
@@ -4,22 +4,22 @@ import "./sb-preview/runtime.js";
   if (o && o.supports && o.supports("modulepreload"))
     return;
   for (const s of document.querySelectorAll('link[rel="modulepreload"]'))
-    n(s);
+    c(s);
   new MutationObserver((s) => {
     for (const t of s)
       if (t.type === "childList")
         for (const r of t.addedNodes)
-          r.tagName === "LINK" && r.rel === "modulepreload" && n(r);
+          r.tagName === "LINK" && r.rel === "modulepreload" && c(r);
   }).observe(document, { childList: !0, subtree: !0 });
-  function c(s) {
+  function n(s) {
     const t = {};
     return s.integrity && (t.integrity = s.integrity), s.referrerPolicy && (t.referrerPolicy = s.referrerPolicy), s.crossOrigin === "use-credentials" ? t.credentials = "include" : s.crossOrigin === "anonymous" ? t.credentials = "omit" : t.credentials = "same-origin", t;
   }
-  function n(s) {
+  function c(s) {
     if (s.ep)
       return;
     s.ep = !0;
-    const t = c(s);
+    const t = n(s);
     fetch(s.href, t);
   }
 })();
@@ -32,21 +32,26 @@ const p = {
   "./_stories/Guide.NewStories.stories.mdx": async () => import("./storybook-7210c201.es.js"),
   "./_stories/Guide.NewIcons.stories.mdx": async () => import("./storybook-96fa3619.es.js"),
   "./_stories/Guide.NewComponents.stories.mdx": async () => import("./storybook-0bb9982d.es.js"),
-  "./_stories/Contributions.stories.mdx": async () => import("./storybook-f5f81122.es.js"),
-  "./packages/waffle-chart/src/_stories/WaffleChart.stories.tsx": async () => import("./storybook-989c440d.es.js"),
-  "./packages/markup-include/src/_stories/MarkupInclude.stories.tsx": async () => import("./storybook-d44fd7d3.es.js"),
-  "./packages/map/src/_stories/CdcMap.stories.tsx": async () => import("./storybook-55fa7204.es.js"),
-  "./packages/data-bite/src/_stories/DataBite.stories.tsx": async () => import("./storybook-fc0eb6ef.es.js"),
-  "./packages/dashboard/src/_stories/Dashboard.stories.tsx": async () => import("./storybook-bf241bb8.es.js"),
-  "./packages/core/components/_stories/Table.stories.tsx": async () => import("./storybook-df88f5a1.es.js"),
-  "./packages/core/components/_stories/DataTable.stories.tsx": async () => import("./storybook-34bdbc66.es.js"),
-  "./packages/chart/src/_stories/ChartBrush.stories.tsx": async () => import("./storybook-866e6aed.es.js"),
-  "./packages/chart/src/_stories/Chart.tooltip.stories.tsx": async () => import("./storybook-eea291f1.es.js"),
-  "./packages/chart/src/_stories/Chart.stories.tsx": async () => import("./storybook-066b9522.es.js"),
+  "./_stories/Contributions.stories.mdx": async () => import("./storybook-e38fcd55.es.js"),
+  "./packages/waffle-chart/src/_stories/WaffleChart.stories.tsx": async () => import("./storybook-b9315889.es.js"),
+  "./packages/markup-include/src/_stories/MarkupInclude.stories.tsx": async () => import("./storybook-f1dfc413.es.js"),
+  "./packages/map/src/_stories/CdcMap.stories.tsx": async () => import("./storybook-b556567d.es.js"),
+  "./packages/data-bite/src/_stories/DataBite.stories.tsx": async () => import("./storybook-0543d3b0.es.js"),
+  "./packages/dashboard/src/_stories/Dashboard.stories.tsx": async () => import("./storybook-fea43e0e.es.js"),
+  "./packages/core/components/_stories/Table.stories.tsx": async () => import("./storybook-17800994.es.js"),
+  "./packages/core/components/_stories/MultiSelect.stories.tsx": async () => import("./storybook-8745e6e3.es.js"),
+  "./packages/core/components/_stories/Inputs.stories.tsx": async () => import("./storybook-f0ab469f.es.js"),
+  "./packages/core/components/_stories/EditorPanel.stories.tsx": async () => import("./storybook-513dac28.es.js"),
+  "./packages/core/components/_stories/DataTable.stories.tsx": async () => import("./storybook-51a487c6.es.js"),
+  "./packages/chart/src/_stories/ChartSuppress.stories.tsx": async () => import("./storybook-cb7e29db.es.js"),
+  "./packages/chart/src/_stories/ChartEditor.stories.tsx": async () => import("./storybook-411fd857.es.js"),
+  "./packages/chart/src/_stories/ChartBrush.stories.tsx": async () => import("./storybook-efd35c0c.es.js"),
+  "./packages/chart/src/_stories/Chart.tooltip.stories.tsx": async () => import("./storybook-1ef1d25a.es.js"),
+  "./packages/chart/src/_stories/Chart.stories.tsx": async () => import("./storybook-7c5059dc.es.js"),
   "./packages/core/components/ui/_stories/Title.stories.tsx": async () => import("./storybook-074f4ef7.es.js"),
-  "./packages/core/components/ui/_stories/Icon.stories.tsx": async () => import("./storybook-b42b35d4.es.js"),
-  "./packages/core/components/ui/_stories/Accordion.stories.tsx": async () => import("./storybook-0fa38ecd.es.js"),
-  "./packages/core/components/managers/_stories/DataDesigner.stories.tsx": async () => import("./storybook-db42b83a.es.js"),
+  "./packages/core/components/ui/_stories/Icon.stories.tsx": async () => import("./storybook-1f17c18d.es.js"),
+  "./packages/core/components/ui/_stories/Accordion.stories.tsx": async () => import("./storybook-a54c198d.es.js"),
+  "./packages/core/components/managers/_stories/DataDesigner.stories.tsx": async () => import("./storybook-95264c00.es.js"),
   "./packages/core/components/elements/_stories/Card.stories.tsx": async () => import("./storybook-26ffaadf.es.js"),
   "./packages/core/components/elements/_stories/Button.stories.tsx": async () => import("./storybook-ba9904c8.es.js")
 };
