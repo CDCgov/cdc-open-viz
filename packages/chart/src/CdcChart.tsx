5947e9fd391eb664d1fcaec5215ef7f04cb9b069
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-01-04 15:46:41 -0800

    [DEV-6435] Forest Plot > Minor Updates

--- packages/chart/src/CdcChart.tsx
+++ packages/chart/src/CdcChart.tsx
@@ -1053,7 +1053,7 @@ export default function CdcChart({ configUrl, config: configObj, isEditor = fals
                   )}
                 </>
               )}
-              {!config.legend.hide && config.visualizationType !== 'Spark Line' && <Legend />}
+              {!config.legend.hide && config.visualizationType !== 'Spark Line' && config.visualizationType !== 'Forest Plot' && <Legend />}
             </div>
             {/* Link */}
             {isDashboard && config.table && config.table.show && config.table.showDataTableLink ? tableLink : link && link}
