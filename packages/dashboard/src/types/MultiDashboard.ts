65acf5d6441aeb2a958e3f25d39211b5a692ecb9
Lacey <pro7@cdc.gov>
2024-01-17 10:47:02 -0500

    removed multi-dashboard commits

--- packages/dashboard/src/types/MultiDashboard.ts
+++
@@ -1,8 +0,0 @@
-import { Visualization } from '@cdc/core/types/Visualization'
-import { Dashboard } from './Dashboard'
-import { DashboardConfig } from './DashboardConfig'
-import { Row } from './Row'
-
-export type MultiDashboard = DashboardConfig & {
-  multiDashboards: Record<string, { dashboard: Dashboard; rows: Row[]; visualizations: Record<string, Visualization> }>
-}
