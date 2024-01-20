0fcbb6c2d04d85b614da81687122e13b62ce14b0
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-01-19 18:56:38 -0800

    [editor-context] start adding editor panel context to charts

--- packages/chart/src/components/EditorPanel/components/Panels.tsx
+++ packages/chart/src/components/EditorPanel/components/Panels.tsx
@@ -2,12 +2,14 @@ import ForestPlotSettings from './Panel.ForestPlotSettings'
 import Series from './Panel.Series.jsx'
 import DateHighlighting from './Panel.DateHighlighting'
 import Regions from './Panel.Regions'
+import General from './Panel.General'
 
 const Panels = {
   ForestPlot: ForestPlotSettings,
   Series: Series,
   DateHighlighting,
-  Regions
+  Regions,
+  General
 }
 
 export default Panels
