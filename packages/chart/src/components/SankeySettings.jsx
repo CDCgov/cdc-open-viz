e6ddc8e3392e0b3497017c8ba38619190175f149
Navreet Bajwa <navbajwa@deloitte.com>
2024-01-25 16:00:26 -0500

    initial updates

--- packages/chart/src/components/SankeySettings.jsx
+++ packages/chart/src/components/SankeySettings.jsx
@@ -7,6 +7,83 @@ import Icon from '@cdc/core/components/ui/Icon'
 
 import { AccordionItem, AccordionItemHeading, AccordionItemPanel, AccordionItemButton } from 'react-accessible-accordion'
 
+const updateField = (section, subsection, fieldName, newValue) => {
+  if (null === section && null === subsection) {
+    let updatedConfig = { ...config, [fieldName]: newValue }
+    enforceRestrictions(updatedConfig)
+    updateConfig(updatedConfig)
+    return
+  }
+
+  const isArray = Array.isArray(config[section])
+
+  let sectionValue = isArray ? [...config[section], newValue] : { ...config[section], [fieldName]: newValue }
+
+  if (null !== subsection) {
+    if (isArray) {
+      sectionValue = [...config[section]]
+      sectionValue[subsection] = { ...sectionValue[subsection], [fieldName]: newValue }
+    } else if (typeof newValue === 'string') {
+      sectionValue[subsection] = newValue
+    } else {
+      sectionValue = { ...config[section], [subsection]: { ...config[section][subsection], [fieldName]: newValue } }
+    }
+  }
+
+  let updatedConfig = { ...config, [section]: sectionValue }
+
+  enforceRestrictions(updatedConfig)
+
+  updateConfig(updatedConfig)
+}
+
+/* eslint-disable react-hooks/rules-of-hooks */
+const TextField = memo(({ label, tooltip, section = null, subsection = null, fieldName, updateField, value: stateValue, type = 'input', i = null, min = null, ...attributes }) => {
+  const [value, setValue] = useState(stateValue)
+
+  const [debouncedValue] = useDebounce(value, 500)
+
+  useEffect(() => {
+    if ('string' === typeof debouncedValue && stateValue !== debouncedValue) {
+      updateField(section, subsection, fieldName, debouncedValue, i)
+    }
+  }, [debouncedValue]) // eslint-disable-line
+
+  let name = subsection ? `${section}-${subsection}-${fieldName}` : `${section}-${subsection}-${fieldName}`
+
+  const onChange = e => {
+    if ('number' !== type || min === null) {
+      setValue(e.target.value)
+    } else {
+      if (!e.target.value || min <= parseFloat(e.target.value)) {
+        setValue(e.target.value)
+      } else {
+        setValue(min.toString())
+      }
+    }
+  }
+
+  let formElement = <input type='text' name={name} onChange={onChange} {...attributes} value={value} />
+
+  if ('textarea' === type) {
+    formElement = <textarea name={name} onChange={onChange} {...attributes} value={value}></textarea>
+  }
+
+  if ('number' === type) {
+    formElement = <input type='number' name={name} onChange={onChange} {...attributes} value={value} />
+  }
+
+  return (
+    <label>
+      <span className='edit-label column-heading'>
+        {label}
+        {tooltip}
+      </span>
+      {formElement}
+    </label>
+  )
+})
+
 const CheckBox = memo(({ label, value, fieldName, section = null, subsection = null, tooltip, updateField, ...attributes }) => (
   <label className='checkbox column-heading'>
     <input
@@ -25,31 +102,6 @@ const CheckBox = memo(({ label, value, fieldName, section = null, subsection = n
   </label>
 ))
 
-let updateField = (section, subsection, fieldName, value, i) => regionUpdate(fieldName, value, i)
-
-  let removeColumn = i => {
-    let regions = []
-
-    if (config.regions) {
-      regions = [...config.regions]
-    }
-
-    regions.splice(i, 1)
-
-    updateConfig({ ...config, regions })
-  }
-
-  let addColumn = () => {
-    let regions = []
-
-    if (config.regions) {
-      regions = [...config.regions]
-    }
-
-    regions.push({})
-
-    updateConfig({ ...config, regions })
-  }
 
 const SankeySettings = () => {
     const { config } = useContext(ConfigContext)
@@ -62,9 +114,11 @@ const SankeySettings = () => {
         </AccordionItemButton>
       </AccordionItemHeading>
       <AccordionItemPanel>
-      <CheckBox value={config.enableKPIs} fieldName='enableKPIs' label='Enable KPIs' updateField={updateField} />
-      <CheckBox value={config.enableStoryNodes} fieldName='enableStoryNodes' label='Enable StoryNodes' updateField={updateField} />
-      <CheckBox value={config.enableTooltips} fieldName='enableTooltips' label='Enable Tooltips' updateField={updateField} />
+      <CheckBox value={config.enableKPIs} fieldName='enableKPIs' label='Enable KPIs'/>
+      {config.enableKPIs && <TextField value={config.numKPIs} fieldName='numKPIs' label='Number of KPIs' type='number' min='1' max='3' placeholder='No. KPIs' updateField={updateField}/>}
+      {config.numKPIs && <p>hi</p>}
+      <CheckBox value={config.enableStoryNodes} fieldName='enableStoryNodes' label='Enable StoryNodes'  />
+      <CheckBox value={config.enableTooltips} fieldName='enableTooltips' label='Enable Tooltips'  />
       </AccordionItemPanel>
     </AccordionItem>
   )
