568ec16df378f61890c2a16aae842b3d92d7411c
Murad CDC <atamurad.babakulyyev@gmail.com>
2024-01-25 11:22:45 -0500

    [merge-test-to-dev] Add dashed Legend

--- packages/chart/src/components/Legend/Legend.Component.tsx
+++ packages/chart/src/components/Legend/Legend.Component.tsx
@@ -33,6 +33,26 @@ const Legend: React.FC<LegendProps> = ({ config, colorScale, seriesHighlight, hi
     unknown: 'block'
   })
 
+  const renderDashes = style => {
+    const dashCount = style === 'Dashed Small' ? 3 : 2
+    const dashClass = `dashes ${style.toLowerCase().replace(' ', '-')}`
+
+    return (
+      <div className={dashClass}>
+        {Array.from({ length: dashCount }, (_, i) => (
+          <span key={i}>-</span>
+        ))}
+      </div>
+    )
+  }
+  const renderDashesOrCircle = style => {
+    if (['Dashed Small', 'Dashed Medium', 'Dashed Large'].includes(style)) {
+      return renderDashes(style)
+    } else if (style === 'Open Circles') {
+      return <div className='dashes open-circles'></div>
+    }
+  }
+
   const isBottomOrSmallViewport = legend.position === 'bottom' || ['sm', 'xs', 'xxs'].includes(currentViewport)
 
   const legendClasses = {
@@ -51,97 +71,121 @@ const Legend: React.FC<LegendProps> = ({ config, colorScale, seriesHighlight, hi
       <LegendOrdinal scale={colorScale} itemDirection='row' labelMargin='0 20px 0 0' shapeMargin='0 10px 0'>
         {labels => {
           return (
-            <div className={innerClasses.join(' ')}>
-              {formatLabels(labels as Label[]).map((label, i) => {
-                let className = ['legend-item', `legend-text--${label.text.replace(' ', '').toLowerCase()}`]
-                let itemName = label.datum
+            <>
+              <div className={innerClasses.join(' ')}>
+                {formatLabels(labels as Label[]).map((label, i) => {
+                  let className = ['legend-item', `legend-text--${label.text.replace(' ', '').toLowerCase()}`]
+                  let itemName = label.datum
 
-                // Filter excluded data keys from legend
-                if (config.exclusions.active && config.exclusions.keys?.includes(itemName)) {
-                  return null
-                }
-
-                if (runtime.seriesLabels) {
-                  let index = config.runtime.seriesLabelsAll.indexOf(itemName)
-                  itemName = config.runtime.seriesKeys[index]
-
-                  if (runtime?.forecastingSeriesKeys?.length > 0) {
-                    itemName = label.text
+                  // Filter excluded data keys from legend
+                  if (config.exclusions.active && config.exclusions.keys?.includes(itemName)) {
+                    return null
                   }
-                }
 
-                if (seriesHighlight.length > 0 && false === seriesHighlight.includes(itemName)) {
-                  className.push('inactive')
-                }
+                  if (runtime.seriesLabels) {
+                    let index = config.runtime.seriesLabelsAll.indexOf(itemName)
+                    itemName = config.runtime.seriesKeys[index]
 
-                return (
-                  <LegendItem
-                    className={className.join(' ')}
-                    tabIndex={0}
-                    key={`legend-quantile-${i}`}
-                    onKeyPress={e => {
-                      if (e.key === 'Enter') {
+                    if (runtime?.forecastingSeriesKeys?.length > 0) {
+                      itemName = label.text
+                    }
+                  }
+
+                  if (seriesHighlight.length > 0 && false === seriesHighlight.includes(itemName)) {
+                    className.push('inactive')
+                  }
+
+                  return (
+                    <LegendItem
+                      className={className.join(' ')}
+                      tabIndex={0}
+                      key={`legend-quantile-${i}`}
+                      onKeyPress={e => {
+                        if (e.key === 'Enter') {
+                          highlight(label)
+                        }
+                      }}
+                      onClick={() => {
                         highlight(label)
-                      }
-                    }}
-                    onClick={() => {
-                      highlight(label)
-                    }}
-                  >
-                    {config.visualizationType === 'Line' && config.legend.lineMode ? (
-                      <svg width={40} height={20}>
-                        <Line from={{ x: 10, y: 10 }} to={{ x: 40, y: 10 }} stroke={label.value} strokeWidth={2} strokeDasharray={handleLineType(config.series[i]?.type ? config.series[i]?.type : '')} />
-                      </svg>
-                    ) : (
-                      <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <LegendCircle margin='0' fill={label.value} display={displayScale(label.datum)} />
-                        <div style={{ marginTop: '2px', marginRight: '6px' }}>{label.icon}</div>
-                      </div>
-                    )}
+                      }}
+                    >
+                      {config.visualizationType === 'Line' && config.legend.lineMode ? (
+                        <svg width={40} height={20}>
+                          <Line from={{ x: 10, y: 10 }} to={{ x: 40, y: 10 }} stroke={label.value} strokeWidth={2} strokeDasharray={handleLineType(config.series[i]?.type ? config.series[i]?.type : '')} />
+                        </svg>
+                      ) : (
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                          <LegendCircle margin='0' fill={label.value} display={displayScale(label.datum)} />
+                          <div style={{ marginTop: '2px', marginRight: '6px' }}>{label.icon}</div>
+                        </div>
+                      )}
 
-                    <LegendLabel align='left' margin='0 0 0 4px'>
-                      {label.text}
-                    </LegendLabel>
-                  </LegendItem>
-                )
-              })}
+                      <LegendLabel align='left' margin='0 0 0 4px'>
+                        {label.text}
+                      </LegendLabel>
+                    </LegendItem>
+                  )
+                })}
 
-              {highLightedLegendItems.map((bar, i) => {
-                // if duplicates only return first item
-                let className = 'legend-item'
-                let itemName = bar.legendLabel
+                {highLightedLegendItems.map((bar, i) => {
+                  // if duplicates only return first item
+                  let className = 'legend-item'
+                  let itemName = bar.legendLabel
 
-                if (!itemName) return false
-                if (seriesHighlight.length > 0 && false === seriesHighlight.includes(itemName)) {
-                  className += ' inactive'
-                }
-                return (
-                  <LegendItem
-                    className={className}
-                    tabIndex={0}
-                    key={`legend-quantile-${i}`}
-                    onKeyPress={e => {
-                      if (e.key === 'Enter') {
+                  if (!itemName) return false
+                  if (seriesHighlight.length > 0 && false === seriesHighlight.includes(itemName)) {
+                    className += ' inactive'
+                  }
+                  return (
+                    <LegendItem
+                      className={className}
+                      tabIndex={0}
+                      key={`legend-quantile-${i}`}
+                      onKeyPress={e => {
+                        if (e.key === 'Enter') {
+                          highlight(bar.legendLabel)
+                        }
+                      }}
+                      onClick={() => {
                         highlight(bar.legendLabel)
-                      }
-                    }}
-                    onClick={() => {
-                      highlight(bar.legendLabel)
-                    }}
-                  >
-                    <LegendCircle fill='transparent' borderColor={bar.color ? bar.color : `rgba(255, 102, 1)`} />{' '}
-                    <LegendLabel align='left' margin='0 0 0 4px'>
-                      {bar.legendLabel ? bar.legendLabel : bar.value}
-                    </LegendLabel>
-                  </LegendItem>
-                )
-              })}
-              {seriesHighlight.length > 0 && (
-                <button className={`legend-reset ${config.theme}`} onClick={labels => highlightReset(labels)} tabIndex={0}>
-                  Reset
-                </button>
-              )}
-            </div>
+                      }}
+                    >
+                      <LegendCircle fill='transparent' borderColor={bar.color ? bar.color : `rgba(255, 102, 1)`} />{' '}
+                      <LegendLabel align='left' margin='0 0 0 4px'>
+                        {bar.legendLabel ? bar.legendLabel : bar.value}
+                      </LegendLabel>
+                    </LegendItem>
+                  )
+                })}
+                {seriesHighlight.length > 0 && (
+                  <button className={`legend-reset ${config.theme}`} onClick={labels => highlightReset(labels)} tabIndex={0}>
+                    Reset
+                  </button>
+                )}
+              </div>
+
+              <>
+                {config.preliminaryData.some(pd => pd.label) && (
+                  <>
+                    <hr></hr>
+                    <div className={config.legend.singleRow && isBottomOrSmallViewport ? 'legend-container__inner bottom single-row' : 'dash-left'}>
+                      {config.preliminaryData.map((pd, index) => {
+                        return (
+                          <div className='dash-container' key={index}>
+                            {pd.label && (
+                              <>
+                                <div className='dash-inner'>{renderDashesOrCircle(pd.style)}</div>
+                                <div style={{ marginLeft: '7px' }}>{pd.label}</div>
+                              </>
+                            )}
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </>
+                )}
+              </>
+            </>
           )
         }}
       </LegendOrdinal>
