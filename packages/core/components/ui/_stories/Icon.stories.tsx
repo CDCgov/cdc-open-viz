c50026cfd7412f199ed84f824550d4ce16fed1d6
Lacey <pro7@cdc.gov>
2024-01-26 12:51:10 -0500

    added icon-story with all icons displayed

--- packages/core/components/ui/_stories/Icon.stories.tsx
+++ packages/core/components/ui/_stories/Icon.stories.tsx
@@ -2,21 +2,31 @@ import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import Icon, { ICON_TYPES } from '../Icon'
+import { iconHash } from '../Icon'
 
-const meta: Meta<typeof Icon> = {
+const iconNames = Object.keys(iconHash)
+
+const IconArray = () => {
+  return (
+    <>
+      {iconNames.map(name => (
+        <div>
+          <span>
+            <Icon display={name} /> {name}{' '}
+          </span>
+        </div>
+      ))}
+    </>
+  )
+}
+
+const meta: Meta<typeof IconArray> = {
   title: 'Components/Atoms/Icon',
-  component: Icon,
-  parameters: {
-    display: ICON_TYPES
-  }
+  component: IconArray
 }
 
 type Story = StoryObj<typeof Icon>
 
-export const Primary: Story = {
-  args: {
-    display: 'question'
-  }
-}
+export const Primary: Story = {}
 
 export default meta
