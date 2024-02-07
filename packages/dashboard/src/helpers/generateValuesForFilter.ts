13835c41222f5d517ccc753a72447ede1aca5998
Lacey <pro7@cdc.gov>
2024-02-07 12:57:03 -0500

    refactored filterData and moved Filters to external component, added vite coverage reporter

--- packages/dashboard/src/helpers/generateValuesForFilter.ts
+++ packages/dashboard/src/helpers/generateValuesForFilter.ts
@@ -1,21 +1,11 @@
-import { FilterBehavior } from '../components/Header/Header'
-
-// Gets filter values from API response
-export const generateValuesForAPIFilter = (columnName, _data): string[] => {
-  type Row = { [key: string]: any }
-  return Object.values(_data)
-    .filter(row => row && !!(row as Row)[columnName])
-    .map(row => (row as Row)[columnName])
-}
-
 // Gets filter values from dataset
-export const generateValuesForFilter = (columnName, _data, filterBehavior) => {
+export const generateValuesForFilter = (columnName, _data) => {
   const values: string[] = []
 
   Object.keys(_data).forEach(key => {
     _data[key]?.forEach(row => {
       const value = row[columnName]
-      if (value && false === values.includes(value)) {
+      if (!values.includes(value)) {
         values.push(value)
       }
     })
