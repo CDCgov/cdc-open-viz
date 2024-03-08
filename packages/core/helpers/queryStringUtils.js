51124ea9eb5a6a3e47a88d3b87f74e883c3653f8
mpallansch <matthew.pallansch@gmail.com>
2024-03-08 14:30:01 -0500

    Removes Case Sensitivity in QS Usage

--- packages/core/helpers/queryStringUtils.js
+++ packages/core/helpers/queryStringUtils.js
@@ -2,8 +2,12 @@ export function getQueryStringFilterValue(filter) {
   const urlParams = new URLSearchParams(window.location.search)
   if(filter.setByQueryParameter){ // Only check the query string if the filter is supposed to be set by QS param
     const filterValue = urlParams.get(filter.setByQueryParameter)
-    if(filterValue && filter.values && filter.values.includes(filterValue)){ // Only use the QS param if it is one of the valid options in the "values" property of that filter
-      return filterValue
+    if(filterValue && filter.values){
+      for(let i = 0; i < filter.values.length; i++){
+        if(filter.values[i] && filter.values[i].toLowerCase() === filterValue.toLowerCase()){
+          return filter.values[i]
+        }
+      }
     }
   }
 }
