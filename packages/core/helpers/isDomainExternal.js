77948f1765fe03aa0788e7caa439cbbb24a70eae
mpallansch <matthew.pallansch@gmail.com>
2024-03-13 13:29:12 -0400

    Dev 7280 (#1175)
    * Adds Logic For External Link Indicator
    
    * Adds External Icon to Click Tooltip

---
+++ packages/core/helpers/isDomainExternal.js
@@ -0,0 +1,30 @@
+export default function isDomainExternal(domain) {
+  const internalDomains = [
+    'cdc.gov',
+    'localhost',
+    'facebook.com',
+    'twitter.com',
+    'linkedin.com',
+    'pinterest.com',
+    'youtube.com',
+    'youtube-nocookie.com',
+    'plus.google.com',
+    'instagram.com',
+    'flickr.com',
+    'tumblr.com',
+    'cdc.sharepoint.com',
+    'vaccines.gov',
+    'vacunas.gov'
+  ]
+
+  const hostname = (new URL(domain, window.location.origin)).hostname;
+  let external = true
+
+  internalDomains.forEach(internalDomain => {
+    if(hostname.indexOf(internalDomain) !== -1 && hostname.indexOf(internalDomain) === (hostname.length - internalDomain.length)){
+      external = false
+    }
+  })
+
+  return external
+}
