23da6838f1d627debd8147b4dfda9c51dfb75676
Navreet Bajwa <navbajwa@deloitte.com>
2024-04-02 13:47:43 -0400

    working on active and inactive state

--- packages/chart/src/components/BumpChart/components/BumpChart.tsx
+++ packages/chart/src/components/BumpChart/components/BumpChart.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
 import { LinePath } from '@visx/shape'
 import { Group } from '@visx/group'
 import { ChartConfig } from '../../../types/ChartConfig'
@@ -33,8 +34,19 @@ const BumpChart: React.FC<BumpChartProps> = ({ width = 700, height = 900, config
     }
   }
 
+  const [activeState, setActiveState] = useState<string>('')
+
+  const handleClick = (seriesItem: string) => {
+    setActiveState(seriesItem)
+    console.log(activeState)
+    console.log(colorScale(activeState))
+  }
+
+  const clearClick = () => {
+    setActiveState('')
+  }
+
   return (
-    
     <Group className='bump-chart-lines' width={width} height={height} left={Number(config.runtime.yAxis.size)} key='bump-chart-lines'>
       {seriesKeys.map(seriesItem =>
         config.data.map((d, dataIndex) => {
@@ -60,46 +72,46 @@ const BumpChart: React.FC<BumpChartProps> = ({ width = 700, height = 900, config
           }
           return (
             <>
-            <g>
-              <circle
-                r={10}
-                cx={Number(xScale(handleX(d.year)))}
-                cy={Number(yScale(d[seriesItem]))}
-                stroke='#CACACA'
-                strokeWidth={1}
-                fill='none'
-                style={{ zIndex: 2}}
+              <g>
+                <circle
+                  r={10}
+                  cx={Number(xScale(handleX(d.year)))}
+                  cy={Number(yScale(d[seriesItem]))}
+                  stroke='#CACACA'
+                  strokeWidth={1}
+                  fill='none'
+                  style={{ zIndex: 2}}
+                />
+                <text
+                  x={Number(xScale(handleX(d.year))) - 4}
+                  y={Number(yScale(d[seriesItem])) + 4}
+                  fill='#000000'
+                  fontSize={13}
+                  style={{ zIndex: 2}}
+                > {d[seriesItem]}</text>
+              </g>
+              <LinePath
+                curve={allCurves['curveCardinal']}
+                key={key}
+                data-key={key}
+                data-key-year={seriesItem}
+                data={config.data}
+                x={data => Number(xScale(handleX(data[config.xAxis.dataKey])))}
+                y={data => Number(yScale(data[seriesItem]))}
+                stroke={(seriesItem === activeState || activeState === '') ? colorScale(seriesItem) : '#000000'}
+                onClick={() => handleClick(seriesItem)}
+                onMouseLeave={() => clearClick()}
+                strokeWidth={5}
+                defined={(item, i) => {
+                  return item[seriesItem] !== '' && item[seriesItem] !== null && item[seriesItem] !== undefined
+                }}
+                shapeRendering='geometricPrecision' // Enable anti-aliasing?
+                style={{ zIndex: 1}}
               />
-              <text
-                x={Number(xScale(handleX(d.year))) - 4}
-                y={Number(yScale(d[seriesItem])) + 4}
-                fill='#000000'
-                fontSize={13}
-                style={{ zIndex: 2}}
-              > {d[seriesItem]}</text>
-            </g>
-            <LinePath
-              curve={allCurves['curveCardinal']}
-              key={key}
-              data-key={key}
-              data-key-year={seriesItem}
-              data={config.data}
-              x={data => Number(xScale(handleX(data[config.xAxis.dataKey])))}
-              y={data => Number(yScale(data[seriesItem]))}
-              stroke={colorScale(seriesItem)}
-              strokeWidth={2}
-              fill='transparent'
-              defined={(item, i) => {
-                return item[seriesItem] !== '' && item[seriesItem] !== null && item[seriesItem] !== undefined
-              }}
-              shapeRendering='geometricPrecision' // Enable anti-aliasing?
-              style={{ zIndex: 1}}
-            />
             </>
-            
           )
         })
-      )}
+      )}    
     </Group>
   )
 }
