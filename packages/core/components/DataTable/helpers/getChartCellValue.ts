1d09f1a4d5b14e624cee6019143ff820fbd92a2b
Murad CDC <atamurad.babakulyyev@gmail.com>
2024-04-30 08:22:51 -0400

    [DEV-4523] Add suppression to Data Table cells

--- packages/core/components/DataTable/helpers/getChartCellValue.ts
+++ packages/core/components/DataTable/helpers/getChartCellValue.ts
@@ -1,7 +1,24 @@
 import { parseDate, formatDate } from '@cdc/core/helpers/cove/date'
 import { formatNumber } from '@cdc/core/helpers/cove/number'
 import { TableConfig } from '../types/TableConfig'
-
+// icons for cell value if these are suppressed
+const suppressionIcons = {
+  Asterisk: '\u002A',
+  'Double Asterisks': '\u002A\u002A',
+  Dagger: '\u2020',
+  'Double Daggers': '\u2021\u2021',
+  'Section Sign': '\u00A7',
+  Pilcrow: '\u00B6',
+  Hash: '\u0023'
+}
+// functiion helps to check if cell value should be suppressed
+const shouldSuppress = (column, cellValue, preliminaryData) => {
+  return preliminaryData?.some(pd => {
+    const matchesColumn = pd.column ? pd.column === column : true
+    const matchesValue = String(cellValue) === String(pd.value)
+    return matchesColumn && matchesValue && pd.symbol && pd.type === 'suppression'
+  })
+}
 // if its additional column, return formatting params
 const isAdditionalColumn = (column, config) => {
   let inthere = false
@@ -30,6 +47,7 @@ export const getChartCellValue = (row: string, column: string, config: TableConf
   const rowObj = runtimeData[row]
   let cellValue // placeholder for formatting below
   let labelValue = rowObj[column] // just raw X axis string
+  console.log(column, 'column')
   if (column === config.xAxis?.dataKey) {
     // not the prettiest, but helper functions work nicely here.
     cellValue = config.xAxis?.type === 'date' ? formatDate(config.table?.dateDisplayFormat || config.xAxis?.dateDisplayFormat, parseDate(config.xAxis?.dateParseFormat, labelValue)) : labelValue
@@ -53,6 +71,10 @@ export const getChartCellValue = (row: string, column: string, config: TableConf
       cellValue = config.dataFormat ? formatNumber(runtimeData[row][column], resolvedAxis, false, config) : runtimeData[row][column]
     }
   }
+
+  config.preliminaryData.forEach(pd => {
+    cellValue = shouldSuppress(column, labelValue, config.preliminaryData) ? suppressionIcons[pd?.symbol] : cellValue
+  })
 
   return cellValue
 }
