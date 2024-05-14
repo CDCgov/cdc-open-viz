2af2eb96d91419d2c4531689913a7d9f70093124
mpallansch <matthew.pallansch@gmail.com>
2024-05-14 14:53:53 -0400

    Reverts Debug Change

--- packages/core/components/MediaControls.jsx
+++ packages/core/components/MediaControls.jsx
@@ -60,8 +60,7 @@ const generateMedia = (state, type, elementToCapture) => {
   switch (type) {
     case 'image':
       html2canvas(baseSvg, {ignoreElements: el => el.className?.indexOf && el.className.search(/download-buttons|download-links|data-table-container/) !== -1}).then(canvas => {
-        document.body.appendChild(canvas);
-        //saveImageAs(canvas.toDataURL(), filename + '.png')
+        saveImageAs(canvas.toDataURL(), filename + '.png')
       })
       return
     case 'pdf':
