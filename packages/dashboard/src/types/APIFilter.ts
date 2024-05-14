68016aaa80899d40b4047051b532ed5fe1f6721a
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-05-14 13:02:10 -0700

    Revert "[dev] Merge branch 'test' into dev"
    This reverts commit 8afb5e3fac4e3adb8c819288075969e961b6bea2, reversing
    changes made to 67c47630d0135f01eb535de2f90d99495996769e.

--- packages/dashboard/src/types/APIFilter.ts
+++ packages/dashboard/src/types/APIFilter.ts
@@ -1,5 +1,4 @@
-export type APIFilter = Record<'apiEndpoint'|'valueSelector'|'textSelector', string> & {
-    heirarchyLookup?: string
-    autoLoad?: boolean
-    defaultValue?: string
+export type APIFilter = Record<'apiEndpoint' | 'valueSelector' | 'textSelector', string> & {
+  autoLoad?: boolean
+  defaultValue?: string
 }
