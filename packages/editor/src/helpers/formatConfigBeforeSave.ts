bedeee3c3052c0e7a2539a5a8d11b2cabaff93a2
Lacey <pro7@cdc.gov>
2024-05-21 10:23:29 -0400

    fixed format before save

--- packages/editor/src/helpers/formatConfigBeforeSave.ts
+++ packages/editor/src/helpers/formatConfigBeforeSave.ts
@@ -3,52 +3,63 @@ import { Visualization } from '@cdc/core/types/Visualization'
 import { DashboardConfig } from '@cdc/dashboard/src/types/DashboardConfig'
 import _ from 'lodash'
 
-export const formatConfigBeforeSave = configToStrip => {
-  let strippedConfig = { ...configToStrip }
+const cleanDashboardFootnotes = (config: DashboardConfig) => {
+  // strip any blank footnote visualizations
+  const footnoteIds: string[] = []
 
-  if (strippedConfig.type === 'dashboard') {
-    if (strippedConfig.datasets) {
-      Object.keys(strippedConfig.datasets).forEach(datasetKey => {
-        delete strippedConfig.datasets[datasetKey].formattedData
-        if (strippedConfig.datasets[datasetKey].dataUrl) {
-          delete strippedConfig.datasets[datasetKey].data
-        }
-      })
-    }
-    if (strippedConfig.visualizations) {
-      Object.keys(strippedConfig.visualizations).forEach(vizKey => {
-        strippedConfig.visualizations[vizKey] = _.omit(strippedConfig.visualizations[vizKey], ['runtime', 'formattedData', 'data'])
-      })
-    }
-    if (strippedConfig.rows) {
-      strippedConfig.rows.forEach(row => {
-        if (row.dataKey) {
-          row = _.omit(row, ['data', 'formattedData'])
-        }
-      })
-    }
-
-    // strip any blank footnote visualizations
-    const footnoteIds: string[] = []
-
-    ;(strippedConfig as DashboardConfig).rows.forEach(row => {
+  if (config.rows) {
+    config.rows.forEach(row => {
       if (row.footnotesId) {
-        const { dataKey, staticFootnotes } = strippedConfig.visualizations[row.footnotesId] as Footnotes
+        const { dataKey, staticFootnotes } = config.visualizations[row.footnotesId] as Footnotes
         if (!dataKey && !staticFootnotes?.length) {
-          delete strippedConfig.visualizations[row.footnotesId]
+          delete config.visualizations[row.footnotesId]
         } else {
           footnoteIds.push(row.footnotesId)
         }
       }
     })
+  }
 
-    Object.keys(strippedConfig.visualizations).forEach(vizKey => {
-      const viz: Visualization = strippedConfig.visualizations[vizKey]
+  if (config.visualizations) {
+    Object.keys(config.visualizations).forEach(vizKey => {
+      const viz: Visualization = config.visualizations[vizKey]
       if (viz.type === 'footnotes' && !footnoteIds.includes(vizKey)) {
         // if footnote isn't being used by any rows, remove it
-        delete strippedConfig.visualizations[vizKey]
+        delete config.visualizations[vizKey]
       }
     })
+  }
+}
+
+const cleanDashboardData = (config: DashboardConfig) => {
+  if (config.datasets) {
+    Object.keys(config.datasets).forEach(datasetKey => {
+      delete config.datasets[datasetKey].formattedData
+      if (config.datasets[datasetKey].dataUrl) {
+        delete config.datasets[datasetKey].data
+      }
+    })
+  }
+  if (config.visualizations) {
+    Object.keys(config.visualizations).forEach(vizKey => {
+      config.visualizations[vizKey] = _.omit(config.visualizations[vizKey], ['runtime', 'formattedData', 'data'])
+    })
+  }
+  if (config.rows) {
+    config.rows.forEach(row => {
+      if (row.dataKey) {
+        row = _.omit(row, ['data', 'formattedData'])
+      }
+    })
+  }
+}
+
+export const formatConfigBeforeSave = configToStrip => {
+  let strippedConfig = { ...configToStrip }
+  if (strippedConfig.type === 'dashboard') {
+    cleanDashboardData(strippedConfig)
+
+    cleanDashboardFootnotes(strippedConfig)
   } else {
     delete strippedConfig.runtime
     delete strippedConfig.formattedData
