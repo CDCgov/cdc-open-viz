760baae6691ec74277ab3602ccf1f9cfd532ec20
Murad CDC <atamurad.babakulyyev@gmail.com>
2024-06-05 11:25:12 -0400

    [DEV-4215a] Update legend positions on dashboards

--- packages/chart/src/CdcChart.tsx
+++ packages/chart/src/CdcChart.tsx
@@ -1119,12 +1119,13 @@ export default function CdcChart({ configUrl, config: configObj, isEditor = fals
   }
 
   const getChartWrapperClasses = () => {
+    const isLegendOnBottom = legend?.position === 'bottom' || ['sm', 'xs', 'xxs'].includes(currentViewport)
     const classes = ['chart-container', 'p-relative']
     if (config.legend?.position === 'bottom') classes.push('bottom')
     if (config.legend?.hide) classes.push('legend-hidden')
     if (lineDatapointClass) classes.push(lineDatapointClass)
     if (!config.barHasBorder) classes.push('chart-bar--no-border')
-    if (config.brush?.active && dashboardConfig?.type === 'dashboard') classes.push('dashboard-brush')
+    if (config.brush?.active && dashboardConfig?.type === 'dashboard' && (!isLegendOnBottom || config.legend.hide)) classes.push('dashboard-brush')
     classes.push(...contentClasses)
     return classes
   }
