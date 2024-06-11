ebf53501ecbe4773ebbbccf4fe11aea7d43060d0
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-06-11 09:15:41 -0700

    [dev] tooltip hotfix

--- packages/map/src/CdcMap.tsx
+++ packages/map/src/CdcMap.tsx
@@ -1727,9 +1727,6 @@ const CdcMap = ({ className, config, navigationHandler: customNavigationHandler,
                       {'data' === general.type && logo && <img src={logo} alt='' className='map-logo' />}
                     </>
                   )}
-                  {!window.matchMedia('(any-hover: none)').matches && 'hover' === tooltips.appearanceType && (
-                    <ReactTooltip id={`tooltip__${tooltipId}`} float={true} className={`${tooltips.capitalizeLabels ? 'capitalize tooltip tooltip-test' : 'tooltip tooltip-test'}`} style={{ background: `rgba(255,255,255, ${state.tooltips.opacity / 100})`, color: 'black' }} />
-                  )}
                 </section>
 
                 {general.showSidebar && 'navigation' !== general.type && <Legend ref={legendRef} skipId={tabId} />}
