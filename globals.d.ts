24a0b22b1884a83b45141e03116cd9e1b5b80d9e
Lacey <pro7@cdc.gov>
2024-06-27 10:18:14 -0400

    unit testing pipeline

--- globals.d.ts
+++ globals.d.ts
@@ -1,3 +1,5 @@
 declare module '*.svg'
 
+declare module '*.svg?react'
+
 declare module '*.json'
--- packages/map/src/CdcMap.tsx
+++ packages/map/src/CdcMap.tsx
@@ -33,7 +33,7 @@ import colorPalettes from '@cdc/core/data/colorPalettes'
 import initialState from './data/initial-state'
 
 // Assets
-import ExternalIcon from './images/external-link.svg'
+import ExternalIcon from './images/external-link.svg?react'
 
 // Sass
 import './scss/main.scss'
