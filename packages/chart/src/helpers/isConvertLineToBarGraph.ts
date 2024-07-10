3548847c40a64a86012f0a617e239d01f110beba
Atash3000 <31108020+Atash3000@users.noreply.github.com>
2024-07-10 16:37:13 -0400

    [DEV-8667] Add filled circles to line effect (#1385)
    * [DEV-8667] Add Filled Circles to Line Effect
    
    * [DEV-8667] Update tooltip text
    
    * [DEV-8667] Update style
    
    * [DEV-8667] update const
    
    ---------
    
    Co-authored-by: Adam Doe <17502761+adamdoe@users.noreply.github.com>

--- packages/chart/src/helpers/isConvertLineToBarGraph.ts
+++ packages/chart/src/helpers/isConvertLineToBarGraph.ts
@@ -1,4 +1,4 @@
 export const isConvertLineToBarGraph = (visualizationType, filteredData, allowLineToBarGraph) => {
-  const convertLineToBarGraph = visualizationType === 'Line' && filteredData.length < 3 && allowLineToBarGraph ? true : false
+  const convertLineToBarGraph = visualizationType === 'Line' && filteredData?.length < 3 && allowLineToBarGraph ? true : false
   return convertLineToBarGraph
 }
