f0c6df11644263a492d0c18386fce0170eb9fc04
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-07-11 10:20:12 -0700

    [DEV-8312] add configure button to sankey setup steps (#1398)
    * [DEV-8312] Remove Data Designer Steps for Sankey
    
    * [DEV-8312] remove columns panel on sankey diagrams
    
    * [DEV-8312] add configure your data button to sankey

--- packages/editor/src/components/DataImport.tsx
+++ packages/editor/src/components/DataImport.tsx
@@ -439,11 +439,16 @@ export default function DataImport() {
 
   let configureData,
     readyToConfigure = false
+
   if (config.type === 'dashboard') {
     readyToConfigure = Object.keys(config.datasets).length > 0
   } else {
     configureData = config
     readyToConfigure = !!config.formattedData || (config.data && config.dataDescription && transform.autoStandardize(config.data))
+  }
+
+  if (config.visualizationType === 'Sankey' && config.data) {
+    readyToConfigure = true
   }
 
   // Box plots skip the data description steps.
