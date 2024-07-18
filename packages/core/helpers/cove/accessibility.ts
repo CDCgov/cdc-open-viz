30e88d5bcbf49450a686fad76920cd548cb5433a
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-07-18 14:36:36 -0700

    [DEV-8056] update wcag constants

--- packages/core/helpers/cove/accessibility.ts
+++ packages/core/helpers/cove/accessibility.ts
@@ -1,16 +1,18 @@
 import chroma from 'chroma-js'
 
 /**
- * WCAG 2.0 Standard requires 4.5:1 contrast ratio
- * https://www.w3.org/TR/WCAG20-TECHS/G18.html
+ * WCAG 2.1 Standard requires 4.5:1 contrast ratio
+ * TEXT: https://www.w3.org/TR/WCAG20-TECHS/G18.html
+ * NON-TEXT: https://www.w3.org/TR/WCAG20-TECHS/G145.html
  *
  * !important - DO NOT CHANGE.
  */
-export const WCAG_CONTRAST_RATIO = 4.5
+export const WCAG_TEXT_CONTRAST_RATIO = 4.5
+export const WCAG_NON_TEXT_CONTRAST_RATIO = 3.5
 
 export const getContrastColor = (textColor: string, bgColor: string) => {
   if (!bgColor) return
-  if (chroma.contrast(textColor, bgColor) < WCAG_CONTRAST_RATIO) {
+  if (chroma.contrast(textColor, bgColor) < WCAG_TEXT_CONTRAST_RATIO) {
     switch (textColor) {
       case '#FFF':
         return '#000'
@@ -25,5 +27,5 @@ export const getContrastColor = (textColor: string, bgColor: string) => {
 
 export const checkColorContrast = (color1: string, color2: string) => {
   if (!chroma.valid(color1) || !chroma.valid(color2)) return false
-  return chroma.contrast(color1, color2) >= WCAG_CONTRAST_RATIO
+  return chroma.contrast(color1, color2) >= WCAG_NON_TEXT_CONTRAST_RATIO
 }
