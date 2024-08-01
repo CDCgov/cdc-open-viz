d6cb64ffc90610a5146fd1de9106a9f33f086d19
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-08-01 10:31:34 -0700

    [DEV-8861] 4.24.9 > update versioning

---
+++ packages/core/helpers/ver/versionNeedsUpdate.ts
@@ -0,0 +1,8 @@
+export default function versionNeedsUpdate(previousVersion: string, currentVersion: string): boolean {
+  const [prevMajor, prevMinor, prevPatch] = previousVersion.split('.').map(Number)
+  const [currMajor, currMinor, currPatch] = currentVersion.split('.').map(Number)
+  if (currMajor > prevMajor) return true
+  if (currMajor === prevMajor && currMinor > prevMinor) return true
+  if (currMajor === prevMajor && currMinor === prevMinor && currPatch > prevPatch) return true
+  return false
+}
