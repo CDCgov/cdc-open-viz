002584ca7e0525b3d17f0dbf577e618714f49241
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-08-22 11:00:27 -0700

    [DEV-8677] 4.24.9 > Format cell values that are numbers with commas

--- packages/core/components/Table/components/Cell.tsx
+++ packages/core/components/Table/components/Cell.tsx
@@ -1,7 +1,22 @@
 const Cell = ({ children, style, isBold = false }) => {
+  const formatValue = (value: string | number) => {
+    // Check if the value is a number
+    const isNumber = (val: string | number): val is number => {
+      return typeof val === 'number' || !isNaN(Number(val))
+    }
+
+    // If the value is a number, format it with commas
+    if (isNumber(value)) {
+      return new Intl.NumberFormat().format(Number(value))
+    }
+
+    // Return the value as a string if it's not a number
+    return value.toString()
+  }
+
   return (
     <td tabIndex={0} role='gridcell' style={style}>
-      {isBold ? <strong>{children}</strong> : children}
+      {isBold ? <strong>{formatValue(children)}</strong> : formatValue(children)}
     </td>
   )
 }
