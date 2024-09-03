3dbbe3860e2f83e47eb5c6ee56d475f056d38058
Murad CDC <atamurad.babakulyyev@gmail.com>
2024-09-03 13:37:49 -0400

    [DEV-8215] Update Legend box

--- packages/core/components/Legend/Legend.Gradient.tsx
+++ packages/core/components/Legend/Legend.Gradient.tsx
@@ -3,9 +3,7 @@ import { Text } from '@visx/text'
 import { type ViewportSize, type MapConfig } from '@cdc/map/src/types/MapConfig'
 import { type ChartConfig } from '@cdc/chart/src/types/ChartConfig'
 import { getGradientLegendWidth } from '@cdc/core/helpers/getGradientLegendWidth'
-import { scaleLinear } from '@visx/scale'
-import { AxisBottom } from '@visx/axis'
-import { type DimensionsType } from '../../types/Dimensions'
+import { DimensionsType } from '../../types/Dimensions'
 
 type CombinedConfig = MapConfig | ChartConfig
 
@@ -16,7 +14,6 @@ interface GradientProps {
   dimensions: DimensionsType
   currentViewport: ViewportSize
   getTextWidth: (text: string, font: string) => string
-  values?: (number | string)[][]
 }
 
 const LegendGradient = ({
@@ -25,8 +22,7 @@ const LegendGradient = ({
   config,
   dimensions,
   currentViewport,
-  getTextWidth,
-  values
+  getTextWidth
 }: GradientProps): JSX.Element => {
   let [width] = dimensions
 
@@ -52,74 +48,30 @@ const LegendGradient = ({
     return <stop key={index} offset={`${offset}%`} style={{ stopColor: color, stopOpacity: 1 }} />
   })
 
-  const createTicks = () => {
-    if (config.legend.style !== 'gradient') return null
+  // render ticks and labels
+  const ticks = labels.map((key, index) => {
+    const segmentWidth = legendWidth / numTicks
+    const xPositionX = index * segmentWidth + segmentWidth
+    const textAnchor = rotationAngle ? 'end' : 'middle'
+    const verticalAnchor = rotationAngle ? 'middle' : 'start'
 
-    const generateTick = (xPosition, label, key, xPadding = 0) => {
-      const textAnchor = rotationAngle ? 'end' : 'middle'
-      const verticalAnchor = rotationAngle ? 'middle' : 'start'
-      return (
-        <Group top={margin} key={key}>
-          <line x1={xPosition} x2={xPosition} y1={30} y2={boxHeight} stroke='#000' />
-          <Text
-            angle={-config.legend.tickRotation}
-            x={xPosition}
-            y={boxHeight}
-            dy={10}
-            dx={-xPadding}
-            fontSize='14'
-            textAnchor={textAnchor}
-            verticalAnchor={verticalAnchor}
-          >
-            {label}
-          </Text>
-        </Group>
-      )
-    }
-
-    if (config.type === 'map' && config.legend.subStyle === 'smooth' && config.legend.type === 'category') {
-      // const uniqueValues = Array.from(new Set(values.flat().map(Number)))
-      const uniqueValues = Array.from(new Set(labels.map(Number)))
-      console.log(uniqueValues, 'uniqueValues')
-      console.log(labels, 'LABELS')
-      const scale = scaleLinear({
-        domain: [Math.min(...uniqueValues), Math.max(...uniqueValues)],
-        range: [0, legendWidth]
-      })
-      return uniqueValues.slice(0, colors.length).map((value, index) => {
-        const xPosition = scale(value)
-        return generateTick(xPosition, value, `map-${index}`)
-      })
-
-      // return values.map((range, index) => {
-      //   const [start, end] = range.map(Number)
-      //   const xPosition = (scale(start) + scale(end)) / 2
-      //   return generateTick(xPosition, labels[index], `map-${index}`)
-      // })
-    }
-
-    if (config.type === 'chart' && config.legend.subStyle === 'smooth') {
-      const segmentWidth = legendWidth / numTicks
-      return labels.map((key, index) => {
-        const xPosition = index * segmentWidth + segmentWidth / 2
-        return generateTick(xPosition, key, `chart-${index}`, 0)
-      })
-    }
-
-    if (config.legend.subStyle === 'linear blocks') {
-      return labels.map((label, index) => {
-        const segmentWidth = legendWidth / numTicks
-        const xPosition = index * segmentWidth + segmentWidth
-        const textPadding = segmentWidth / 2
-        return generateTick(xPosition, label, `map-${index}`, textPadding)
-      })
-    }
-
-    return null
-  }
-
-  const ticks = createTicks()
-
+    return (
+      <Group top={margin}>
+        <line x1={xPositionX} x2={xPositionX} y1={30} y2={boxHeight} stroke='black' />
+        <Text
+          angle={-config.legend.tickRotation}
+          x={xPositionX}
+          y={boxHeight}
+          dy={10}
+          fontSize='14'
+          textAnchor={textAnchor}
+          verticalAnchor={verticalAnchor}
+        >
+          {key}
+        </Text>
+      </Group>
+    )
+  })
   if ((config.type === 'map' && config.legend.position === 'side') || !config.legend.position) {
     return
   }
@@ -129,13 +81,6 @@ const LegendGradient = ({
   ) {
     return
   }
-
-  const uniqueValues = Array.from(new Set(labels.map(Number)))
-
-  const xScale = scaleLinear({
-    domain: [Math.min(...uniqueValues), Math.max(...uniqueValues)],
-    range: [0, legendWidth]
-  })
 
   if (config.legend.style === 'gradient') {
     return (
@@ -158,33 +103,6 @@ const LegendGradient = ({
           <rect x={1} y={1} width={legendWidth} height={boxHeight} fill={`url(#gradient-smooth-${config.uid || 0})`} />
         )}
 
-        <AxisBottom scale={xScale} top={10} numTicks={uniqueValues.length} tickStroke='black'>
-          {({ ticks, tickLabelProps }) => (
-            <Group top={0}>
-              {ticks.map((tick, i) => {
-                const textAnchor = rotationAngle ? 'end' : 'middle'
-                const verticalAnchor = rotationAngle ? 'middle' : 'start'
-                return (
-                  <Group key={`tick-${tick.value}`} transform={`translate(${tick.to.x},${tick.to.y})`}>
-                    <line stroke='black' y2={0} y1={28} x1={0} x2={0} />
-                    <Text
-                      angle={-rotationAngle}
-                      x={0}
-                      y={boxHeight}
-                      dy='0'
-                      fontSize='14'
-                      textAnchor={textAnchor}
-                      verticalAnchor={verticalAnchor}
-                    >
-                      {tick.formattedValue}
-                    </Text>
-                  </Group>
-                )
-              })}
-            </Group>
-          )}
-        </AxisBottom>
-
         {config.legend.subStyle === 'linear blocks' &&
           colors.map((color, index) => {
             const segmentWidth = legendWidth / numTicks
@@ -205,7 +123,7 @@ const LegendGradient = ({
             )
           })}
         {/* Ticks and labels */}
-        {/* <g>{ticks}</g> */}
+        <g>{ticks}</g>
       </svg>
     )
   }
