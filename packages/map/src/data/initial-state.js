c921c1a102f0a6280c74b71bf7997d829d53a33a
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-09-04 09:06:58 -0700

    4.24.9 > Updates for no location found

--- packages/map/src/data/initial-state.js
+++ packages/map/src/data/initial-state.js
@@ -1,6 +1,7 @@
 export default {
   annotations: [],
   general: {
+    noStateFoundMessage: 'Map Unavailable',
     annotationDropdownText: 'Annotations',
     geoBorderColor: 'darkGray',
     headerColor: 'theme-blue',
@@ -91,8 +92,7 @@ export default {
     opacity: 90
   },
   runtime: {
-    editorErrorMessage: [],
-    noStateFoundMessage: 'No state image found'
+    editorErrorMessage: []
   },
   visual: {
     minBubbleSize: 1,
