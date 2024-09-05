e5c31008ad666373faddb973dc648d4d20676606
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-09-05 12:51:24 -0700

    [dev] update close icon on maps

--- packages/map/src/components/Modal.tsx
+++ packages/map/src/components/Modal.tsx
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react'
-import closeIcon from '../images/close.svg'
+import { useContext } from 'react'
 import LegendShape from '@cdc/core/components/LegendShape'
 import ConfigContext from '../context'
+import Icon from '@cdc/core/components/ui/Icon'
 
 const Modal = () => {
   const { applyTooltipsToGeo, capitalize, applyLegendToRow, viewport, type, content } = useContext(ConfigContext)
@@ -11,10 +11,13 @@ const Modal = () => {
   const legendColors = applyLegendToRow(content.keyedData)
 
   return (
-    <section className={capitalize ? 'modal-content tooltip capitalize ' + viewport : 'modal-content tooltip ' + viewport} aria-hidden='true'>
-      <img src={closeIcon} className='modal-close' alt='Close Modal' />
+    <section
+      className={capitalize ? 'modal-content tooltip capitalize ' + viewport : 'modal-content tooltip ' + viewport}
+      aria-hidden='true'
+    >
       {type === 'data' && <LegendShape fill={legendColors[0]} />}
       <div className='content'>{tooltip}</div>
+      <Icon display='close' alt='Close Modal' size={20} color='#000' className='modal-close' />
     </section>
   )
 }
