4a3576a37fa0957fc952746b4cb03ecd8633e1e8
Lacey <pro7@cdc.gov>
2024-09-11 15:26:47 -0400

    added fix for tooltips

---
+++ packages/core/components/ui/Tooltip.tsx
@@ -0,0 +1,74 @@
+import { useId } from 'react'
+
+// Third Party
+import { Tooltip as ReactTooltip } from 'react-tooltip'
+import type { PlacesType } from 'react-tooltip'
+
+// Styles
+import '../../styles/v2/components/ui/tooltip.scss'
+
+const TooltipTarget = children => children
+const TooltipContent = children => children
+
+type ToolTipProps = {
+  place?: PlacesType
+  trigger?: 'hover' | 'focus' | 'click'
+  float?: boolean
+  shadow?: boolean
+  border?: boolean
+  children: React.ReactElement[]
+  style?: React.CSSProperties
+}
+
+const Tooltip: React.FC<ToolTipProps> & { Target; Content } = ({
+  place = 'top',
+  trigger = 'hover',
+  float = false,
+  shadow = true,
+  border = false,
+  children,
+  style,
+  ...attributes
+}) => {
+  const tooltipTargetChildren = children.find(el => el.type === TooltipTarget)
+  const tooltipContentChildren = children.find(el => el.type === TooltipContent)
+
+  const uid = 'tooltip-' + useId()
+
+  return (
+    <span className='cove-tooltip' style={style} {...attributes}>
+      <a
+        id={uid}
+        role='dialog'
+        tabIndex={0}
+        className='cove-tooltip--target'
+        data-tooltip-float={float}
+        data-tooltip-place={place}
+        data-tooltip-isOpen={true}
+      >
+        {tooltipTargetChildren ? tooltipTargetChildren.props.children : null}
+      </a>
+      <ReactTooltip
+        id={uid}
+        isOpen={true}
+        // ! do not remove the deprecated property, it will break tooltips in the editor.
+        anchorId={uid}
+        className={
+          'cove-tooltip__content' +
+          (' place-' + place) +
+          (!float ? ' cove-tooltip__content--animated' : '') +
+          (trigger === 'click' ? ' interactive' : '') +
+          (border ? ' cove-tooltip--border' : '') +
+          (shadow ? ' has-shadow' : '')
+        }
+      >
+        {tooltipContentChildren ? tooltipContentChildren.props.children : null}
+      </ReactTooltip>
+    </span>
+  )
+}
+
+Tooltip.Target = TooltipTarget
+Tooltip.Content = TooltipContent
+
+export default Tooltip
