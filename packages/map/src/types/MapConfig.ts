ae69244e917807e954c11cbbb09a254cdfa4665c
Monroe Getz <mgetz10@gmail.com>
2024-09-18 13:24:41 -0400

    revert: replace noStateFoundMessage

--- packages/map/src/types/MapConfig.ts
+++ packages/map/src/types/MapConfig.ts
@@ -149,7 +149,9 @@ export type MapConfig = Visualization & {
     opacity: number
   }
   runtime: {
-    editorErrorMessage: string[]
+    editorErrorMessage: string[],
+    // when a single state map doesn't include a fips code show a message...
+    noStateFoundMessage: string
   }
   mapPosition: { coordinates: Coordinate; zoom: number }
   map: {
