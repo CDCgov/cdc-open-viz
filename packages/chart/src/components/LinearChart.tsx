2b453d87dd40d388a28b457410a5b72f66665367
Jay Boice <jboice@gmail.com>
2024-09-23 12:16:00 -0400

    [dev-9287] lighten grid (#1532)
    * Lighten grid
    
    * Format with prettier

--- packages/chart/src/components/LinearChart.tsx
+++ packages/chart/src/components/LinearChart.tsx
@@ -559,7 +559,7 @@ const LinearChart: React.FC<LinearChartProps> = props => {
                               display={(isLogarithmicAxis && showTicks).toString()}
                               from={{ x: tick.from.x + xMax, y: tick.from.y }}
                               to={tick.from}
-                              stroke='rgba(0,0,0,0.3)'
+                              stroke='#d6d6d6'
                             />
                           ) : (
                             ''
@@ -784,11 +784,7 @@ const LinearChart: React.FC<LinearChartProps> = props => {
                           )}
 
                           {runtime.yAxis.rightGridLines ? (
-                            <Line
-                              from={{ x: tick.from.x + xMax, y: tick.from.y }}
-                              to={tick.from}
-                              stroke='rgba(0,0,0,0.3)'
-                            />
+                            <Line from={{ x: tick.from.x + xMax, y: tick.from.y }} to={tick.from} stroke='#d6d6d6' />
                           ) : (
                             ''
                           )}
