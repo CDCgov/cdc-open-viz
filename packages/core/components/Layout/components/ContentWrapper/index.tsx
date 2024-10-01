b8294796643eefa5fad5334c37c24f1a726dba91
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-10-01 15:29:16 -0700

    [layout-updates] add contentwrapper types

--- packages/core/components/Layout/components/ContentWrapper/index.tsx
+++ packages/core/components/Layout/components/ContentWrapper/index.tsx
@@ -1,7 +1,14 @@
 import useDataVizClasses from './useDataVizClasses.ts'
 
+type ContentWrapperProps = {
+  config: any
+  ariaLabel: string
+  children: React.ReactNode
+  viewport: any
+}
+
 // All content under the title/header in the visualization
-const ContentWrapper = ({ config, ariaLabel, missingRequiredSections = () => false, newViz, children, viewport }) => {
+const ContentWrapper: React.FC<ContentWrapperProps> = ({ config, ariaLabel, children, viewport }) => {
   const { contentClasses } = useDataVizClasses(config, viewport)
   return (
     <div className={contentClasses.join(' ')} aria-label={ariaLabel} tabIndex={0}>
