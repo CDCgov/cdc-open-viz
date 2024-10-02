7a89cd9598753c92d4f01c86bac3106c04cb259d
Lacey <pro7@cdc.gov>
2024-10-02 14:52:46 -0400

    enhancement dev-8930 transpose table

--- packages/core/components/DataTable/helpers/getDataSeriesColumns.ts
+++ packages/core/components/DataTable/helpers/getDataSeriesColumns.ts
@@ -56,6 +56,10 @@ export const getDataSeriesColumns = (config: TableConfig, isVertical: boolean, r
     }
   })
 
-  tmpSeriesColumns.sort((a, b) => columnOrderingHash[a] - columnOrderingHash[b])
+  tmpSeriesColumns.sort((a, b) => {
+    if (a === 'pivotColumn') return -1
+    if (b === 'pivotColumn') return 1
+    return columnOrderingHash[a] - columnOrderingHash[b]
+  })
   return tmpSeriesColumns
 }
