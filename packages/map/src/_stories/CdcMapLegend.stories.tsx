0f9ac1a1a2e6a672093b9d723969b27482a787b3
Monroe <35408073+Mgetz10@users.noreply.github.com>
2024-10-31 11:49:28 -0400

    [DEV-9652] fix legend border off center (#1621)

---
+++ packages/map/src/_stories/CdcMapLegend.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import CdcMap from '../CdcMap'
+import SingleStateWithFilters from './_mock/DEV-8942.json'
+import UsGradient from './_mock/usa-state-gradient.json'
+import { editConfigKeys } from '@cdc/chart/src/helpers/configHelpers'
+
+const meta: Meta<typeof CdcMap> = {
+  title: 'Components/Templates/Map/Legend',
+  component: CdcMap
+}
+
+type Story = StoryObj<typeof CdcMap>
+
+export default meta
+
+export const Single_State_With_Filters: Story = {
+  args: {
+    config: editConfigKeys(SingleStateWithFilters, [
+      { path: ['legend', 'style'], value: 'gradient' },
+      { path: ['legend', 'position'], value: 'top' },
+      { path: ['legend', 'hideBorder'], value: true },
+      { path: ['legend', 'title'], value: '' },
+      { path: ['legend', 'description'], value: '' }
+    ])
+  }
+}
+
+export const US_State_Gradient: Story = {
+  args: {
+    config: UsGradient
+  }
+}
+
+export const US_State_Gradient_Smooth: Story = {
+  args: {
+    config: editConfigKeys(UsGradient, [{ path: ['legend', 'subStyle'], value: 'smooth' }])
+  }
+}
