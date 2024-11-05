6909c99dab1f3e382e96f52f55cabdb9c64f00b1
Murad CDC <atamurad.babakulyyev@gmail.com>
2024-11-05 16:09:20 -0500

    [DEV-9689] Add storybook

---
+++ packages/chart/src/_stories/ChartLine.Suppression.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Chart from '../CdcChart'
+import suppressionConfig from './_mock/suppression_mock.json'
+import SuppressedConfig from './_mock/bar-chart-suppressed.json'
+const meta: Meta<typeof Chart> = {
+  title: 'Components/Templates/Chart/Suppression',
+  component: Chart
+}
+
+type Story = StoryObj<typeof Chart>
+
+export const SuppressedLines: Story = {
+  args: {
+    config: suppressionConfig,
+    isEditor: false
+  }
+}
+export const SuppressedBars: Story = {
+  args: {
+    config: SuppressedConfig,
+    isEditor: false
+  }
+}
+
+export default meta
