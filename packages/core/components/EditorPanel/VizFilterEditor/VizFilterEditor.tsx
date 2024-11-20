9ebe77f39e1927c833de1b79567153beb24b57e0
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-11-20 07:48:48 -0800

    [DEV-9564] When filters are hidden hide the intro text (#1661)

--- packages/core/components/EditorPanel/VizFilterEditor/VizFilterEditor.tsx
+++ packages/core/components/EditorPanel/VizFilterEditor/VizFilterEditor.tsx
@@ -67,7 +67,12 @@ const VizFilterEditor: React.FC<VizFilterProps> = ({ config, updateField, rawDat
 
   const addNewFilter = () => {
     const filters = config.filters ? [...config.filters] : []
-    const newVizFilter: VizFilter = { values: [], filterStyle: 'dropdown', id: Date.now() } as VizFilter
+    const newVizFilter: VizFilter = {
+      values: [],
+      filterStyle: 'dropdown',
+      id: Date.now(),
+      showDropdown: true
+    } as VizFilter
     filters.push(newVizFilter)
     updateField(null, null, 'filters', filters)
   }
