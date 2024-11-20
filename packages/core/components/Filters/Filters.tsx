f5f4f9b7942012c2ef352db18fd1110ec2dcdb23
Josh Lacey <williamjoshualacey@gmail.com>
2024-11-20 13:55:46 -0500

    Revert "Fix: Dev-9864 Duplicate Dropdown label in Dashboard (#1702)"
    This reverts commit eddc3a772e4f11474f3784fee5e7360861523943.

--- packages/core/components/Filters/Filters.tsx
+++ packages/core/components/Filters/Filters.tsx
@@ -470,6 +470,7 @@ const Filters = (props: FilterProps) => {
                 activeSubGroup={(singleFilter.subGrouping?.active as string) || (singleFilter.queuedActive || [])[1]}
                 filterIndex={outerIndex}
                 options={getNestedOptions(singleFilter)}
+                listLabel={label}
                 handleSelectedItems={value => changeFilterActive(outerIndex, value)}
               />
             )}
