5f6e3adbb0d59045592b515f3c928d782bdbcd09
Monroe <35408073+Mgetz10@users.noreply.github.com>
2024-11-22 13:37:29 -0500

    fix: make arrow functions

--- packages/core/helpers/viewports.ts
+++ packages/core/helpers/viewports.ts
@@ -2,14 +2,9 @@ import { ViewportSize } from '@cdc/map/src/types/MapConfig'
 
 const BREAKPOINTS = ['xxs', 'xs', 'sm', 'md', 'lg']
 
-export function isBelowBreakpoint(breakpoint: ViewportSize, currentViewport: ViewportSize) {
-  return BREAKPOINTS.indexOf(currentViewport) < BREAKPOINTS.indexOf(breakpoint)
-}
+export const isBelowBreakpoint = (breakpoint: ViewportSize, currentViewport: ViewportSize) =>
+  BREAKPOINTS.indexOf(currentViewport) < BREAKPOINTS.indexOf(breakpoint)
 
-export function isLegendWrapViewport(currentViewport) {
-  return isBelowBreakpoint('sm', currentViewport)
-}
+export const isLegendWrapViewport = currentViewport => isBelowBreakpoint('sm', currentViewport)
 
-export function isMobileHeightViewport(currentViewport) {
-  return isBelowBreakpoint('sm', currentViewport)
-}
+export const isMobileHeightViewport = currentViewport => isBelowBreakpoint('sm', currentViewport)
