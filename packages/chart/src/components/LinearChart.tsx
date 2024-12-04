68f2dddd05c3341705557eb9f6a671504394d8fb
Monroe <35408073+Mgetz10@users.noreply.github.com>
2024-12-04 11:32:38 -0500

    Fix [DEV-9961] no data throws bug (#1738)
    * fix: no data throws bug
    
    * enhancement: dont run if not date time
    
    * enhancement: refactor calculation to improve readability

--- packages/chart/src/components/LinearChart.tsx
+++ packages/chart/src/components/LinearChart.tsx
@@ -52,6 +52,7 @@ type LinearChartProps = {
 const BOTTOM_LABEL_PADDING = 9
 const X_TICK_LABEL_PADDING = 3
 const DEFAULT_TICK_LENGTH = 8
+const MONTH_AS_MS = 1000 * 60 * 60 * 24 * 30
 
 const LinearChart = forwardRef<SVGAElement, LinearChartProps>(({ parentHeight, parentWidth }, svgRef) => {
   // prettier-ignore
@@ -240,9 +241,11 @@ const LinearChart = forwardRef<SVGAElement, LinearChartProps>(({ parentHeight, p
       hideTooltip
   })
   // get the number of months between the first and last date
+  const { dataKey } = runtime.xAxis
   const dateSpanMonths =
-    [0, data.length - 1].map(i => parseDate(data[i][runtime.xAxis.dataKey])).reduce((a, b) => Math.abs(a - b), 0) /
-    (1000 * 60 * 60 * 24 * 30)
+    data.length && isDateTime
+      ? [0, data.length - 1].map(i => parseDate(data[i][dataKey])).reduce((a, b) => Math.abs(a - b)) / MONTH_AS_MS
+      : 0
   const useDateSpanMonths = isDateTime && dateSpanMonths > xTickCount
 
   // GETTERS & FUNCTIONS
