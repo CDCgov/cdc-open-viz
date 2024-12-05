47464525d0b529bc1215e6cf704e1a576f3140f8
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2024-12-05 10:34:02 -0800

    [DEV-9840-2] DEV-9840 > Use dynamic data series

--- packages/chart/src/components/EditorPanel/EditorPanel.tsx
+++ packages/chart/src/components/EditorPanel/EditorPanel.tsx
@@ -1497,7 +1497,8 @@ const EditorPanel = () => {
                             </Panels.Series.Wrapper>
                           )}
                         </>
-                        {config.series && config.series.length <= 1 && config.visualizationType === 'Bar' && (
+                        {((config.series && config.series.length <= 1 && config.visualizationType === 'Bar') ||
+                          (config.series && config.series.length <= 1 && config.visualizationType === 'Line')) && (
                           <>
                             <span className='divider-heading'>Confidence Keys</span>
                             <Select
