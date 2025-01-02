12897f7b72c16bb1bdfa23670d2be044d92f95ad
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2025-01-02 13:12:48 -0800

    [DEV-9163] Updates for dom-to-image on scatter plot (#1672)
    * [DEV-9163] 4.24.11 > Updates for dom-to-image on scatter plot
    
    * [DEV-9163] add mock data

---
+++ packages/chart/src/_stories/Chart.ScatterPlot.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Chart from '../CdcChart'
+import scatterPlotDownloadImage from './_mock/scatterplot-image-download.json'
+
+const meta: Meta<typeof Chart> = {
+  title: 'Components/Templates/Chart/Scatter Plot Download Image',
+  component: Chart
+}
+
+type Story = StoryObj<typeof Chart>
+
+export const ScatterPlot_Download_Image: Story = {
+  args: {
+    config: scatterPlotDownloadImage,
+    isEditor: false
+  }
+}
+
+export default meta
