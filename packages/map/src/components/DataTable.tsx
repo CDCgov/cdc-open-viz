5e0600ac99e8f2bc47f2c6f0b0c237ad6f894f70
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2025-01-08 09:17:26 -0800

    [resize-observer] fix resize observer issue (#1818)
    * [resize-observer] fix resize observer issue
    
    * [resize-observer] resolve color update issue

--- packages/map/src/components/DataTable.tsx
+++ packages/map/src/components/DataTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, memo } from 'react'
+import React, { useEffect, useState, memo, useContext } from 'react'
 
 import Papa from 'papaparse'
 import ExternalIcon from '../images/external-link.svg' // TODO: Move to Icon component
@@ -11,7 +11,7 @@ import SkipTo from '@cdc/core/components/elements/SkipTo'
 
 import Loading from '@cdc/core/components/Loading'
 import { navigationHandler } from '../helpers/navigationHandler'
-import useResizeObserver from '../hooks/useResizeObserver'
+import ConfigContext from '../context'
 
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex, jsx-a11y/no-static-element-interactions */
 const DataTable = props => {
@@ -33,10 +33,10 @@ const DataTable = props => {
     setFilteredCountryCode
   } = props
 
+  const { currentViewport: viewport } = useContext(ConfigContext)
   const [expanded, setExpanded] = useState(expandDataTable)
   const [sortBy, setSortBy] = useState({ column: 'geo', asc: false })
   const [accessibilityLabel, setAccessibilityLabel] = useState('')
-  const { currentViewport: viewport } = useResizeObserver(false)
   const fileName = `${mapTitle || 'data-table'}.csv`
 
   // Catch all sorting method used on load by default but also on user click
