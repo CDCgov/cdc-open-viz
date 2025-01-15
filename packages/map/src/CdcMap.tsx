8ba513a992731290f7bca5dc8ecf7a06fd0a87be
Josh Lacey <williamjoshualacey@gmail.com>
2025-01-15 10:56:11 -0500

    Patch fix/dev-10201 debug (#1836)
    * patch fix debugging dev-10201
    
    * patch fix debugging dev-10201

--- packages/map/src/CdcMap.tsx
+++ packages/map/src/CdcMap.tsx
@@ -917,7 +917,7 @@ const CdcMap = ({
         newFilter.active = active ?? values[0] // Default to first found value
         newFilter.filterStyle = obj.filters[idx].filterStyle ? obj.filters[idx].filterStyle : 'dropdown'
         newFilter.showDropdown = showDropdown
-        newFilter.subGrouping = obj.filters[idx].subGrouping || {}
+        newFilter.subGrouping = obj.filters[idx].subGrouping
 
         filters.push(newFilter)
       }
@@ -1568,7 +1568,7 @@ const CdcMap = ({
     const hashFilters = hashObj(state.filters)
     let filters
 
-    if (state.filters && hashFilters !== runtimeFilters.fromHash) {
+    if (state.filters && (config || hashFilters !== runtimeFilters.fromHash)) {
       filters = generateRuntimeFilters(state, hashFilters, runtimeFilters)
 
       if (filters) {
