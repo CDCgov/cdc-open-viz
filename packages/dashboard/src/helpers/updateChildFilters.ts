0904d7db99bd6f2498ad2c618b9643b96f6a0b54
Murad CDC <atamurad.babakulyyev@gmail.com>
2025-01-16 16:37:06 -0500

    [DEV-10046a] update filters

--- packages/dashboard/src/helpers/updateChildFilters.ts
+++ packages/dashboard/src/helpers/updateChildFilters.ts
@@ -3,28 +3,33 @@ import _ from 'lodash'
 
 export const updateChildFilters = (newSharedFilters: SharedFilter[], data: Record<string, any>): SharedFilter[] => {
   const dataSet = Object.values(data).flat()
+  // filter out Data type filters
+  const filters = newSharedFilters.filter(filter => filter.type === 'datafilter')
   // Find indexes of all child filters
-  const childFilterIndexes: number[] = newSharedFilters
+  const childFilterIndexes: number[] = filters
     .map((filter, index) => (filter.parents ? index : -1))
     .filter(index => index !== -1)
-
   if (childFilterIndexes.length === 0) return newSharedFilters
 
   // deep copy of the shared filters
-  const updatedFilters = _.cloneDeep(newSharedFilters)
+  const updatedFilters = _.cloneDeep(filters)
 
   // Update each child filter
   childFilterIndexes.forEach(childIndex => {
-    const childFilter: SharedFilter = newSharedFilters[childIndex]
-    const parentFilter: SharedFilter = newSharedFilters.find(
-      filter => String(childFilter.parents) === String(filter.key)
-    )
+    const childFilter: SharedFilter = filters[childIndex]
+    const parentFilter: SharedFilter = filters.find(filter => String(childFilter.parents) === String(filter.key))
+    const isParentMultiSelect = parentFilter.filterStyle === 'multi-select'
 
     if (parentFilter) {
       // Filter dataset based on parent's active value
-      const parentsActiveValues: string[] = dataSet.filter((d: Record<string, any>) =>
-        parentFilter.active.includes(d[parentFilter.columnName])
-      )
+      const parentsActiveValues: string[] = dataSet.filter((d: Record<string, any>) => {
+        if (isParentMultiSelect) {
+          // If multi-select, check if the parent active array includes the value
+          return parentFilter.active.includes(d[parentFilter.columnName])
+        } else {
+          return parentFilter.active === d[parentFilter.columnName]
+        }
+      })
       // Get unique active values for the child filter
       const uniqChildValues = _.uniq(parentsActiveValues.map(d => d[childFilter.columnName]).filter(Boolean))
 
