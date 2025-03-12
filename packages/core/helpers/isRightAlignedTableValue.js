6d96267e9c853a36d8700640d543be02bd59dd10
Jay Boice <jboice@gmail.com>
2025-03-12 14:32:06 -0400

    Don't consider dates to be numbers for table alignment

--- packages/core/helpers/isRightAlignedTableValue.js
+++ packages/core/helpers/isRightAlignedTableValue.js
@@ -8,6 +8,10 @@ export default function isRightAlignedTableValue(value = '') {
     return !Number.isNaN(value)
   }
   if (typeof value === 'string') {
+    // Dates are not considered numbers
+    if (/^\d{4}-\d{2}-\d{2}$/.test(value) || /^\d{1,2}\/\d{1,2}\/\d{2,4}$/.test(value)) {
+      return false
+    }
     return numericStrings.includes(value) || /^[\$\d\.\%\,\-\s\(\)CI]*$/.test(value)
   }
   return false
