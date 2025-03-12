28be22567ca87f1376713516c6b4db059eecc3bb
Jay Boice <jboice@gmail.com>
2025-03-12 14:47:38 -0400

    Adjust regex

--- packages/core/helpers/isRightAlignedTableValue.js
+++ packages/core/helpers/isRightAlignedTableValue.js
@@ -8,8 +8,8 @@ export default function isRightAlignedTableValue(value = '') {
     return !Number.isNaN(value)
   }
   if (typeof value === 'string') {
-    // Dates are not considered numbers
-    if (/^\d{4}-\d{2}-\d{2}$/.test(value) || /^\d{1,2}\/\d{1,2}\/\d{2,4}$/.test(value)) {
+    // Dates like 2024-09-12 are not considered numbers
+    if (/^\d{4}-\d{1,2}-\d{1,2}$/.test(value)) {
       return false
     }
     return numericStrings.includes(value) || /^[\$\d\.\%\,\-\s\(\)CI]*$/.test(value)
