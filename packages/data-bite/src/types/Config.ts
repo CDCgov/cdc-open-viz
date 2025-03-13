c8b14cfff01bf4caaf1f92a4494bf3d3499ad952
Atash3000 <31108020+Atash3000@users.noreply.github.com>
2025-03-13 11:34:44 -0400

    [DEV-10447a] update show title on Data bite (#1991)

--- packages/data-bite/src/types/Config.ts
+++ packages/data-bite/src/types/Config.ts
@@ -34,7 +34,7 @@ export type Config = {
     background: boolean
     hideBackgroundColor: boolean
     borderColorTheme: boolean
-    hideTitle: boolean
+    showTitle: boolean
   }
   general: {
     isCompactStyle: boolean
