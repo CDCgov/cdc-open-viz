4481bf230fe1aa9736199ac4c91520ea17f35142
Monroe <35408073+Mgetz10@users.noreply.github.com>
2025-03-26 15:48:59 -0400

    fix: dashboard dynamic category issue

--- packages/chart/src/helpers/dataHelpers.ts
+++ packages/chart/src/helpers/dataHelpers.ts
@@ -3,8 +3,17 @@ import { ChartConfig } from '../types/ChartConfig'
 export const getSeriesWithData = (config: ChartConfig) => {
   const { filters, data, runtime } = config
   const { series } = runtime
-
   const filteredData = data.filter(d => filters.every(f => d[f.columnName] === f.active))
 
-  return series.filter(s => filteredData.some(d => d[s.dynamicCategory || s.dataKey])).map(s => s.name || s.dataKey)
+  const result = series
+    .map(s => ({
+      ...s,
+      data: filteredData
+        .filter(d => (s.dynamicCategory ? d[s.dynamicCategory || s.dataKey] === s.dataKey : d[s.dataKey]))
+        .filter(d => !s.dynamicCategory || d[s.originalDataKey])
+    }))
+    .filter(s => s.data.length)
+    .map(s => s.name || s.dataKey)
+
+  return result
 }
