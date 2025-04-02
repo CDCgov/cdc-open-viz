a1beb85e2a162b6dac11df9fb6e5df2c43211080
Jay Boice <jboice@gmail.com>
2025-04-02 10:32:58 -0400

    Use constant for app font color

--- packages/core/helpers/constants.ts
+++ packages/core/helpers/constants.ts
@@ -1,6 +1,7 @@
 export const APP_FONT_SIZE = 18
 
 export const COOL_GRAY_90 = getComputedStyle(document.documentElement).getPropertyValue('--cool-gray-90').trim()
-export const APP_FONT_COLOR = COOL_GRAY_90
+//export const APP_FONT_COLOR = COOL_GRAY_90
+export const APP_FONT_COLOR = '#1C1D1F'
 
 export const EDITOR_WIDTH = 350
