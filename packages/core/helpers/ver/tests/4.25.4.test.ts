787a810f4ab46f475e1c52adade81b2ea40a3ef0
Monroe <35408073+Mgetz10@users.noreply.github.com>
2025-04-10 11:02:14 -0400

    fix [coveUpdateWorker] remove redundant multidashboard updater (#2065)
    * fix: remove redundant multidashboard updater
    
    * fix: remove multidashboard test

--- packages/core/helpers/ver/tests/4.25.4.test.ts
+++ packages/core/helpers/ver/tests/4.25.4.test.ts
@@ -2,7 +2,6 @@ import { expect, describe, it } from 'vitest'
 import { makeChartLegendsUnified } from '../4.25.4'
 import { ChartConfig } from '@cdc/chart/src/types/ChartConfig'
 import { DashboardConfig } from '@cdc/dashboard/src/types/DashboardConfig'
-import { MultiDashboard, MultiDashboardConfig } from '@cdc/dashboard/src/types/MultiDashboard'
 
 describe('makeChartLegendsUnified(config) ', () => {
   it('sets chart legends unified to true', () => {
@@ -21,29 +20,5 @@ describe('makeChartLegendsUnified(config) ', () => {
     makeChartLegendsUnified(mockConfig)
     expect(mockConfig.visualizations['1'].legend?.unified).toBe(true)
     expect(mockConfig.visualizations['2'].legend?.unified).toBe(true)
-  })
-  it('sets multiDashboard nested chart legends unified to true', () => {
-    const mockConfig = {
-      type: 'dashboard',
-      multiDashboards: [
-        {
-          visualizations: {
-            '1': { type: 'chart', legend: { unified: false } },
-            '2': { type: 'chart', legend: { unified: false } }
-          }
-        },
-        {
-          visualizations: {
-            '3': { type: 'chart', legend: { unified: false } },
-            '4': { type: 'chart', legend: { unified: false } }
-          }
-        }
-      ] as unknown as MultiDashboard[]
-    } as Partial<MultiDashboardConfig>
-    makeChartLegendsUnified(mockConfig)
-    expect(mockConfig.multiDashboards[0].visualizations['1'].legend?.unified).toBe(true)
-    expect(mockConfig.multiDashboards[0].visualizations['2'].legend?.unified).toBe(true)
-    expect(mockConfig.multiDashboards[1].visualizations['3'].legend?.unified).toBe(true)
-    expect(mockConfig.multiDashboards[1].visualizations['4'].legend?.unified).toBe(true)
   })
 })
