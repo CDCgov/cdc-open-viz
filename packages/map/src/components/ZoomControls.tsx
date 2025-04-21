f15adad1455a58cb5df09d3390b61bcc8e640f95
adamdoe <17502761+adamdoe@users.noreply.github.com>
2025-04-21 13:21:16 -0700

    Updates for switching "state" to "config"

--- packages/map/src/components/ZoomControls.tsx
+++ packages/map/src/components/ZoomControls.tsx
@@ -11,8 +11,8 @@ type ZoomControlsProps = {
 }
 
 const ZoomControls: React.FC<ZoomControlsProps> = ({ handleZoomIn, handleZoomOut, handleReset }) => {
-  const { state, setState, setRuntimeData, position } = useContext<MapContext>(ConfigContext)
-  if (!state.general.allowMapZoom) return
+  const { config, setState, setRuntimeData, position } = useContext<MapContext>(ConfigContext)
+  if (!config.general.allowMapZoom) return
   return (
     <div className='zoom-controls' data-html2canvas-ignore='true'>
       <button onClick={() => handleZoomIn(position)} aria-label='Zoom In'>
@@ -26,17 +26,17 @@ const ZoomControls: React.FC<ZoomControlsProps> = ({ handleZoomIn, handleZoomOut
           <line x1='5' y1='12' x2='19' y2='12' />
         </svg>
       </button>
-      {state.general.type === 'bubble' && (
+      {config.general.type === 'bubble' && (
         <button
-          onClick={() => handleReset(state, setState, setRuntimeData)}
+          onClick={() => handleReset(config, setState, setRuntimeData)}
           className='reset'
           aria-label='Reset Zoom and Map Filters'
         >
           Reset Filters
         </button>
       )}
-      {(state.general.type === 'world-geocode' || state.general.geoType === 'single-state') && (
-        <button onClick={() => handleReset(state, setState, setRuntimeData)} className='reset' aria-label='Reset Zoom'>
+      {(config.general.type === 'world-geocode' || config.general.geoType === 'single-state') && (
+        <button onClick={() => handleReset(config, setState, setRuntimeData)} className='reset' aria-label='Reset Zoom'>
           Reset Zoom
         </button>
       )}
