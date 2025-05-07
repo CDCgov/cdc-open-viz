10e8ca3acc6cae571ca98d06547ba55b268c437d
Monroe <35408073+Mgetz10@users.noreply.github.com>
2025-05-07 11:02:17 -0400

    feature: trimmed data table config

--- packages/data-table/src/types/Config.ts
+++ packages/data-table/src/types/Config.ts
@@ -1,8 +1,28 @@
+import { Column } from '@cdc/core/types/Column'
+import { DataDescription } from '@cdc/core/types/DataDescription'
+import { General } from '@cdc/core/types/General'
 import { Table } from '@cdc/core/types/Table'
+import { Version } from '@cdc/core/types/Version'
+import { VizFilter } from '@cdc/core/types/VizFilter'
 
 export type Config = {
+  columns?: Record<string, Column>
   data?: Object[]
+  dataDescription: Partial<DataDescription>
+  dataFormat?: DataFormat
   dataUrl?: string
-  dataDescription?: string
+  filters?: VizFilter[]
+  general?: General
   table: Table
+  type: 'table'
+  version: Version
+  visualizationType: 'Table'
+}
+
+type DataFormat = {
+  abbreviated: boolean
+  commas: boolean
+  prefix: string
+  roundTo: number
+  suffix: string
 }
