21e3dc723824a309c7c7d663d562965c0cc8b6f6
Jay Boice <jboice@gmail.com>
2025-05-15 10:20:45 -0400

    More vega work

--- packages/editor/src/helpers/vegaConfig.ts
+++ packages/editor/src/helpers/vegaConfig.ts
@@ -1,6 +1,8 @@
 import { DataTransform } from '@cdc/core/helpers/DataTransform'
+import { formatDate } from '@cdc/core/helpers/cove/date.js'
 import { _ } from 'lodash'
 import * as vegaLite from 'vega-lite'
+import * as vega from 'vega'
 
 const CURVE_LOOKUP = {
   linear: 'Linear',
@@ -12,9 +14,10 @@ const CURVE_LOOKUP = {
 }
 
 export const parseVegaConfig = vegaConfig => {
-  if (vegaConfig['$schema'].includes('vega-lite')) {
+  try {
     vegaConfig = vegaLite.compile(vegaConfig).spec
-  }
+  } catch {}
+  console.log('------------- vega config -------------')
   console.log(vegaConfig)
   return vegaConfig
 }
@@ -64,8 +67,26 @@ const getMaxGroupSize = (data, groupBy) => {
 }
 
 const getVegaData = vegaConfig => {
-  const vegaData = Array.isArray(vegaConfig.data) ? vegaConfig.data.find(d => d.values) : vegaConfig.data
-  return Array.isArray(vegaData?.values) ? vegaData.values : undefined
+  const mainMark = getMainMark(vegaConfig)
+  const groupMark = getGroupMark(vegaConfig)
+  const name = groupMark?.from?.facet?.data || mainMark.from.data
+  const view = new vega.View(vega.parse(vegaConfig)).run()
+  const data = view.data(name)
+  const keysToRemove = getKeysToRemove(vegaConfig)
+  data.forEach(d => {
+    keysToRemove.forEach(k => delete d[k])
+    Object.keys(d).forEach(k => {
+      if (d[k] > 1000000000 && d[k] % 1000 === 0) {
+        d[k] = new Date(d[k])
+      }
+      if (d[k] instanceof Date) {
+        d[k] = formatDate('%Y-%m-%d', d[k] - 1000 * 60 * 60 * 10)
+      }
+    })
+  })
+  console.log('------------- vega data -------------')
+  console.log(data)
+  return data
 }
 
 const getGroupMark = vegaConfig => {
@@ -75,17 +96,58 @@ const getGroupMark = vegaConfig => {
 const getSeriesKey = (vegaConfig, xField, yField) => {
   const groupMark = getGroupMark(vegaConfig)
   let seriesKey = groupMark?.from?.facet?.groupby
+  if (seriesKey) return seriesKey
+
+  const mainMark = getMainMark(vegaConfig)
+  const enterEncoder = mainMark.encode.enter
+  const updateEncoder = mainMark.encode.update
+  seriesKey =
+    enterEncoder?.fill?.field ||
+    updateEncoder?.fill?.field ||
+    enterEncoder?.stroke?.field ||
+    updateEncoder?.stroke?.field
+  if (seriesKey) return seriesKey
+
   const stack = getStack(vegaConfig)
-  const vegaData = getVegaData(vegaConfig)
-  const groupBy = _.difference(stack.groupby, [xField, yField])
-  if (stack && !seriesKey && getMaxGroupSize(vegaData, groupBy) > 1) {
-    seriesKey = _.difference(Object.keys(vegaData[0]), [xField, yField])[0]
+  if (stack) {
+    const vegaData = getVegaData(vegaConfig)
+    const groupBy = _.difference(stack.groupby, [xField, yField])
+    if (getMaxGroupSize(vegaData, groupBy) > 1) {
+      let possibleKeys = _.difference(Object.keys(vegaData[0]), [xField, yField])
+      const groupSizes = Object.fromEntries(possibleKeys.map(k => [k, getMaxGroupSize(vegaData, [...groupBy, ...[k]])]))
+      possibleKeys = possibleKeys.filter(k => groupSizes[k] > 1)
+      if (possibleKeys.length) {
+        return possibleKeys.sort((a, b) => (groupSizes[a] > groupSizes[b] ? 1 : -1))[0]
+      }
+    }
   }
-  return seriesKey
+  return undefined
+}
+
+const getKeysToRemove = vegaConfig => {
+  let keysToRemove = []
+  const stack = getStack(vegaConfig)
+  if (stack) {
+    keysToRemove = [...keysToRemove, ...(stack.as || ['y0', 'y1'])]
+  }
+  return keysToRemove
+}
+
+const getXDateFormat = (xField, vegaData) => {
+  const str = vegaData[0][xField].toString()
+  if (str.match(/\d{4}\-\d{2}\-\d{2}/)) {
+    return '%Y-%m-%d'
+  } else if (str.match(/\d{2}\-\d{2}\-\d{4}/)) {
+    return '%m-%d-%Y'
+  } else if (str.match(/\d{2}\/\d{2}\/\d{4}/)) {
+    return '%m/%d/%Y'
+  }
 }
 
 export const convertVegaConfig = (configType: string, vegaConfig: any, config: any) => {
   delete config.newViz
+
+  const vegaData = getVegaData(vegaConfig)
 
   config.vegaType = configType
   config.vegaConfig = vegaConfig
@@ -118,12 +180,19 @@ export const convertVegaConfig = (configType: string, vegaConfig: any, config: a
     const xField = enterEncoder?.x?.field || updateEncoder?.x?.field
     const yField = stackField || enterEncoder?.y?.field || updateEncoder?.y?.field
     const seriesKey = getSeriesKey(vegaConfig, xField, yField)
-    const vegaData = getVegaData(vegaConfig)
 
+    const xDateFormat = getXDateFormat(xField, vegaData)
     const bottomAxis = vegaConfig.axes.sort((a, b) => (a.grid ? 1 : -1)).find(a => a.orient === 'bottom')
     config.xAxis = config.xAxis || {}
     config.xAxis.dataKey = xField
     config.xAxis.label = bottomAxis?.title
+    if (xDateFormat) {
+      config.xAxis.type = configType === 'Bar' ? 'date' : 'date-time'
+      config.xAxis.dateParseFormat = xDateFormat
+      config.xAxis.dateDisplayFormat = '%b. %Y'
+      config.xAxis.showYearsOnce = true
+      config.xAxis.label = ''
+    }
 
     const leftAxis = vegaConfig.axes.sort((a, b) => (a.grid ? 1 : -1)).find(a => a.orient === 'left')
     config.yAxis = config.yAxis || {}
@@ -149,7 +218,7 @@ export const convertVegaConfig = (configType: string, vegaConfig: any, config: a
 
       config.series = [
         {
-          dataKey: stackField || yField,
+          dataKey: yField,
           type: configType,
           axis: 'Left',
           tooltip: true
@@ -168,12 +237,12 @@ export const convertVegaConfig = (configType: string, vegaConfig: any, config: a
     }
   }
 
-  const vegaData = getVegaData(vegaConfig)
   if (vegaData) {
     config.data = vegaData
     config = loadedVegaConfigData(config)
   }
 
+  console.log('------------- cove config -------------')
   console.log(config)
 
   return config
