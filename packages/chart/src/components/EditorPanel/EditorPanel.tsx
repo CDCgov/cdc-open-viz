cf61a8ff9ea8de224121dd5c2f4a0691f1d6bde3
Atash3000 <31108020+Atash3000@users.noreply.github.com>
2025-06-20 10:32:17 -0400

    Fix brush config (#2188)

--- packages/chart/src/components/EditorPanel/EditorPanel.tsx
+++ packages/chart/src/components/EditorPanel/EditorPanel.tsx
@@ -2926,7 +2926,7 @@ const EditorPanel: React.FC<ChartEditorPanelProps> = ({ datasets }) => {
                       />
                       {visHasBrushChart() && (
                         <CheckBox
-                          value={config.brush?.active}
+                          value={config.xAxis.brushActive}
                           section='brush'
                           fieldName='active'
                           label='Brush Slider '
