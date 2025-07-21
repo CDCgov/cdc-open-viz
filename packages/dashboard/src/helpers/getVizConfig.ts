98aa3e34fa4ee32e41bfeae0c6f04fc9b570ca9b
Murad CDC <atamurad.babakulyyev@gmail.com>
2025-07-21 15:32:01 -0400

    Allow Markup Includes & Filtered Text to Blank Out on Reset Value

--- packages/dashboard/src/helpers/getVizConfig.ts
+++ packages/dashboard/src/helpers/getVizConfig.ts
@@ -88,6 +88,20 @@ export const getVizConfig = (
     }
     return visConfigWithFootnotes
   }
+  // for markup-include & filtered text reset data on Reset Value on Filters
+  const filters = config.dashboard.sharedFilters
+
+  const isResetActive = filters.some(f => f.resetLabel && f.resetLabel === f.active)
+  const isMarkupUsed = filters.some(f => f?.usedBy?.includes(visualizationKey))
+  const isFilteredTextUsed = filters.some(f => f?.usedBy?.includes(visualizationKey))
+
+  if (
+    visualizationKey.startsWith('markup-include') ||
+    (visualizationKey.startsWith('filtered-text') && isResetActive && (isMarkupUsed || isFilteredTextUsed))
+  ) {
+    visualizationConfig.data = []
+    visualizationConfig.formattedData = []
+  }
 
   return visualizationConfig as AnyVisualization
 }
