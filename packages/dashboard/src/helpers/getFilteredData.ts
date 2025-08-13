af1e885742c6bc33d190a5d8e67e574af8d101c0
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2025-08-13 12:37:33 -0700

    fix row filtering and footnotes (#2269)

--- packages/dashboard/src/helpers/getFilteredData.ts
+++ packages/dashboard/src/helpers/getFilteredData.ts
@@ -7,7 +7,7 @@ import { getVizKeys } from './getVizKeys'
 
 export const getApplicableFilters = (dashboard: Dashboard, key: string | number): false | SharedFilter[] => {
   const c = dashboard.sharedFilters?.filter(
-    sharedFilter => sharedFilter.usedBy && sharedFilter.usedBy.indexOf(`${key}`) !== -1
+    sharedFilter => (sharedFilter.usedBy && sharedFilter.usedBy.indexOf(`${key}`) !== -1) || sharedFilter.usedBy?.indexOf(key) !== -1
   )
   return c?.length > 0 ? c : false
 }
