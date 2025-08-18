a2ed473565875cfdbf7ec1eba5ce2647454da694
Monroe <35408073+Mgetz10@users.noreply.github.com>
2025-08-18 17:00:07 -0400

    fix [DEV-10732] multi state migration issue (#2274)
    * fix: migration issue
    
    * fix: outline flashing and state shifting

--- packages/map/src/components/UsaMap/components/SingleState/SingleState.StateOutput.tsx
+++ packages/map/src/components/UsaMap/components/SingleState/SingleState.StateOutput.tsx
@@ -2,18 +2,25 @@ import { useContext } from 'react'
 import { Topology } from 'topojson-client'
 import ConfigContext from '../../../../context'
 import { getGeoStrokeColor } from '../../../../helpers/colors'
+import { getStatesPicked } from '../../../../helpers/getStatesPicked'
 
 type StateOutputProps = {
   topoData: Topology
   path: any
   scale: any
+  runtimeData?: any
 }
 
-const StateOutput: React.FC<StateOutputProps> = ({ topoData, path, scale }: StateOutputProps) => {
+const StateOutput: React.FC<StateOutputProps> = ({ topoData, path, scale, runtimeData }: StateOutputProps) => {
   const { config } = useContext(ConfigContext)
   if (!topoData?.states) return null
+
+  // Use filter-aware state selection instead of direct config access
+  const statesPickedData = getStatesPicked(config, runtimeData)
+  const stateNames = statesPickedData.map(sp => sp.stateName)
+
   const statesPicked = topoData.states.filter(s => {
-    return config.general.statesPicked.map(sp => sp.stateName).includes(s.properties.name)
+    return stateNames.includes(s.properties.name)
   })
 
   const geoStrokeColor = getGeoStrokeColor(config)
