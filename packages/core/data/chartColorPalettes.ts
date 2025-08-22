a3deb0603b052e0b309beca298662fe0c50bcec4
adamdoe <17502761+adamdoe@users.noreply.github.com>
2025-08-22 10:33:47 -0700

    standardize casing

--- packages/core/data/chartColorPalettes.ts
+++ packages/core/data/chartColorPalettes.ts
@@ -16,15 +16,15 @@ const colorPalettes2 = {
   ],
 
   'qualitative-soft': [
-    '#A6CEE3',
-    '#1F78B4',
-    '#B2DF8A',
-    '#33A02C',
-    '#FB9A99',
-    '#E31A1C',
-    '#FDBF6F',
-    '#FF7F00',
-    '#ACA9EB'
+    '#a6cee3',
+    '#1f78b4',
+    '#b2df8a',
+    '#33a02c',
+    '#fb9a99',
+    '#e31a1c',
+    '#fdbf6f',
+    '#ff7f00',
+    '#aca9eb'
   ],
   qualitative1: [
     '#a6cee3',
@@ -35,9 +35,9 @@ const colorPalettes2 = {
     '#e31a1c',
     '#6a3d9a',
     '#cab2d6',
-    '#E31A90',
-    '#15017A',
-    '#C2C0FC'
+    '#e31a90',
+    '#15017a',
+    '#c2c0fc'
   ],
 
   qualitative2: ['#7fc97f', '#beaed4', '#ff9', '#386cb0', '#f0027f', '#bf5b17', '#666', '#fedab8'],
@@ -46,60 +46,60 @@ const colorPalettes2 = {
 
   qualitative4: ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ff3', '#a65628', '#f781bf'],
 
-  'sequential-blue': ['#C6DBEF', '#9ECAE1', '#6BAED6', '#4292C6', '#2171B5', '#084594'],
-  'sequential-blue-2-(MPX)': ['#D5F6F9', '#99E2ED', '#5FB6D1', '#3189B0', '#116091', '#0A3E72'],
-  'sequential-orange-(MPX)': ['#FFEFCF', '#FFD49C', '#F7A866', '#EB7723', '#B95117', '#862B0B'],
-  'sequential-green': ['#C7E9C0', '#A1D99B', '#74C476', '#41AB5D', '#238B45', '#005A32'],
+  'sequential-blue': ['#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594'],
+  'sequential-blue-2-(MPX)': ['#d5f6f9', '#99e2ed', '#5fb6d1', '#3189b0', '#116091', '#0a3e72'],
+  'sequential-orange-(MPX)': ['#ffefcf', '#ffd49c', '#f7a866', '#eb7723', '#b95117', '#862b0b'],
+  'sequential-green': ['#c7e9c0', '#a1d99b', '#74c476', '#41ab5d', '#238b45', '#005a32'],
 
   colorblindsafe: ['#e69f00', '#56b4e9', '#009e73', '#f0e442', '#0072b2', '#d55e00', '#cc79a7', '#000000', '#7e5803']
 }
 
 export const colorPalettes3 = {
-  'monochrome-1': ['#A6CEE3', '#15017A'],
-  'monochrome-2': ['#C2C0FC', '#15017A'],
+  'monochrome-1': ['#a6cee3', '#15017a'],
+  'monochrome-2': ['#c2c0fc', '#15017a'],
   'monochrome-3': ['#cab2d6', '#6a3d9a'],
-  'monochrome-4': ['#C2C0FC', '#6a3d9a'],
+  'monochrome-4': ['#c2c0fc', '#6a3d9a'],
   'monochrome-5': ['#fedab8', '#bf5b17'],
   'cool-1': ['#b2df8a', '#1f78b4'],
-  'cool-2': ['#a6cee3', '#72D66B'],
-  'cool-3': ['#C2C0FC', '#386cb0'],
-  'cool-4': ['#72D66B', '#6a3d9a'],
+  'cool-2': ['#a6cee3', '#72d66b'],
+  'cool-3': ['#c2c0fc', '#386cb0'],
+  'cool-4': ['#72d66b', '#6a3d9a'],
   'cool-5': ['#a6cee3', '#6a3d9a'],
   'warm-1': ['#e31a1c', '#fedab8'],
   'complementary-1': ['#1f78b4', '#e6ab02'],
   'complementary-2': ['#1f78b4', '#ff7f00'],
   'complementary-3': ['#6a3d9a', '#ff7f00'],
   'complementary-4': ['#6a3d9a', '#e6ab02'],
-  'complementary-5': ['#df168c', '#1EB386']
+  'complementary-5': ['#df168c', '#1eb386']
 }
 
 const sequentialColors = {
-  'Sequential Blue': ['#C6DBEF', '#9ECAE1', '#6BAED6', '#4292C6', '#2171B5', '#084594'],
-  'Sequential Blue Two': ['#D5F6F9', '#99E2ED', '#5FB6D1', '#3189B0', '#116091', '#0A3E72'],
+  'Sequential Blue': ['#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594'],
+  'Sequential Blue Two': ['#d5f6f9', '#99e2ed', '#5fb6d1', '#3189b0', '#116091', '#0a3e72'],
   'Sequential Blue Three': [
-    '#F5FEFF',
-    '#E1FBFF',
-    '#C0F2FD',
-    '#94E2ED',
-    '#5EBAD4',
-    '#3695BE',
-    '#2273A0',
-    '#0E5181',
+    '#f5feff',
+    '#e1fbff',
+    '#c0f2fd',
+    '#94e2ed',
+    '#5ebad4',
+    '#3695be',
+    '#2273a0',
+    '#0e5181',
     '#093460'
   ],
-  'Sequential Orange': ['#FFEFCF', '#FFD49C', '#F7A866', '#EB7723', '#B95117', '#862B0B'],
+  'Sequential Orange': ['#ffefcf', '#ffd49c', '#f7a866', '#eb7723', '#b95117', '#862b0b'],
   'Sequential Orange Two': [
-    '#FFFDF0',
-    '#FFF7DC',
-    '#FFE9C2',
-    '#FFD097',
-    '#F7A866',
-    '#EB7723',
-    '#B95117',
+    '#fffdf0',
+    '#fff7dc',
+    '#ffe9c2',
+    '#ffd097',
+    '#f7a866',
+    '#eb7723',
+    '#b95117',
     '#853209',
-    '#661F00'
+    '#661f00'
   ],
-  'Sequential Green': ['#C7E9C0', '#A1D99B', '#74C476', '#41AB5D', '#238B45', '#005A32']
+  'Sequential Green': ['#c7e9c0', '#a1d99b', '#74c476', '#41ab5d', '#238b45', '#005a32']
 }
 
 // Process and export chart color palettes
