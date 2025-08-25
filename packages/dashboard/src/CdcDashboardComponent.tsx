bad77ad96cff2181862a9a9a26fceb93302a5faa
adamdoe <17502761+adamdoe@users.noreply.github.com>
2025-08-25 09:24:01 -0700

    Revert "testing breaking dashboard configs"
    This reverts commit 757f8df7645d1b05f884d89ba42aa0fe7bc716eb.

--- packages/dashboard/src/CdcDashboardComponent.tsx
+++ packages/dashboard/src/CdcDashboardComponent.tsx
@@ -166,8 +166,11 @@ export default function CdcDashboard({
         })
 
         const dataNeedsUpdate = reloadURLHelpers.isUpdateNeeded(filters, currentQSParams, updatedQSParams)
-
-        if (!!newFilters || dataNeedsUpdate) {
+        const alreadyFetched = !!dataset.data
+        if (alreadyFetched) {
+          dataWasFetched = true
+          newData[datasetKey] = dataset.data
+        } else if (!!newFilters || dataNeedsUpdate) {
           dataWasFetched = true
           const dataUrlFinal = reloadURLHelpers.getDataURL(
             { ...currentQSParams, ...updatedQSParams },
