9da10bdb2880f3782a7e7d8038879c327ff01c12
adamdoe <17502761+adamdoe@users.noreply.github.com>
2025-09-11 15:05:52 -0700

    update default color for charts

--- packages/chart/src/data/initial-state.js
+++ packages/chart/src/data/initial-state.js
@@ -29,9 +29,9 @@ export default {
     showZeroValueData: true,
     hideNullValue: true,
     palette: {
-      isReversed: false,
+      isReversed: true,
       version: '2.0',
-      name: 'sequential_blue'
+      name: 'sequential_bluereverse'
     }
   },
   padding: {
