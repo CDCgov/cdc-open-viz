f234b9d8b3fd0537bdf53a2136605e540df1b659
Monroe Getz <mgetz10@gmail.com>
2025-09-17 14:09:04 -0400

    fix: more CI issues

--- packages/chart/vitest.config.ts
+++ packages/chart/vitest.config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['../../vitest.setup.ts']
+    setupFiles: ['../../vitest.setup.ts'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.storybook/**',
+      '**/*.stories.*',
+      '**/storybook-static/**'
+    ]
   }
 })
