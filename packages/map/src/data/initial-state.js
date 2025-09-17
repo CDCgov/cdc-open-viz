1a12cfc399d7950611f683b325f436248bafd651
Josh Lacey <williamjoshualacey@gmail.com>
2025-09-17 12:57:22 -0400

    fix [dev-11414] fixed single state no data state (#2304)
    * added enhancement for dev-11414 single state no data message
    
    * changed variable name for clarification

--- packages/map/src/data/initial-state.js
+++ packages/map/src/data/initial-state.js
@@ -2,147 +2,144 @@ import { USE_V2_MIGRATION } from '@cdc/core/helpers/constants'
 
 // Dynamic initial state based on migration flag
 const createInitialState = () => {
-  const paletteDefaults = USE_V2_MIGRATION ? {
-    isReversed: true,
-    name: 'sequential_blue',
-    version: '2.0'
-  } : {
-    isReversed: true,
-    name: 'bluegreen',
-    version: '1.0'
-  }
+  const paletteDefaults = USE_V2_MIGRATION
+    ? {
+        isReversed: true,
+        name: 'sequential_blue',
+        version: '2.0'
+      }
+    : {
+        isReversed: true,
+        name: 'bluegreen',
+        version: '1.0'
+      }
 
   return {
-  annotations: [],
-  general: {
-    navigationTarget: '_self',
-    noStateFoundMessage: 'Map Unavailable',
-    annotationDropdownText: 'Annotations',
-    geoBorderColor: 'darkGray',
-    headerColor: 'theme-blue',
-    title: '',
-    showTitle: true,
-    showSidebar: true,
-    showDownloadMediaButton: false,
-    displayAsHex: false,
-    displayStateLabels: true,
-    territoriesAlwaysShow: false,
-    language: 'en',
-    geoType: 'single-state',
-    geoLabelOverride: '',
-    hasRegions: false,
-    fullBorder: false,
-    type: 'data',
-    convertFipsCodes: true,
-    palette: paletteDefaults,
-    allowMapZoom: true,
-    hideGeoColumnInTooltip: false,
-    hidePrimaryColumnInTooltip: false,
-    statesPicked: [
-      {
-        fipsCode: '01',
-        stateName: 'Alabama'
-      }
-    ]
-  },
-  type: 'map',
-  columns: {
-    geo: {
-      name: 'FIPS Codes',
-      label: 'Location',
-      tooltip: false,
-      dataTable: true
+    annotations: [],
+    general: {
+      navigationTarget: '_self',
+      noDataMessage: 'No State Selected',
+      annotationDropdownText: 'Annotations',
+      geoBorderColor: 'darkGray',
+      headerColor: 'theme-blue',
+      title: '',
+      showTitle: true,
+      showSidebar: true,
+      showDownloadMediaButton: false,
+      displayAsHex: false,
+      displayStateLabels: true,
+      territoriesAlwaysShow: false,
+      language: 'en',
+      geoType: 'single-state',
+      geoLabelOverride: '',
+      hasRegions: false,
+      fullBorder: false,
+      type: 'data',
+      convertFipsCodes: true,
+      palette: paletteDefaults,
+      allowMapZoom: true,
+      hideGeoColumnInTooltip: false,
+      hidePrimaryColumnInTooltip: false,
+      statesPicked: []
     },
-    primary: {
-      dataTable: true,
-      tooltip: true,
-      prefix: '',
-      suffix: '',
-      name: '',
-      label: '',
-      roundToPlace: 0
+    type: 'map',
+    columns: {
+      geo: {
+        name: 'FIPS Codes',
+        label: 'Location',
+        tooltip: false,
+        dataTable: true
+      },
+      primary: {
+        dataTable: true,
+        tooltip: true,
+        prefix: '',
+        suffix: '',
+        name: '',
+        label: '',
+        roundToPlace: 0
+      },
+      navigate: {
+        name: ''
+      },
+      latitude: { name: '' },
+      longitude: { name: '' }
     },
-    navigate: {
-      name: ''
+    legend: {
+      descriptions: {},
+      specialClasses: [],
+      unified: false,
+      singleColumn: false,
+      singleRow: false,
+      verticalSorted: false,
+      showSpecialClassesLast: false,
+      dynamicDescription: false,
+      type: 'equalnumber',
+      numberOfItems: 3,
+      position: 'side',
+      title: '',
+      style: 'circles',
+      subStyle: 'linear blocks',
+      tickRotation: '',
+      singleColumnLegend: false,
+      hideBorder: false,
+      groupBy: ''
     },
-    latitude: { name: '' },
-    longitude: { name: '' }
-  },
-  legend: {
-    descriptions: {},
-    specialClasses: [],
-    unified: false,
-    singleColumn: false,
-    singleRow: false,
-    verticalSorted: false,
-    showSpecialClassesLast: false,
-    dynamicDescription: false,
-    type: 'equalnumber',
-    numberOfItems: 3,
-    position: 'side',
-    title: '',
-    style: 'circles',
-    subStyle: 'linear blocks',
-    tickRotation: '',
-    singleColumnLegend: false,
-    hideBorder: false,
-    groupBy: ''
-  },
-  filters: [],
-  data: [],
-  table: {
-    wrapColumns: false,
-    label: 'Data Table',
-    expanded: false,
-    limitHeight: false,
-    height: '',
-    caption: '',
-    showDownloadUrl: false,
-    showDataTableLink: true,
-    showDownloadLinkBelow: true,
-    showFullGeoNameInCSV: false,
-    forceDisplay: true,
-    download: false,
-    indexLabel: '',
-    cellMinWidth: '0',
-    collapsible: true
-  },
-  tooltips: {
-    appearanceType: 'hover',
-    linkLabel: 'Learn More',
-    opacity: 90
-  },
-  runtime: {
-    editorErrorMessage: []
-  },
-  visual: {
-    minBubbleSize: 1,
-    maxBubbleSize: 20,
-    extraBubbleBorder: false,
-    cityStyle: 'circle',
-    cityStyleLabel: '',
-    showBubbleZeros: false,
-    additionalCityStyles: [],
-    geoCodeCircleSize: 8,
-    showBubbleZeros: false
-  },
-  mapPosition: { coordinates: [0, 30], zoom: 1 },
-  map: {
-    layers: [],
-    patterns: []
-  },
-  hexMap: {
-    type: '',
-    shapeGroups: [
-      {
-        legendTitle: '',
-        legendDescription: '',
-        items: [{ key: '', shape: 'Arrow Up', column: '', operator: '=', value: '' }]
-      }
-    ]
-  },
-  filterBehavior: 'Filter Change',
-  filterIntro: ''
+    filters: [],
+    data: [],
+    table: {
+      wrapColumns: false,
+      label: 'Data Table',
+      expanded: false,
+      limitHeight: false,
+      height: '',
+      caption: '',
+      showDownloadUrl: false,
+      showDataTableLink: true,
+      showDownloadLinkBelow: true,
+      showFullGeoNameInCSV: false,
+      forceDisplay: true,
+      download: false,
+      indexLabel: '',
+      cellMinWidth: '0',
+      collapsible: true
+    },
+    tooltips: {
+      appearanceType: 'hover',
+      linkLabel: 'Learn More',
+      opacity: 90
+    },
+    runtime: {
+      editorErrorMessage: []
+    },
+    visual: {
+      minBubbleSize: 1,
+      maxBubbleSize: 20,
+      extraBubbleBorder: false,
+      cityStyle: 'circle',
+      cityStyleLabel: '',
+      showBubbleZeros: false,
+      additionalCityStyles: [],
+      geoCodeCircleSize: 8,
+      showBubbleZeros: false
+    },
+    mapPosition: { coordinates: [0, 30], zoom: 1 },
+    map: {
+      layers: [],
+      patterns: []
+    },
+    hexMap: {
+      type: '',
+      shapeGroups: [
+        {
+          legendTitle: '',
+          legendDescription: '',
+          items: [{ key: '', shape: 'Arrow Up', column: '', operator: '=', value: '' }]
+        }
+      ]
+    },
+    filterBehavior: 'Filter Change',
+    filterIntro: ''
   }
 }
 
--- packages/map/src/helpers/getStatesPicked.ts
+++ packages/map/src/helpers/getStatesPicked.ts
@@ -3,9 +3,12 @@ import { supportedStatesFipsCodes } from '../data/supported-geos'
 
 export const getStatesPicked = (config, runtimeData) => {
   const stateNames = getFilterControllingStatesPicked(config, runtimeData)
-
-  return stateNames.map(stateName => ({
-    fipsCode: Object.keys(supportedStatesFipsCodes).find(key => supportedStatesFipsCodes[key] === stateName),
-    stateName
-  }))
+  return stateNames.map(stateName => {
+    const matchedStateFips = Object.values(supportedStatesFipsCodes).find(name => name === stateName)
+    if (!matchedStateFips) console.error(`State name "${stateName}" not found.`)
+    return {
+      fipsCode: matchedStateFips,
+      stateName
+    }
+  })
 }
