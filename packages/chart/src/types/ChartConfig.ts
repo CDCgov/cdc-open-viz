d766b19fac4e32bed2d17ad09d5e991b8e23d54f
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2025-11-07 11:35:28 -0800

    [DEV-11546] resolve data rounding (#2391)

--- packages/chart/src/types/ChartConfig.ts
+++ packages/chart/src/types/ChartConfig.ts
@@ -76,6 +76,7 @@ type DataFormat = {
   bottomSuffix: string
   commas: boolean
   prefix: string
+  preserveOriginalDecimals?: boolean
   rightCommas: boolean
   rightPrefix: string
   rightRoundTo: number
