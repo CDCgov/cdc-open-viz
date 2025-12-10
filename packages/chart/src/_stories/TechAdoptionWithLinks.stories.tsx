7ec6786c784cbe39e874e2047e15fb6c10e72224
adamdoe <17502761+adamdoe@users.noreply.github.com>
2025-12-10 12:59:43 -0800

    Enhance horizontal bar chart story by enabling editor mode and adding single series configuration

--- packages/chart/src/_stories/TechAdoptionWithLinks.stories.tsx
+++ packages/chart/src/_stories/TechAdoptionWithLinks.stories.tsx
@@ -1,6 +1,7 @@
 import config from './../../examples/tech-adoption-with-links.json'
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import Chart from '../CdcChart'
+import { editConfigKeys } from '@cdc/core/helpers/configHelpers'
 
 const meta: Meta<typeof Chart> = {
   title: 'Components/Templates/Chart/Bar Chart/Horizontal Bar Chart Link Titles',
@@ -12,7 +13,14 @@ type Story = StoryObj<typeof Chart>
 export const horizontal_barchart_links: Story = {
   args: {
     config: config,
-    isEditor: false
+    isEditor: true
+  }
+}
+
+export const horizontal_barchart_links_single_series: Story = {
+  args: {
+    config: editConfigKeys(config, [{ path: ['tooltips', 'singleSeries'], value: true }]),
+    isEditor: true
   }
 }
 
