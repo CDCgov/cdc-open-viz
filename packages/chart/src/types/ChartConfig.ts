a0c6cc41a4fcecaca3be353b197d302da53b6c4c
adamdoe <17502761+adamdoe@users.noreply.github.com>
2026-01-19 19:15:43 -0800

    fix: update chart configuration types and make properties optional

--- packages/chart/src/types/ChartConfig.ts
+++ packages/chart/src/types/ChartConfig.ts
@@ -1,15 +1,6 @@
 import { DataVisualizationConfig } from '@cdc/core/types/BaseVisualizationConfig'
 import { Axis } from '@cdc/core/types/Axis'
 import { MarkupConfig } from '@cdc/core/types/MarkupVariable'
-import { DataRow } from '@cdc/core/types/DataRow'
-import { type ForestPlotConfigSettings } from './ForestPlot'
-import { type Column } from '@cdc/core/types/Column'
-import { type Series } from '@cdc/core/types/Series'
-import { Runtime } from '@cdc/core/types/Runtime'
-import { Table } from '@cdc/core/types/Table'
-import { BoxPlot } from '@cdc/core/types/BoxPlot'
-import { General as CoreGeneral } from '@cdc/core/types/General'
-import { PaletteConfig } from '@cdc/core/types/Palette'
 
 // --- ChartConfigBase and all chart config interfaces ---
 
@@ -29,8 +20,8 @@ export interface ChartConfigBase extends DataVisualizationConfig, MarkupConfig {
   formattedData: any[] & { urlFiltered: boolean }
   introText: string
   labels: boolean
-  legend: any
-  orientation: any
+  legend?: any
+  orientation?: any
   palette: string
   preliminaryData: any[]
   runtime: any
@@ -56,8 +47,8 @@ export interface ChartConfigBase extends DataVisualizationConfig, MarkupConfig {
   twoColor?: { palette: string }
   visual: any
   visualizationSubType?: string
-  xAxis: any
-  yAxis: any
+  xAxis?: any
+  yAxis?: any
   hideXAxisLabel?: boolean
   hideYAxisLabel?: boolean
   xScale?: Function
@@ -81,6 +72,7 @@ export interface ChartConfigBase extends DataVisualizationConfig, MarkupConfig {
 
 export interface BarChartConfig extends ChartConfigBase {
   visualizationType: 'Bar'
+  orientation: 'horizontal' | 'vertical'
   barHasBorder: 'true' | 'false'
   barHeight: number
   barSpace: number
@@ -91,10 +83,21 @@ export interface BarChartConfig extends ChartConfigBase {
   lollipopColorStyle?: 'regular' | 'two-tone'
   lollipopShape?: string
   lollipopSize?: 'small' | 'medium' | 'large'
+  xAxis: Axis & {
+    type: 'categorical' | 'date' | 'date-time'
+  }
+  yAxis: Axis & {
+    type: 'linear' | 'logarithmic'
+  }
+  legend: ChartConfigBase['legend'] & {
+    hide?: boolean
+    position?: 'top' | 'bottom' | 'left' | 'right'
+  }
 }
 
 export interface LineChartConfig extends ChartConfigBase {
   visualizationType: 'Line'
+  orientation: 'vertical'
   allowLineToBarGraph?: boolean
   convertLineToBarGraph?: boolean
   isolatedDotsSameSize?: boolean
@@ -102,62 +105,150 @@ export interface LineChartConfig extends ChartConfigBase {
   lineDatapointStyle?: 'hidden' | 'always show' | 'hover'
   showLineSeriesLabels?: boolean
   showAreaUnderLine?: boolean
+  xAxis: Axis & {
+    type: 'date' | 'date-time' | 'categorical'
+  }
+  yAxis: Axis & {
+    type: 'linear' | 'logarithmic'
+  }
+  legend: ChartConfigBase['legend'] & {
+    hide?: boolean
+    position?: 'top' | 'bottom' | 'left' | 'right'
+  }
 }
 
 export interface PieChartConfig extends ChartConfigBase {
   visualizationType: 'Pie'
   pieType?: string
   showPiePercent?: boolean
+  xAxis?: Axis
+  yAxis?: Axis
+  legend: ChartConfigBase['legend'] & {
+    hide?: boolean
+    position?: 'top' | 'bottom' | 'left' | 'right'
+    behavior?: 'highlight' | string
+  }
 }
 
 export interface BoxPlotChartConfig extends ChartConfigBase {
   visualizationType: 'Box Plot'
+  orientation: 'horizontal' | 'vertical'
   boxplot: any
   confidenceKeys: any
+  xAxis: Axis & {
+    type: 'categorical' | 'linear'
+  }
+  yAxis: Axis & {
+    type: 'categorical' | 'linear'
+  }
 }
 
 export interface ScatterPlotChartConfig extends ChartConfigBase {
   visualizationType: 'Scatter Plot'
+  orientation: 'vertical'
+  xAxis: Axis & {
+    type: 'linear' | 'date' | 'date-time'
+  }
+  yAxis: Axis & {
+    type: 'linear' | 'logarithmic'
+  }
 }
 
 export interface AreaChartConfig extends ChartConfigBase {
   visualizationType: 'Area Chart'
+  orientation: 'vertical'
   showAreaUnderLine?: boolean
+  xAxis: Axis & {
+    type: 'date' | 'date-time' | 'categorical'
+  }
+  yAxis: Axis & {
+    type: 'linear' | 'logarithmic'
+  }
 }
 
 export interface PairedBarChartConfig extends ChartConfigBase {
   visualizationType: 'Paired Bar'
+  orientation: 'horizontal'
+  xAxis: Axis & {
+    type: 'linear'
+  }
+  yAxis: Axis & {
+    type: 'categorical'
+  }
 }
 
 export interface DeviationBarChartConfig extends ChartConfigBase {
   visualizationType: 'Deviation Bar'
+  orientation: 'horizontal' | 'vertical'
+  xAxis: Axis
+  yAxis: Axis
 }
 
 export interface SparkLineChartConfig extends ChartConfigBase {
   visualizationType: 'Spark Line'
+  orientation: 'vertical'
+  xAxis: Axis & {
+    type: 'date' | 'categorical'
+  }
+  yAxis: Axis & {
+    type: 'linear'
+  }
+  legend?: ChartConfigBase['legend']
 }
 
 export interface ComboChartConfig extends ChartConfigBase {
   visualizationType: 'Combo'
+  orientation: 'vertical'
   stackedAreaChartLineType?: string
+  xAxis: Axis & {
+    type: 'date' | 'date-time' | 'categorical'
+  }
+  yAxis: Axis & {
+    type: 'linear' | 'logarithmic'
+  }
 }
 
 export interface ForecastingChartConfig extends ChartConfigBase {
   visualizationType: 'Forecasting'
+  orientation: 'vertical'
+  xAxis: Axis & {
+    type: 'date' | 'date-time'
+  }
+  yAxis: Axis & {
+    type: 'linear'
+  }
 }
 
 export interface BumpChartConfig extends ChartConfigBase {
   visualizationType: 'Bump Chart'
+  orientation: 'vertical'
+  xAxis: Axis & {
+    type: 'date' | 'date-time' | 'categorical'
+  }
+  yAxis: Axis & {
+    type: 'linear'
+  }
 }
 
 export interface WarmingStripesChartConfig extends ChartConfigBase {
   visualizationType: 'Warming Stripes'
   warmingStripesIntervals?: number
+  xAxis?: Axis
+  yAxis?: Axis
+  orientation?: never
+  legend?: ChartConfigBase['legend']
 }
 
 export type ForestPlotChartConfig = {
   visualizationType: 'Forest Plot'
+  orientation: 'horizontal'
   forestPlot: any
+  xAxis: Axis & {
+    type: 'linear'
+  }
+  yAxis: Axis & {
+    type: 'categorical'
+  }
 } & ChartConfigBase &
   MarkupConfig
 
