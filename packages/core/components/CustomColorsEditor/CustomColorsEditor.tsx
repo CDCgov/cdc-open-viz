60243696da95f78ab59dd031705f47d46b66a365
adamdoe <17502761+adamdoe@users.noreply.github.com>
2026-02-23 17:04:17 -0800

    fix: update default colors to use EMPTY_COLORS constant and improve formatting

--- packages/core/components/CustomColorsEditor/CustomColorsEditor.tsx
+++ packages/core/components/CustomColorsEditor/CustomColorsEditor.tsx
@@ -9,8 +9,10 @@ interface CustomColorsEditorProps {
   minColors?: number
 }
 
+const EMPTY_COLORS: string[] = []
+
 const CustomColorsEditor: React.FC<CustomColorsEditorProps> = ({
-  colors = [],
+  colors = EMPTY_COLORS,
   onChange,
   label = 'Custom Colors',
   minColors = 1
@@ -38,7 +40,7 @@ const CustomColorsEditor: React.FC<CustomColorsEditorProps> = ({
   const handleMoveUp = (index: number) => {
     if (index > 0) {
       const newColors = [...colors]
-      ;[newColors[index - 1], newColors[index]] = [newColors[index], newColors[index - 1]]
+        ;[newColors[index - 1], newColors[index]] = [newColors[index], newColors[index - 1]]
       onChange(newColors)
     }
   }
@@ -46,7 +48,7 @@ const CustomColorsEditor: React.FC<CustomColorsEditorProps> = ({
   const handleMoveDown = (index: number) => {
     if (index < colors.length - 1) {
       const newColors = [...colors]
-      ;[newColors[index], newColors[index + 1]] = [newColors[index + 1], newColors[index]]
+        ;[newColors[index], newColors[index + 1]] = [newColors[index + 1], newColors[index]]
       onChange(newColors)
     }
   }
@@ -82,12 +84,12 @@ const CustomColorsEditor: React.FC<CustomColorsEditorProps> = ({
 
       <div className="custom-colors-notice">
         <svg className="notice-icon" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
         </svg>
         <span>
           <strong>Accessibility Notice:</strong> When using custom colors, conduct a{' '}
-            Section 508 review to ensure adequate color contrast and accessibility compliance.{' '}
+          Section 508 review to ensure adequate color contrast and accessibility compliance.{' '}
         </span>
       </div>
 
