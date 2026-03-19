a44bdc4f6678acacc3ebb71599bcb950c2cd21e7
Kim Hart <103072652+khartco@users.noreply.github.com>
2026-03-19 12:33:08 -0600

    [DEV-12107] Country to State data table filter (#2642)
    * DEV-12107 add filtered state fips code passed to data table for filtering on click
    
    * DEV-12107 Fix for more state types in data
    
    * DEV-12107 Address review comments
    
    * DEV-12101 Adddress zero padding in front of state code comments

--- packages/map/src/types/MapContext.ts
+++ packages/map/src/types/MapContext.ts
@@ -13,6 +13,7 @@ export type MapContext = {
   displayDataAsText: string | number
   displayGeoName: (key: string, displayOverride?: string) => string
   filteredCountryCode: string
+  filteredStateCode: string
   generateRuntimeData: (
     configObj: MapConfig,
     filters: VizFilter[],
@@ -36,6 +37,7 @@ export type MapContext = {
   runtimeLegend
   setParentConfig: Function
   setRuntimeData: Function
+  setFilteredStateCode: (stateCode: string) => void
   setSharedFilterValue: Function
   setConfig: (newState: MapConfig) => MapConfig
   config: MapConfig
