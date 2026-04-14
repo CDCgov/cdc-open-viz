ab072b471b4257ddd88d5069e6a836e620668092
Vidal Iseghohimen <38519184+Viseghoh@users.noreply.github.com>
2026-04-14 10:01:54 -0700

    Remove Tests

--- packages/map/src/helpers/getCountyTopoSources.ts
+++
@@ -1,5 +0,0 @@
-import usExtendedGeography from '../components/UsaMap/data/us-extended-geography.json'
-
-export const getCountyTopoSources = <T>(response: T, showTerritories: boolean) => {
-  return showTerritories ? [response, usExtendedGeography] : [response]
-}
