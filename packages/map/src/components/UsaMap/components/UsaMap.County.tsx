9184386b48fcefa2027eb9abcc4bf609d6c7f8de
Josh Lacey <williamjoshualacey@gmail.com>
2026-04-20 18:06:08 -0400

    fix [dev-12825] hsa highlight not working when pushing event through console. (#2748)

--- packages/map/src/components/UsaMap/components/UsaMap.County.tsx
+++ packages/map/src/components/UsaMap/components/UsaMap.County.tsx
@@ -1112,7 +1112,7 @@ const CountyMap = () => {
     }
 
     drawCanvas()
-  }, [isLoading, topoData, focus, runtimeLegend, runtimeData, featureArray, config])
+  }, [isLoading, topoData, focus, runtimeLegend, runtimeData, featureArray, config, filteredCountyCode])
 
   const showManualZoomControls = config.general.allowMapZoom
   const showResetControl = (hasMoved || focus.id) && (showManualZoomControls || config.general.type === 'us-geocode')
