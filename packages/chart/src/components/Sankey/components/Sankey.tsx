88da7f7e3cf18344a3d741ed9655edbf6af3a2ba
adamdoe <17502761+adamdoe@users.noreply.github.com>
2026-04-24 14:14:47 -0700

    fix: sankey width

--- packages/chart/src/components/Sankey/components/Sankey.tsx
+++ packages/chart/src/components/Sankey/components/Sankey.tsx
@@ -43,7 +43,7 @@ const Sankey = ({ width, height, runtime }: SankeyProps) => {
   useEffect(() => {
     let largest = 0
     groupRefs?.current?.map(g => {
-      const groupWidth = g?.getBoundingClientRect().width
+      const groupWidth = g?.getBBox?.().width || g?.getBoundingClientRect?.().width || 0
       if (groupWidth > largest) {
         largest = groupWidth
       }
@@ -458,7 +458,7 @@ const Sankey = ({ width, height, runtime }: SankeyProps) => {
         >
           <Group className='links'>{allLinks}</Group>
           <Group className='nodes'>{allNodes}</Group>
-          <Group className='finalNodes' style={{ display: 'none' }}>
+          <Group className='finalNodes' style={{ visibility: 'hidden', pointerEvents: 'none' }} aria-hidden='true'>
             {finalNodes}
           </Group>
         </svg>
