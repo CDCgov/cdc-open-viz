70faa6e2178a6445d55a74fedae95b8bc56c4045
Adam Doe <17502761+adamdoe@users.noreply.github.com>
2026-07-02 12:10:58 -0700

    fix: charts > no-full-lodash-import rule (#2924)
    * fix: charts > no-full-lodash-import rule
    
    * fix flaky test

--- packages/chart/src/helpers/getHasBoundarySuppression.ts
+++ packages/chart/src/helpers/getHasBoundarySuppression.ts
@@ -1,4 +1,5 @@
-import _ from 'lodash'
+import pick from 'lodash/pick'
+import values from 'lodash/values'
 import { ChartConfig } from '../types/ChartConfig'
 
 type GetHasBoundarySuppressionProps = {
@@ -15,9 +16,9 @@ export const getHasBoundarySuppression = ({ rows, config }: GetHasBoundarySuppre
 
   return boundaryRows.some(item =>
     suppressionRules.some(({ column, value }) => {
-      const values = _.values(_.pick(item, config.runtime?.seriesKeys))
+      const seriesValues = values(pick(item, config.runtime?.seriesKeys))
       const dynamicCategory = config.series[0]?.dynamicCategory
-      const match = column ? item[column] === value : values.includes(value)
+      const match = column ? item[column] === value : seriesValues.includes(value)
       const dynamic = dynamicCategory && (item[dynamicCategory] === column || !column)
 
       return Boolean(match || dynamic)
