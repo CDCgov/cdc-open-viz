fa7b2a7913aadba41eb603064f83a69954549942
Jay Boice <jboice@gmail.com>
2026-07-20 09:42:38 -0400

    Cap width of nested dropdowns when note is present

--- packages/dashboard/src/components/DashboardFilters/_stories/DashboardFilters.stories.tsx
+++ packages/dashboard/src/components/DashboardFilters/_stories/DashboardFilters.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react-vite'
+import { expect } from 'storybook/test'
 import DashboardFilters from '../DashboardFilters'
 import '../../../scss/main.scss'
 
@@ -74,6 +75,69 @@ export const WithClearButton: Story = {
     showSubmit: true,
     applyFilters: () => {},
     handleReset: () => {}
+  }
+}
+
+const criddLongDiseaseLabel =
+  'Paratyphoid fever (Salmonella enterica serotypes Paratyphi A, B (tartrate negative) and C (S. Paratyphi) infection)'
+
+export const NestedDropdownWithNoteWidthCap: Story = {
+  args: {
+    filters: [
+      {
+        type: 'datafilter',
+        key: 'Condition',
+        filterStyle: 'nested-dropdown',
+        columnName: 'category',
+        showDropdown: true,
+        active: '',
+        resetLabel: 'Type to search for a disease',
+        displaySubgroupingOnly: true,
+        values: ['Enteric Diseases', 'Respiratory diseases'],
+        orderedValues: ['Enteric Diseases', 'Respiratory diseases'],
+        subGrouping: {
+          active: '',
+          columnName: 'condition',
+          valuesLookup: {
+            'Enteric Diseases': {
+              values: ['Campylobacteriosis', criddLongDiseaseLabel],
+              orderedValues: ['Campylobacteriosis', criddLongDiseaseLabel]
+            },
+            'Respiratory diseases': {
+              values: ['Tuberculosis'],
+              orderedValues: ['Tuberculosis']
+            }
+          }
+        },
+        note: 'You can search for a disease name or for keywords.'
+      } as any,
+      {
+        type: 'datafilter',
+        key: 'Topic',
+        values: ['Adult'],
+        active: 'Adult',
+        columnName: 'topic',
+        showDropdown: true,
+        id: 1,
+        parents: []
+      } as any
+    ],
+    show: [0, 1],
+    apiFilterDropdowns: {},
+    handleOnChange: () => {}
+  },
+  play: async ({ canvasElement }) => {
+    const form = canvasElement.querySelector('.dashboard-filters__form') as HTMLElement
+    const field = canvasElement.querySelector('.dashboard-filters__field:has(.nested-dropdown)') as HTMLElement
+    const note = field.querySelector('.filters-section__note-text') as HTMLElement
+    const nestedDropdown = field.querySelector('.nested-dropdown') as HTMLElement
+    const inputContainer = field.querySelector('.nested-dropdown-input-container') as HTMLElement
+
+    expect(form).toHaveClass('filters-section__wrapper--multiple')
+    expect(note).toBeInTheDocument()
+    expect(inputContainer).toHaveAttribute('data-sizing-text', criddLongDiseaseLabel)
+    expect(getComputedStyle(field).maxWidth).toBe(getComputedStyle(note).maxWidth)
+    expect(nestedDropdown.getBoundingClientRect().width).toBeLessThanOrEqual(field.getBoundingClientRect().width + 1)
   }
 }
 
